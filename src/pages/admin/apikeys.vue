bec991c7dd242905d0a11e000c5f8e9213457e43
Kanishk Gupta <kanishkgupta1234@gmail.com>
2021-06-15 18:10:37 +0530

    feat: Added APIKeys page

--- src/pages/admin/apikeys.vue
+++ src/pages/admin/apikeys.vue
@@ -1,10 +1,16 @@
 <template>
+  <div class="p-4">
+    <h1 class="mb-6 ml-1 text-xl font-normal tracking-tight">API Keys</h1>
+    <ListAPIKeys />
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
-export default defineComponent ({ 
+import ListAPIKeys from "~/components/admin/apikeys/listApiKeys.vue";
+export default defineComponent({
+  components: { ListAPIKeys },
+  setup() {},
 });
 </script>
 <style lang="less" module>
-    
 </style>

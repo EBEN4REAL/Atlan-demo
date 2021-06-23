c2433fbf4563180fe8d0f3f48eed50f4b7ab49e2
alii13 <shekhaliul44@gmail.com>
2021-06-24 00:11:51 +0530

    fix: health page

--- src/api/keyMaps/health/index.ts
+++ src/api/keyMaps/health/index.ts
@@ -4,7 +4,7 @@ export const PING_HEKA = "PING_HEKA";
 export const PING_USER = "PING_USER";
 
 const health: Record<string, (...params: any) => string> = {
-  [PING_HEKA]: () => getHealthPath("query", "/debug/health"),
+  [PING_HEKA]: () => getHealthPath("heka/api/query", "/debug/health"),
   [PING_USER]: () => getHealthPath("auth", "/debug/health"),
 };
 

0c878cf495427c973bdb9a8f2ec2f5be08d43f53
alii13 <shekhaliul44@gmail.com>
2021-06-23 13:47:34 +0530

    fix: healt

--- src/api/atlas/health.ts
+++ src/api/atlas/health.ts
@@ -1,10 +1,10 @@
 import { useAPI } from "~/api/useAPIv2";
 
 const pingHeka = () => {
-  return useAPI("PING_HEKA", "GET", {});
+  return useAPI("PING_HEKA", "GET", { cache: "true" });
 };
 const pingUser = () => {
-  return useAPI("PING_USER", "GET", {});
+  return useAPI("PING_USER", "GET", { cache: "true" });
 };
 
 export const Health = {

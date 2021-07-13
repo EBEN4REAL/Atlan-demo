6b40db6ea3ea9c7fb70dc953a1d85e9476d6fe70
alii13 <shekhaliul44@gmail.com>
2021-07-13 22:28:05 +0530

    random

--- src/pinia/discovery/state.ts
+++ src/pinia/discovery/state.ts
@@ -1,3 +1,27 @@
 export interface State {}
 
-export const state: State = {};
+export const state: State = {
+  filters: {
+    status: {
+      checked: [],
+      condition: "OR",
+      criterion: [],
+    },
+    classifications: {
+      checked: [],
+      condition: "OR",
+      criterion: [],
+    },
+    owners: {
+      userValue: "",
+      groupValue: "",
+      condition: "OR",
+      criterion: [],
+    },
+    advanced: {
+      list: [],
+      condition: "OR",
+      criterion: [],
+    },
+  },
+};

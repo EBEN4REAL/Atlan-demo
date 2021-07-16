a0530f9b9bca6a7bbfbe850f0c6d71dc7050e4da
nobal <nobal@atlan.com>
2021-07-16 15:55:09 +0530

    downgrade vue3-virtual-scroller

--- src/modules/scroller.ts
+++ src/modules/scroller.ts
@@ -1,7 +1,7 @@
-// import { UserModule } from "~/types/vitessg";
-// import VueVirtualScroller from "vue3-virtual-scroller";
-// import "vue3-virtual-scroller/dist/vue3-virtual-scroller.css";
+import { UserModule } from "~/types/vitessg";
+import VueVirtualScroller from "vue3-virtual-scroller";
+import "vue3-virtual-scroller/dist/vue3-virtual-scroller.css";
 
-// export const install: UserModule = ({ app }) => {
-//   app.use(VueVirtualScroller);
-// };
+export const install: UserModule = ({ app }) => {
+  app.use(VueVirtualScroller);
+};

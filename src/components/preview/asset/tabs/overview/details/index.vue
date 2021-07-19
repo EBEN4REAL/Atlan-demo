baa7e2bca6ae6375b646f3d00f305fbced8ead26
Varun <varun@atlan.com>
2021-07-19 19:03:13 +0530

    SQL Preview for Views
    - SQL Highlighter

--- src/components/preview/asset/tabs/overview/details/index.vue
+++ src/components/preview/asset/tabs/overview/details/index.vue
@@ -4,11 +4,40 @@
       class="px-2"
       v-if="
         item?.typeName?.toLowerCase() === 'table' ||
-        item?.typeName?.toLowerCase() === 'view'
+        item?.typeName?.toLowerCase() === 'view' ||
+        item?.typeName?.toLowerCase() === 'materialisedview' ||
+        item?.typeName?.toLowerCase() === 'tablepartition'
       "
     >
       <p class="mb-0 text-sm tracking-wide text-gray-400">Rows/Columns</p>
       <p class="mb-0 text-gray">
+        <span
+          v-if="
+            item?.typeName.toLowerCase() === 'view' ||
+            item?.typeName.toLowerCase() === 'materialisedview'
+          "
+        >
+          <a-popover placement="left">
+            <template #content>
+              <div
+                class="overflow-y-auto"
+                style="max-width: 350px; max-height: 250px"
+              >
+                <span
+                  v-html="getSQLHighlighted(item?.attributes.viewDefinition)"
+                >
+                </span>
+              </div>
+              <div class="flex justify-between mt-3">
+                <a>Copy</a>
+                <a>View Lineage</a>
+              </div>
+            </template>
+            <span class="font-bold tracking-wide cursor-pointer text-primary">
+              SQL</span
+            >,
+          </a-popover>
+        </span>
         <span v-if="item?.typeName.toLowerCase() === 'table'">
           <span class="font-bold tracking-wide">{{
             rowCount(item, true)
@@ -37,6 +66,8 @@ import AssetMixin from "~/mixins/asset";
 import Description from "./description.vue";
 import Status from "./status.vue";
 import Owners from "./owners.vue";
+import { highlight } from "sql-highlight";
+import { format } from "sql-formatter";
 
 export default defineComponent({
   mixins: [AssetMixin],
@@ -51,13 +82,40 @@ export default defineComponent({
     },
   },
   setup(props) {
-    return {};
+    const getSQLHighlighted = (text: string) => {
+      return highlight(format(text), {
+        html: true,
+      });
+    };
+    return {
+      getSQLHighlighted,
+    };
   },
 });
 </script>
   
   
      
-<style lang="less" module>
+<style lang="less">
+code.sql {
+  font-family: monospace;
+  background-color: #000;
+}
+
+.sql-hl-keyword {
+  color: #f000f0;
+}
+.sql-hl-function {
+  color: #ff0000;
+}
+.sql-hl-number {
+  color: #00ff00;
+}
+.sql-hl-special {
+  color: #00ff00;
+}
+.sql-hl-bracket {
+  color: #ffff00;
+}
 </style>
           

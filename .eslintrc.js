7fcb10fbd81991d3b7a100c1e367a6fe923f5fa1
Shubhankar Khare <shubhankar@ATMAC038.local>
2021-07-23 16:31:42 +0530

    feat: linting and pre-commits

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:vue/recommended',
+    'airbnb-base',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint',
+    'prettier'
+  ],
+  rules: {
+  },
+};

ac878332e571d463f4320adbd54c0f4b063e6cc9
Kanishk Gupta <kanishkgupta1234@gmail.com>
2021-07-27 19:16:37 +0530

    WIP: Asset profile header

--- src/components/asset/assetProfile/assetHeader.vue
+++ src/components/asset/assetProfile/assetHeader.vue
@@ -1,78 +1,88 @@
 <template>
-  <div>
-    <div class="flex flex-row items-center mb-0">
-      <component :is="asset.typeName" class="w-auto h-6 mr-2"></component>
-      <p
-        class="mb-0 text-xl font-semibold leading-none tracking-wide text-gray-700 "
-      >
-        {{ title(asset) }}
-      </p>
-      <StatusBadge
-        :statusId="asset?.attributes?.assetStatus"
-        class="w-8 ml-2"
-        :key="asset.guid"
-      ></StatusBadge>
+    <div>
+        <div class="flex flex-row items-center mb-0">
+            <component :is="asset.typeName" class="w-auto h-6 mr-2"></component>
+            <p
+                class="mb-0 text-xl font-semibold leading-none tracking-wide text-gray-700 "
+            >
+                {{ title(asset) }}
+            </p>
+            <StatusBadge
+                :statusId="asset?.attributes?.assetStatus"
+                class="w-8 ml-2"
+                :key="asset.guid"
+            ></StatusBadge>
+        </div>
+        <div
+            class="flex flex-row items-start content-center w-full p-2 mt-4 space-x-4  justify-items-start"
+        >
+            <p class="flex items-center content-center">
+                <img :src="logo(asset)" class="w-auto h-4 mr-2" />
+                <RelationshipBadge :item="asset"></RelationshipBadge>
+            </p>
+            <p
+                v-if="['table', 'view'].includes(asset?.typeName.toLowerCase())"
+                class="mb-0 text-gray-600"
+            >
+                <span v-if="asset?.typeName.toLowerCase() === 'table'">
+                    <span class="font-bold tracking-wide text-gray-700"
+                        >~{{ rowCount(asset, true) }}</span
+                    >
+                    rows,
+                </span>
+                <span class="font-bold tracking-wide text-gray-700">{{
+                    columnCount(asset, true)
+                }}</span>
+                cols
+            </p>
+            <div class="flex flex-col">
+                <span class="text-gray-500">
+                    Created:
+                    <span class="text-gray-700">
+                        {{ dayjs().from(asset?.attributes?.__timestamp, true) }}
+                        ago,
+                        {{ asset?.attributes?.__createdBy }}
+                    </span>
+                </span>
+                <span class="text-gray-500">
+                    Last updated:
+                    <span class="text-gray-700">
+                        {{
+                            dayjs().from(
+                                asset?.attributes?.__modificationTimestamp,
+                                true
+                            )
+                        }}
+                        ago,
+                        {{ asset?.attributes?.__modifiedBy }}
+                    </span>
+                </span>
+            </div>
+            <div class="text-xs text-gray-500">
+                <fa icon="fal analytics" class="pushtop"></fa>
+                {{
+                    numeralFormat(asset?.attributes?.popularityScore, '0[.]00')
+                }}
+            </div>
+        </div>
     </div>
-    <div
-      class="flex flex-row items-center content-center w-full p-2 space-x-4  justify-items-start"
-    >
-      <p class="flex items-center content-center mt-3.5">
-        <img :src="logo(asset)" class="w-auto h-4 mr-2" />
-        <RelationshipBadge :item="asset"></RelationshipBadge>
-      </p>
-      <p
-        v-if="['table', 'view'].includes(asset?.typeName.toLowerCase())"
-        class="mb-0 text-gray-600"
-      >
-        <span v-if="asset?.typeName.toLowerCase() === 'table'">
-          <span class="font-bold tracking-wide text-gray-700"
-            >~{{ rowCount(asset, true) }}</span
-          >
-          rows,
-        </span>
-        <span class="font-bold tracking-wide text-gray-700">{{
-          columnCount(asset, true)
-        }}</span>
-        cols
-      </p>
-      <span class="text-gray-500">
-        Created:
-        <span class="text-gray-700">
-          {{ dayjs().from(asset?.attributes?.__timestamp, true) }} ago,
-          {{ asset?.attributes?.__createdBy }}
-        </span>
-      </span>
-      <span class="text-gray-500">
-        Last updated:
-        <span class="text-gray-700">
-          {{ dayjs().from(asset?.attributes?.__modificationTimestamp, true) }}
-          ago,
-          {{ asset?.attributes?.__modifiedBy }}
-        </span>
-      </span>
-      <div class="text-xs text-gray-500">
-        <fa icon="fal analytics" class="pushtop"></fa>
-        {{ numeralFormat(asset?.attributes?.popularityScore, "0[.]00") }}
-      </div>
-    </div>
-  </div>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
-import AssetMixin from "~/mixins/asset";
-import StatusBadge from "@common/badge/status/index.vue";
-import RelationshipBadge from "@common/badge/relationship/index.vue";
-import dayjs from "dayjs";
+import { defineComponent } from 'vue'
+import AssetMixin from '~/mixins/asset'
+import StatusBadge from '@common/badge/status/index.vue'
+import RelationshipBadge from '@common/badge/relationship/index.vue'
+import dayjs from 'dayjs'
 
 export default defineComponent({
-  mixins: [AssetMixin],
-  props: ["asset"],
-  components: { StatusBadge, RelationshipBadge },
-  setup(props) {
-    return {
-      asset: props.asset,
-      dayjs,
-    };
-  },
-});
+    mixins: [AssetMixin],
+    props: ['asset'],
+    components: { StatusBadge, RelationshipBadge },
+    setup(props) {
+        return {
+            asset: props.asset,
+            dayjs,
+        }
+    },
+})
 </script>
--- src/pages/assets/[id]/[tab].vue
+++ src/pages/assets/[id]/[tab].vue
@@ -1,82 +1,73 @@
 <template>
-  <LoadingView v-if="loading" />
-  <ErrorView v-else-if="error" :error="error" />
-<<<<<<< HEAD:src/pages/a/[id]/[tab].vue
-  <div v-else class="h-screen">
-    <div class="p-4 pb-2 h-18">
-=======
-  <div v-else class="w-full">
-    <div class="h-24 p-4">
->>>>>>> 590c0a3bcc66c7ca1f7a00ceff0ed76350fb989c:src/pages/assets/[id]/[tab].vue
-      <AssetHeader :asset="response?.entities[0]" />
+    <LoadingView v-if="loading" />
+    <ErrorView v-else-if="error" :error="error" />
+    <div v-else class="w-full">
+        <div class="h-24 p-4">
+            <AssetHeader :asset="response?.entities[0]" />
+        </div>
+        <a-menu
+            v-model:selectedKeys="current"
+            mode="horizontal"
+            @select="selectTab"
+        >
+            <a-menu-item key="overview"> Overview </a-menu-item>
+            <a-menu-item key="lineage"> Lineage </a-menu-item>
+        </a-menu>
+        <div>
+            <component
+                :is="selectedTab"
+                :asset="response?.entities[0] || {}"
+            ></component>
+        </div>
     </div>
-    <a-menu
-      v-model:selectedKeys="current"
-      mode="horizontal"
-      @select="selectTab"
-    >
-      <a-menu-item key="overview"> Overview </a-menu-item>
-      <a-menu-item key="lineage"> Lineage </a-menu-item>
-    </a-menu>
-<<<<<<< HEAD:src/pages/a/[id]/[tab].vue
-    <div class="p-4 overflow-y-scroll h-4/5">
-=======
-    <div>
->>>>>>> 590c0a3bcc66c7ca1f7a00ceff0ed76350fb989c:src/pages/assets/[id]/[tab].vue
-      <component
-        :is="selectedTab"
-        :asset="response?.entities[0] || {}"
-      ></component>
-    </div>
-  </div>
 </template>
 <script lang="ts">
-import { computed, defineComponent, ref } from "vue";
-import { useRoute, useRouter } from "vue-router";
+import { computed, defineComponent, ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 
-import useAsset from "~/composables/asset/useAsset";
+import useAsset from '~/composables/asset/useAsset'
 
-import Overview from "~/components/asset/assetProfile/tabs/overview.vue";
-import Lineage from "~/components/asset/assetProfile/tabs/lineage.vue";
-import AssetHeader from "~/components/asset/assetProfile/assetHeader.vue";
-import LoadingView from "@common/loaders/section.vue";
-import ErrorView from "@common/error/index.vue";
+import Overview from '~/components/asset/assetProfile/tabs/overview.vue'
+import Lineage from '~/components/asset/assetProfile/tabs/lineage.vue'
+import AssetHeader from '~/components/asset/assetProfile/assetHeader.vue'
+import LoadingView from '@common/loaders/section.vue'
+import ErrorView from '@common/error/index.vue'
 export default defineComponent({
-  components: {
-    overview: Overview,
-    lineage: Lineage,
-    AssetHeader,
-    LoadingView,
-    ErrorView,
-  },
-  setup() {
-    const route = useRoute();
-    const id = computed(() => route?.params?.id || "");
-    const tab = computed(() => route?.params?.tab || "");
+    components: {
+        overview: Overview,
+        lineage: Lineage,
+        AssetHeader,
+        LoadingView,
+        ErrorView,
+    },
+    setup() {
+        const route = useRoute()
+        const id = computed(() => route?.params?.id || '')
+        const tab = computed(() => route?.params?.tab || '')
 
-    const current = ref([tab.value]);
-    const selectedTab = ref(tab.value);
-    const router = useRouter();
+        const current = ref([tab.value])
+        const selectedTab = ref(tab.value)
+        const router = useRouter()
 
-    const { response, error, loading } = useAsset({
-      entityId: id.value,
-    });
+        const { response, error, loading } = useAsset({
+            entityId: id.value,
+        })
 
-    const selectTab = (item: any) => {
-      selectedTab.value = item.key;
-      router.replace(`/assets/${id.value}/${item.key}`);
-    };
+        const selectTab = (item: any) => {
+            selectedTab.value = item.key
+            router.replace(`/assets/${id.value}/${item.key}`)
+        }
 
-    return {
-      current,
-      selectTab,
-      selectedTab,
-      response,
-      error,
-      loading,
-    };
-  },
-});
+        return {
+            current,
+            selectTab,
+            selectedTab,
+            response,
+            error,
+            loading,
+        }
+    },
+})
 </script>
 <route lang="yaml">
 meta:

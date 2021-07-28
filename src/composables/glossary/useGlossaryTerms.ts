809b65e733c0f9aad2e6788df12774ae0365a64b
Aayush Dongre <aayushdongre4@gmail.com>
2021-07-28 22:19:14 +0530

    feat: add pagination to get glossaryTerms

--- src/composables/glossary/useGlossaryTerms.ts
+++ src/composables/glossary/useGlossaryTerms.ts
@@ -1,16 +1,25 @@
-import { ref } from 'vue';
+import { ref, Ref, watch } from 'vue';
 
 import { useAPI } from "~/api/useAPI"
 import { GET_GLOSSARY_TERMS } from "~/api/keyMaps/glossary"
 import { Components } from "~/api/atlas/client";
 
+interface pathVariables {
+    guid?: string;
+    offset?: number;
+    limit?: number;
+}
 
 const useGlossaryTerms = () => {
     const entityGuid = ref<string>();
+    const requestOffset = ref(0);
+    const defaultLimit = 20;
 
-    const pathObject = ref({
+    const pathObject: Ref<pathVariables> = ref({
         guid: entityGuid
     })
+
+    const terms: Ref<Components.Schemas.AtlasGlossaryTerm[]> = ref([]);
 
     const { data, error, isValidating: isLoading, mutate } = useAPI<Components.Schemas.AtlasGlossaryTerm[]>(GET_GLOSSARY_TERMS, 'GET', {
         cache: true,
@@ -19,15 +28,37 @@ const useGlossaryTerms = () => {
         // url
     })
 
-    const fetchGlossaryTerms = (guid: string) => {
+    watch(data, (newData) => {
+        if (newData?.length)
+            terms.value = [...terms.value, ...newData]
+    })
+
+    const fetchGlossaryTerms = (guid: string, limit?: number) => {
         entityGuid.value = guid;
-        pathObject.value = { guid };
+        pathObject.value = { guid, limit };
 
         mutate()
     }
 
+    const fetchGlossaryTermsPaginated = ({ guid, offset, limit, refreshSamePage }: { guid: string, offset?: number, limit?: number, refreshSamePage?: boolean }) => {
+        entityGuid.value = guid;
 
-    return { data, error, isLoading, fetchGlossaryTerms }
+        if (offset) requestOffset.value = offset;
+        if (refreshSamePage) requestOffset.value -= limit ?? defaultLimit;
+
+        pathObject.value = {
+            guid,
+            offset: requestOffset.value,
+            limit: limit ?? defaultLimit
+        };
+
+        mutate();
+
+        requestOffset.value += limit ?? defaultLimit;
+    }
+
+
+    return { data, error, isLoading, fetchGlossaryTerms, fetchGlossaryTermsPaginated }
 }
 
 export default useGlossaryTerms;
--- src/pages/glossary/[id].vue
+++ src/pages/glossary/[id].vue
@@ -1,136 +1,136 @@
 <template>
-  <div class="px-12 pr-0 mb-12">
-    <div class="flex flex-row mt-6 mb-5">
-      <div class="mr-5">
-        <img :src="GlossarySvg"/>
-      </div>
-      <div class="flex flex-col">
-        <span class="secondaryHeading">GLOSSARY</span>
-        <h1 class="text-3xl leading-9 m-0 p-0 text-black font-normal">
-          {{ title }}
-        </h1>
-        <div class="text-sm leading-6 text-gray-400 font-normal">
-          <span class="mr-3">Cerated 2 weeks ago by @anshul</span>
-          <span>&bull;</span>
-          <span class="ml-3">Edited 1 week ago by @anshul</span>
+    <div class="px-12 pr-0 mb-12">
+        <div class="flex flex-row mt-6 mb-5">
+            <div class="mr-5">
+                <img :src="GlossarySvg" />
+            </div>
+            <div class="flex flex-col">
+                <span class="secondaryHeading">GLOSSARY</span>
+                <h1 class="text-3xl leading-9 m-0 p-0 text-black font-normal">
+                    {{ title }}
+                </h1>
+                <div class="text-sm leading-6 text-gray-400 font-normal">
+                    <span class="mr-3">Cerated 2 weeks ago by @anshul</span>
+                    <span>&bull;</span>
+                    <span class="ml-3">Edited 1 week ago by @anshul</span>
+                </div>
+            </div>
         </div>
-      </div>
+        <div>
+            <a-tabs default-active-key="1" class="border-0">
+                <a-tab-pane key="1" tab="Overview">
+                    <div class="flex flex-row m-0 p-0">
+                        <GlossaryProfileOverview :entity="glossary" />
+                        <GlossaryTopTerms
+                            v-if="glossaryTerms?.length"
+                            :terms="glossaryTerms"
+                        />
+                    </div>
+                    <hr />
+                    <GlossaryContinueSettingUp
+                        :terms="glossaryTerms"
+                        :categories="glossaryCategories"
+                    />
+                </a-tab-pane>
+                <a-tab-pane key="2" tab="Terms & Categories">
+                    Terms & Categories
+                </a-tab-pane>
+                <a-tab-pane key="3" tab="Activity"> Activity </a-tab-pane>
+                <a-tab-pane key="4" tab="Bots"> Bots </a-tab-pane>
+                <a-tab-pane key="5" tab="Permissions"> Permissions </a-tab-pane>
+            </a-tabs>
+        </div>
+        <!-- <hr /> -->
     </div>
-    <div>
-      <a-tabs default-active-key="1" class="border-0">
-        <a-tab-pane key="1" tab="Overview">
-          <div class="flex flex-row m-0 p-0">
-            <GlossaryProfileOverview :entity="glossary" />
-            <GlossaryTopTerms
-              v-if="glossaryTerms?.length"
-              :terms="glossaryTerms"
-            />
-          </div>
-          <hr>
-          <GlossaryContinueSettingUp
-            :terms="glossaryTerms"
-            :categories="glossaryCategories"
-          />
-        </a-tab-pane>
-        <a-tab-pane key="2" tab="Terms & Categories">
-          Terms & Categories
-        </a-tab-pane>
-        <a-tab-pane key="3" tab="Activity"> Activity </a-tab-pane>
-        <a-tab-pane key="4" tab="Bots"> Bots </a-tab-pane>
-        <a-tab-pane key="5" tab="Permissions"> Permissions </a-tab-pane>
-      </a-tabs>
-    </div>
-    <!-- <hr /> -->
-  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from "vue";
-import { useHead } from "@vueuse/head";
+import { defineComponent, computed, watch, onMounted  } from 'vue'
 
-import useGTCEntity from "~/composables/glossary/useGtcEntity";
-import useGlossaryTerms from "~/composables/glossary/useGlossaryTerms";
-import useGlossaryCategories from "~/composables/glossary/useGlossaryCategories";
-import { watch } from "vue";
-import { onMounted } from "vue";
+import GlossaryProfileOverview from '@/glossary/glossaryProfileOverview.vue'
+import GlossaryTopTerms from '@/glossary/glossaryTopTerms.vue'
+import GlossaryContinueSettingUp from '@/glossary/glossaryContinueSettingUp.vue'
 
-import Readme from "@/common/readme/index.vue";
-import GlossaryProfileOverview from "@/glossary/glossaryProfileOverview.vue";
-import GlossaryTopTerms from "@/glossary/glossaryTopTerms.vue";
-import GlossaryContinueSettingUp from "@/glossary/glossaryContinueSettingUp.vue";
+import useGTCEntity from '~/composables/glossary/useGtcEntity'
+import useGlossaryTerms from '~/composables/glossary/useGlossaryTerms'
+import useGlossaryCategories from '~/composables/glossary/useGlossaryCategories'
 
-import GlossarySvg from "~/assets/images/gtc/glossary/glossary.png";
+import GlossarySvg from '~/assets/images/gtc/glossary/glossary.png'
 
-interface Proptype{
-  id: string;
+interface Proptype {
+    id: string
 }
 
 export default defineComponent({
-  props: ['id'],
-  components: {
-    Readme,
-    GlossaryProfileOverview,
-    GlossaryTopTerms,
-    GlossaryContinueSettingUp,
-  },
-  setup(props:Proptype) {
-    const id = computed(() => props.id);
+    components: {
+        GlossaryProfileOverview,
+        GlossaryTopTerms,
+        GlossaryContinueSettingUp,
+    },
+    props: ['id'],
+    setup(props: Proptype) {
+        const guid = computed(() => props.id)
 
-    const {
-      data: glossary,
-      error,
-      isLoading,
-      fetchEntity,
-    } = useGTCEntity("glossary");
-    // const {
-    //   data: glossaryTerms,
-    //   error: termsError,
-    //   loading: termsLoading,
-    //   fetchGlossaryTerms,
-    // } = useGlossaryTerms();
-    const {
-      categories: glossaryCategories,
-      error: categoriesError,
-      loading: categoriesLoading,
-      fetchGlossaryCategories,
-      fetchGlossaryCategoriesPaginated
-    } = useGlossaryCategories();
+        const {
+            data: glossary,
+            error,
+            isLoading,
+            fetchEntity,
+        } = useGTCEntity('glossary')
+        const {
+            data: glossaryTerms,
+            error: termsError,
+            loading: termsLoading,
+            fetchGlossaryTermsPaginated,
+        } = useGlossaryTerms()
+        const {
+            categories: glossaryCategories,
+            error: categoriesError,
+            loading: categoriesLoading,
+            fetchGlossaryCategoriesPaginated,
+        } = useGlossaryCategories()
 
-    const title = computed(() => glossary.value?.name);
-    const shortDescription = computed(() => glossary.value?.shortDescription);
-    const termCount = computed(() => glossary.value?.terms?.length ?? 0);
-    onMounted(() => {
-      fetchEntity(id.value);
-      // fetchGlossaryTerms(id.value);
-      fetchGlossaryCategoriesPaginated({guid: id.value, offset: 4});
-    });
+        const title = computed(() => glossary.value?.name)
+        const shortDescription = computed(
+            () => glossary.value?.shortDescription
+        )
+        const termCount = computed(() => glossary.value?.terms?.length ?? 0)
+        onMounted(() => {
+            fetchEntity(guid.value)
+            fetchGlossaryTermsPaginated({ guid: guid.value, offset: 0 })
+            fetchGlossaryCategoriesPaginated({ guid: guid.value, offset: 0 })
+        })
 
-    watch(id, (newGuid) => {
-      fetchEntity(newGuid);
-      // fetchGlossaryTerms(newGuid);
-      // fetchGlossaryCategories(newGuid);
-      fetchGlossaryCategoriesPaginated({guid: newGuid, refreshSamePage: true})
+        watch(guid, (newGuid) => {
+            fetchEntity(newGuid)
+            fetchGlossaryTermsPaginated({
+                guid: newGuid,
+                refreshSamePage: true,
+            })
+            fetchGlossaryCategoriesPaginated({
+                guid: newGuid,
+                refreshSamePage: true,
+            })
+        })
 
-    });
-
-    return {
-      glossary,
-      title,
-      shortDescription,
-      termCount,
-      error,
-      isLoading,
-      GlossarySvg,
-      id,
-      // glossaryTerms,
-      glossaryCategories,
-    };
-  },
-});
+        return {
+            glossary,
+            title,
+            shortDescription,
+            termCount,
+            error,
+            isLoading,
+            GlossarySvg,
+            guid,
+            glossaryTerms,
+            glossaryCategories,
+        }
+    },
+})
 </script>
 <style lang="less">
 .secondaryHeading {
-  @apply tracking-widest text-xs text-gray-400 leading-5;
+    @apply tracking-widest text-xs text-gray-400 leading-5;
 }
 </style>
 

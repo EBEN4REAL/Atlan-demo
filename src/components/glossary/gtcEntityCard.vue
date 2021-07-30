c51a9e72eed341fa1a5dff3c2ad25b955a27ccbd
Aayush Dongre <aayushdongre4@gmail.com>
2021-07-30 12:19:42 +0530

    fix: gtc entity card redirect to profile

--- src/components/glossary/gtcEntityCard.vue
+++ src/components/glossary/gtcEntityCard.vue
@@ -5,7 +5,9 @@
     <img v-else-if="entity.typeName === 'AtlasGlossaryTerm'" :src="TermSvg" /></div>
     
     <div class="flex flex-col ml-4">
-      <h3 class="text-sm leading-5 text-gray-500">{{  entity.displayText }}</h3>
+      <h3 class="text-sm leading-5 text-gray-500">
+        <a-button class="m-0 p-0" type="link" @click="redirectToProfile">{{  entity.displayText }}</a-button>
+      </h3>
       <div class="text-xs leading-4 text-gray-400">
         {{ entity?.attributes?.shortDescription }}
       </div>
@@ -14,6 +16,7 @@
 </template>
 <script lang="ts">
 import { defineComponent, computed } from "vue";
+import { useRouter } from 'vue-router'
 
 import TermSvg from "~/assets/images/gtc/term/term.png";
 import CategorySvg from "~/assets/images/gtc/category/category.png";
@@ -25,11 +28,19 @@ export default defineComponent({
   setup(props) {
     const entity = computed(() => props.entity);
 
+    const router = useRouter();
+
+    const redirectToProfile = () => {
+            if (entity.value.typeName === 'AtlasGlossary') router.push(`/glossary/${entity.value.guid}`)
+            else if(entity.value.typeName === 'AtlasGlossaryCategory') router.push(`/glossary/category/${entity.value.guid}`)
+            else if(entity.value.typeName === 'AtlasGlossaryTerm') router.push(`/glossary/term/${entity.value.guid}`)
+    }
     return {
       entity,
       TermSvg,
       GlossarySvg,
-      CategorySvg
+      CategorySvg,
+      redirectToProfile
     };
   },
 });

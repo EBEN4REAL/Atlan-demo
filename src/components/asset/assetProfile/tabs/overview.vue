436009349be60293bd9754c88010c1c09c06b07b
Shubhankar Khare <shubhankar@ATMAC038.local>
2021-08-04 11:58:41 +0530

    feat: description widget

--- src/components/asset/assetProfile/tabs/overview.vue
+++ src/components/asset/assetProfile/tabs/overview.vue
@@ -1,5 +1,6 @@
+/* eslint-disable vue/no-v-model-argument */
 <template>
-    <div class="w-full h-full px-10 pt-4 overflow-y-auto">
+    <div class="w-full px-10 py-4 overflow-y-auto" style="height: 600px">
         <!--Asset Summary -->
         <div class="mb-8">
             <div
@@ -12,13 +13,53 @@
             >
                 <!-- Description Component -->
                 <div class="mb-8">
-                    <p v-if="asset.attributes.description">
-                        {{ asset.attributes.description }}
+                    <div
+                        v-if="description"
+                        class="flex items-center justify-between"
+                    >
+                        <p class="m-0 truncate overflow-ellipsis">
+                            {{ description }}
+                        </p>
                         <span class="ml-2">.</span>
-                        <a-button type="link"> Edit description </a-button>
-                    </p>
-                    <p v-else class="text-gray-400">No Description Found</p>
+                        <a-button type="link" @click="showModal">
+                            Edit description
+                        </a-button>
+                    </div>
+                    <div v-else class="text-gray-400">
+                        No Description Found
+                        <span class="ml-2">.</span>
+                        <a-button type="link" @click="showModal">
+                            Add description
+                        </a-button>
+                    </div>
                 </div>
+
+                <!-- Edit description modal -->
+                <!-- eslint-disable-next-line vue/no-v-model-argument -->
+                <a-modal v-model:visible="visible" @ok="handleOk">
+                    <template #title
+                        ><p v-if="description">Edit description</p>
+                        <p v-else>Add description</p></template
+                    >
+                    <template #footer>
+                        <a-button key="back" @click="handleCancelModal"
+                            >Cancel</a-button
+                        >
+                        <a-button
+                            key="submit"
+                            type="primary"
+                            :loading="isLoading"
+                            @click="handleOk"
+                            >Update</a-button
+                        >
+                    </template>
+                    <!-- eslint-disable-next-line vue/no-v-model-argument -->
+                    <a-textarea
+                        v-model:value="description"
+                        placeholder="Description"
+                    >
+                    </a-textarea>
+                </a-modal>
 
                 <!-- Table Component -->
                 <a-table
@@ -44,13 +85,50 @@
     // Components
     import Readme from '@/common/readme/index.vue'
 
+    // composables
+    import updateDescription from '~/composables/asset/updateDescription'
+
     export default defineComponent({
         components: { Readme },
         setup(props, context) {
             const asset = ref(context.attrs.asset)
 
+            const visible = ref<boolean>(false)
+            const {
+                isLoading,
+                update,
+                handleCancel,
+                isReady,
+                state,
+                description,
+                isCompleted,
+            } = updateDescription(asset.value)
+
+            const showModal = () => {
+                visible.value = true
+            }
+
+            const handleCancelModal = async () => {
+                await handleCancel()
+                visible.value = false
+            }
+
+            const handleOk = async () => {
+                await update()
+                if (!isCompleted.value) visible.value = false
+            }
             return {
+                showModal,
+                handleOk,
+                visible,
+                isLoading,
                 asset,
+                handleCancel,
+                isReady,
+                state,
+                description,
+                isCompleted,
+                handleCancelModal,
                 columns: [
                     {
                         title: 'Date',

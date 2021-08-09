09ea7323f66dec1a4a0a2dbeffedcb340b326177
Sarbik Betal <sarbikb55@gmail.com>
2021-08-10 01:51:06 +0530

    Column tabs stylings fix

--- src/components/preview/asset/v2/tabs/columns/index.vue
+++ src/components/preview/asset/v2/tabs/columns/index.vue
@@ -1,19 +1,47 @@
 <template>
     <div class="flex flex-col">
-        <a-input class="mb-3" v-model:value="searchTerm" placeholder="Search" />
+        <div class="flex items-center justify-between mt-2 mb-3">
+            <a-input
+                class="mr-2 rounded"
+                v-model:value="searchTerm"
+                placeholder="Search columns"
+            >
+                <template #prefix>
+                    <Fa icon="fal search" class="mr-2 text-gray-description" />
+                </template>
+            </a-input>
+            <a-popover
+                v-model:visible="isFilterVisible"
+                trigger="click"
+                placement="leftTop"
+            >
+                <template #content>
+                    <p class="mb-2 text-sm">Filters</p>
+                    <p class="mb-1 text-xs text-gray-description">By type</p>
+                    <DataTypes v-model:filters="filters" />
+                </template>
+
+                <a-button type="text"
+                    >Filters
+                    <Fa
+                        icon="fas chevron-down"
+                        class="ml-1 transition-transform transform"
+                        :class="isFilterVisible ? '-rotate-180' : 'rotate-0'"
+                /></a-button>
+            </a-popover>
+        </div>
+
         <div
-            class="flex flex-col mb-2"
+            class="flex flex-col mb-4 overflow-y-auto"
             v-for="(asset, index) in filteredList"
             :key="index"
         >
-            <div class="flex items-center">
+            <div class="flex items-center mb-1">
                 <component
                     :is="dataTypeImage(asset)"
-                    class="w-5 h-5 mr-1 text-gray"
+                    class="w-3 h-3 mr-1 text-gray"
                 ></component>
-                <span
-                    class="flex-shrink mr-2 font-bold leading-tight text-gray"
-                >
+                <span class="flex-shrink mr-2 text-xs leading-tight text-gray">
                     {{ asset.displayText }}
                 </span>
                 <div class="chip pkey" v-if="asset.attributes.isPrimary">
@@ -25,24 +53,31 @@
                     <span class="pl-1">Fkey</span>
                 </div>
             </div>
-            <span class="leading-relaxed text-gray-description">
-                {{ asset.attributes.description || '-' }}
+            <span class="text-xs leading-relaxed text-gray-description">
+                {{ asset.attributes.description || 'No description' }}
             </span>
         </div>
-        <div v-if="!isReady">Loading</div>
+        <div
+            v-if="!isReady"
+            class="flex items-center justify-center mt-4 text-sm leading-none"
+        >
+            <a-spin size="small" class="mr-2 leading-none"></a-spin
+            ><span>Getting column info</span>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
-    import { computed, defineComponent, PropType } from 'vue'
+    import { computed, defineComponent, PropType, ref } from 'vue'
     import { toRefs } from '@vueuse/core'
     import { assetInterface } from '~/types/assets/asset.interface'
     import { useColumns } from '~/composables/asset/useColumnRelations'
     import useAssetInfo from '~/composables/asset/useAssetInfo'
     import { dataTypeList } from '~/constant/datatype'
+    import DataTypes from '@common/facets/dataType.vue'
 
     export default defineComponent({
-        components: {},
+        components: { DataTypes },
         props: {
             id: String,
             componentData: {
@@ -54,20 +89,23 @@
             },
         },
         setup(props) {
+            const isFilterVisible = ref(false)
             const { dataTypeImage } = useAssetInfo()
             const { selectedAsset } = toRefs(props)
 
             const assetId = computed(() => selectedAsset.value.guid)
-            const { filteredList, isReady, error, searchTerm } =
+            const { filteredList, isReady, error, searchTerm, filters } =
                 useColumns(assetId)
 
             return {
+                isFilterVisible,
                 filteredList,
                 searchTerm,
                 dataTypeImage,
                 isReady,
                 error,
                 dataTypeList,
+                filters,
             }
         },
     })
@@ -79,6 +117,7 @@
         @apply rounded;
         @apply flex;
         @apply items-center;
+        @apply text-xs;
     }
     .pkey {
         color: #52c7d7;

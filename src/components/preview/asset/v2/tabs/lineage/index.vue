0a80369a191910ec073627d97250c672bf7bd097
Ebuka Ezeh <ebuka@atlan.com>
2021-08-14 00:28:40 +0100

    update: Migrate lineage tab to asset preview V2

--- src/components/preview/asset/v2/tabs/lineage/index.vue
+++ src/components/preview/asset/v2/tabs/lineage/index.vue
@@ -1,27 +1,223 @@
 <template>
-    <div class="w-full">Lineage</div>
+    <!-- <LoadingView
+        v-if="Object.keys(filteredLineageList).length === 0 || loading"
+        class="mt-2"
+    ></LoadingView> -->
+    <div
+        v-if="loading"
+        class="flex items-center justify-center mt-4 text-sm leading-none"
+    >
+        <a-spin size="small" class="mr-2 leading-none"></a-spin
+        ><span>Getting lineage data</span>
+    </div>
+    <a-collapse
+        v-else
+        v-model="activeKey"
+        :bordered="false"
+        :accordion="true"
+        :class="$style.filter"
+        class="bg-transparent"
+        @change="updateActiveKey"
+    >
+        <template #expandIcon="{ isActive }">
+            <fa
+                v-if="!isActive"
+                icon="fas chevron-right"
+                class="text-primary"
+            />
+            <fa v-else icon="fas chevron-down" class="text-primary" />
+        </template>
+        <a-collapse-panel
+            v-for="stream in streams"
+            :key="stream.key"
+            class="mb-4 bg-transparent"
+        >
+            <template #header>
+                <div class="flex items-center justify-between w-full">
+                    <div class="font-semibold">
+                        {{ stream.name }} ({{
+                            lineageList[stream.key]?.length || 0
+                        }})
+                    </div>
+                </div>
+                <router-link
+                    @click="$event.stopPropagation()"
+                    :to="`/assets/${id || guid}/lineage`"
+                    class="flex w-32 underline"
+                    >Graph view
+                    <fa icon="fal external-link-alt" class="w-3 h-3 ml-1"></fa
+                ></router-link>
+            </template>
+
+            <Stream
+                :direction="stream.key"
+                :lineage-list="filteredLineageList[stream.key]"
+            />
+        </a-collapse-panel>
+    </a-collapse>
 </template>
 
 <script lang="ts">
-    import { defineComponent, PropType } from 'vue'
+    // Vue
+    import {
+        defineComponent,
+        ref,
+        Ref,
+        toRefs,
+        ToRefs,
+        watch,
+        provide,
+        onMounted,
+        computed,
+        PropType,
+    } from 'vue'
+    import { useRoute } from 'vue-router'
+
+    // Components
+    import LoadingView from '@common/loaders/section.vue'
+    import Stream from './stream/index.vue'
+    // Composables
+    import useLineage from '~/composables/lineage/useLineage'
+    import * as useLineageCompute from '~/composables/lineage/useLineageCompute'
+    import useLineageFilters from '~/composables/lineage/useLineageFilters'
+    // Types
     import { assetInterface } from '~/types/assets/asset.interface'
+    import { generateUUID } from '~/utils/generator'
 
     export default defineComponent({
-        components: {},
+        components: { Stream, LoadingView },
         props: {
-            id: String,
-            componentData: {
-                type: Object as PropType<any>,
-            },
             selectedAsset: {
                 type: Object as PropType<assetInterface>,
                 required: true,
             },
         },
         setup(props) {
-            return {}
+            const { selectedAsset }: ToRefs = toRefs(props)
+            const activeKey: Ref<string> = ref('upstream')
+            const guid = computed(() => selectedAsset.value.guid)
+            const loading = ref(true)
+            const lineage: Ref<{}> = ref({})
+            const lineageList = ref({})
+            const filteredLineageList = ref({})
+            const assetTypesLengthMap = ref({})
+            const depth = ref(1)
+            const query = ref('')
+            const filters = ref(['Table', 'View', 'Column', 'Bi Dashboard'])
+            const streams = [
+                {
+                    key: 'upstream',
+                    name: 'Upstream',
+                },
+                {
+                    key: 'downstream',
+                    name: 'Downstream',
+                },
+            ]
+
+            /** COMPUTED */
+            const route = useRoute()
+            const id = computed(() => route?.params?.id || '')
+
+            const updateActiveKey = (key) => {
+                if (key) activeKey.value = key
+            }
+
+            const updateDepth = (val: number) => {
+                depth.value = val
+            }
+            const searchQuery = (val: string) => {
+                query.value = val
+            }
+            const updateFilters = (val: Ref<string[]>) => {
+                filters.value = val
+            }
+            const filtersLength = computed(() => filters.value.length)
+
+            const fetch = () => {
+                loading.value = true
+                const currGuid = id.value || guid.value
+                const { data } = useLineage(currGuid, depth.value, 'BOTH')
+                watch(data, () => {
+                    lineage.value = data.value as object
+                    lineageList.value = useLineageCompute.computeGraphRelations(
+                        lineage,
+                        'widget'
+                    )
+                    filter()
+                })
+            }
+
+            const filter = () => {
+                const { data, l } = useLineageFilters(
+                    lineageList,
+                    filters,
+                    query,
+                    activeKey
+                )
+                filteredLineageList.value = data
+                assetTypesLengthMap.value[activeKey.value] = l
+                loading.value = false
+            }
+
+            provide('activeKey', activeKey)
+            provide('updateDepth', updateDepth)
+            provide('updateFilters', updateFilters)
+            provide('searchQuery', searchQuery)
+            provide('assetTypesLengthMap', assetTypesLengthMap)
+
+            watch(depth, () => fetch())
+            watch(guid, () => fetch())
+            watch(id, () => fetch())
+            watch(query, () => filter())
+            watch(activeKey, () => filter())
+            watch(filtersLength, () => filter())
+
+            onMounted(fetch)
+
+            return {
+                id,
+                loading,
+                guid,
+                query,
+                lineage,
+                lineageList,
+                filteredLineageList,
+                assetTypesLengthMap,
+                activeKey,
+                streams,
+                updateActiveKey,
+            }
         },
     })
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+    :global(.ant-tabs .ant-tabs-right-content) {
+        @apply pr-0 !important;
+    }
+</style>
+<style lang="less" module>
+    .filter {
+        :global(.ant-collapse-item) {
+            @apply border-none;
+        }
+
+        :global(.ant-collapse-header) {
+            padding-left: 18px !important;
+            padding-right: 0px !important;
+            @apply flex items-center !important;
+        }
+        :global(.ant-collapse-arrow) {
+            left: 0px !important;
+            font-size: 0.85rem !important;
+            @apply text-primary !important;
+        }
+
+        :global(.ant-collapse-content-box) {
+            padding-right: 0px;
+            padding-left: 0px;
+            padding-top: 0px;
+        }
+    }
+</style>
---
+++ src/components/preview/asset/v2/tabs/lineage/stream/assetList.vue
@@ -0,0 +1,69 @@
+<template>
+    <div
+        v-if="lineageList.length > 0"
+        class="overflow-y-scroll border rounded-sm"
+        style="max-height: 310px"
+    >
+        <table class="w-full table-auto">
+            <tbody>
+                <tr
+                    v-for="(asset, index) in lineageList"
+                    :key="index"
+                    class="border-b"
+                    :class="{
+                        'border-none': index === lineageList.length - 1,
+                    }"
+                >
+                    <router-link :to="`/assets/${asset.guid}/lineage`">
+                        <td
+                            class="flex items-center justify-between p-1 px-3 cursor-pointer  hover:bg-primary-light hover:bg-opacity-20"
+                        >
+                            <div class="flex items-center w-full">
+                                <component
+                                    :is="asset.typeName"
+                                    class="w-auto h-3 mr-2"
+                                ></component>
+                                <div class="w-10/12 truncate">
+                                    {{ asset.displayText }}
+                                </div>
+                            </div>
+                            <img
+                                :src="imgSrc(asset.source)"
+                                class="w-auto h-3"
+                            />
+                        </td>
+                    </router-link>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script lang="ts">
+    // Vue
+    import { defineComponent } from 'vue'
+    // Constants
+    import { SourceList } from '~/constant/source'
+
+    export default defineComponent({
+        props: {
+            lineageList: {
+                type: Array,
+                required: true,
+            },
+        },
+        setup() {
+            const imgSrc = (source: string) => {
+                const item = SourceList.find(
+                    (src) => src.id === source.toLowerCase()
+                )
+                return item?.image || ''
+            }
+
+            return {
+                imgSrc,
+                SourceList,
+            }
+        },
+    })
+</script>

4c968da34955dfd48ee2a8f911df1cc9ef59a9fa
Sarbik Betal <sarbikb55@gmail.com>
2021-08-13 23:17:57 +0530

    Created 2 modes for virtualization and made them as composables

--- src/lib/virtualList.vue
+++ src/lib/virtualList.vue
@@ -19,6 +19,7 @@
                 :key="item[dataKey]"
                 class="flex-shrink-0 virtual-list-item"
             >
+                {{ idx + listIndices[0] }}
                 <slot :item="item" :index="idx + listIndices[0]"></slot>
             </div>
             <!-- This element is only rendered when the virtual list does not end at last element -->
@@ -42,12 +43,14 @@
         watch,
         computed,
         nextTick,
-        reactive,
     } from 'vue'
+    import useDynamicHeightList from './virtualList/dynamicList'
+    import useFixedHeightList from './virtualList/fixedList'
 
     interface Props {
         data: any[]
         dataKey: string
+        variableHeight: boolean
     }
 
     /**
@@ -71,9 +74,13 @@
                 type: String,
                 default: () => 'id',
             },
+            variableHeight: {
+                type: Boolean,
+                default: () => false,
+            },
         },
         setup(props: Props): any {
-            const { data } = toRefs(props)
+            const { data, variableHeight } = toRefs(props)
             const root = ref<Element | null>(null)
             const wrapper = ref<Element | null>(null)
 
@@ -86,27 +93,21 @@
             const loadBottom = ref<Element | null>(null)
             const loadTop = ref<Element | null>(null)
 
-            /** Arbitrary size of a single batch */
-            const GROUP_SIZE = 20
+            const rowCount = computed(() => data.value.length)
 
-            /**
-             * Array containing the height from which the
-             * i-th elemnent start inside the scroll container
-             */
-            const listSize = [0]
-
-            /**
-             * It containes the highest [1] and lowest [0] index of the current visible buffer
-             */
-            const listIndices = ref([0, 0])
+            const { handleIntersection, topHeight, fullHeight, listIndices } =
+                variableHeight.value
+                    ? useDynamicHeightList(rowCount, wrapper)
+                    : useFixedHeightList(rowCount, wrapper)
 
             // We dynamically change these css property to give the illusion of scroll
-            const wrapperStyle = reactive({
-                height: 'auto',
-            })
-            const topStyle = reactive({
-                height: 'auto',
-            })
+            const wrapperStyle = computed(() => ({
+                height: fullHeight.value + 'px',
+            }))
+
+            const topStyle = computed(() => ({
+                height: topHeight.value + 'px',
+            }))
 
             /** Slice of the data that is actually rendered on the screen */
             const pool = computed(() =>
@@ -114,57 +115,6 @@
             )
 
             let iObserver: IntersectionObserver
-
-            /**
-             * Does a binary search on `arr` and return the highest value less than `height`.
-             */
-            function calculateIndexFromHeight(arr: number[], height: number) {
-                // Do a binary search on the list
-                let low = 0
-                let high = Array.isArray(arr)
-                    ? arr.length - 1
-                    : Object.keys(arr).length - 1
-                let mid
-                while (low < high) {
-                    mid = Math.floor((high + low) / 2)
-                    // Check if height is present at middle position
-                    if (arr[mid] === height) {
-                        break
-                    } else if (arr[mid] > height) {
-                        high = mid - 1
-                    } else {
-                        low = mid + 1
-                    }
-                }
-
-                mid = Math.floor((high + low) / 2)
-                if (height <= arr[mid]) return mid - 1
-                return mid
-            }
-
-            /**
-             * Does all the calculations and sets the appropriate indices
-             * for the visible buffer and also sets the proper height
-             * of the containers.
-             */
-            function handleIntersection() {
-                const range: number[] = []
-                range[0] = calculateIndexFromHeight(
-                    listSize,
-                    root.value.scrollTop
-                )
-                range[1] = calculateIndexFromHeight(
-                    listSize,
-                    root.value.scrollTop + root.value?.clientHeight
-                )
-                const diff = Math.round((range[1] - range[0]) / 2)
-                listIndices.value = [
-                    Math.max(0, range[0] - diff),
-                    Math.min(data.value.length, range[1] + diff),
-                ]
-                topStyle.height = `${listSize[listIndices.value[0]]}px`
-                wrapperStyle.height = `${listSize[listSize.length - 1] + 30}px`
-            }
 
             function observeThreshold(observer: IntersectionObserver) {
                 if (loadBottom.value) observer.observe(loadBottom.value)
@@ -187,7 +137,10 @@
                 )
                 if (intersecting) {
                     observer.disconnect()
-                    handleIntersection()
+                    handleIntersection(
+                        root.value?.scrollTop,
+                        root.value?.clientHeight
+                    )
                     nextTick(() => observeThreshold(observer))
                 }
             }
@@ -196,10 +149,10 @@
              * Initializes/resets the internal state of the component
              */
             async function init() {
-                listSize.splice(1)
-                listIndices.value = [0, Math.min(GROUP_SIZE, data.value.length)]
-                topStyle.height = 'auto'
-                wrapperStyle.height = 'auto'
+                // listSize.splice(1)
+                // listIndices.value = [0, Math.min(GROUP_SIZE, data.value.length)]
+                // topStyle.height = 'auto'
+                // wrapperStyle.height = 'auto'
                 iObserver = new IntersectionObserver(observationCallback, {
                     root: root.value,
                     threshold: [0],
@@ -210,33 +163,12 @@
 
             onMounted(init)
 
-            watch(
-                listIndices,
-                () => {
-                    const index = listIndices.value
-                    // Set it to 1 if ref="loadTop" element is visible
-                    const offset = index[0] ? 1 : 0
-
-                    // Keep recording the height of the elements
-                    // TODO: Account for the spacer elements
-                    while (listSize.length < index[1] + offset) {
-                        const nthChild = listSize.length - (index[0] + offset)
-                        const size =
-                            listSize[listSize.length - 1] +
-                            wrapper.value?.children[nthChild].clientHeight
-                        listSize.push(size)
-                    }
-                },
-                { flush: 'post' }
-            )
-
             watch(data, init)
 
             return {
                 pool,
                 root,
                 wrapper,
-                listSize,
                 wrapperStyle,
                 topStyle,
                 listIndices,

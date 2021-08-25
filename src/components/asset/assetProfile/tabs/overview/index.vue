574f080324d8aa7106917045f770b9ab299639a1
Ebuka Ezeh <ebuka@atlan.com>
2021-08-25 15:38:10 +0100

    update: Use injected data

--- src/components/asset/assetProfile/tabs/overview/index.vue
+++ src/components/asset/assetProfile/tabs/overview/index.vue
@@ -1,14 +1,14 @@
 <template>
-    <!-- nonBiAssets -->
-    <nonBiAsset v-if="assetType === 'nonBiAsset'" />
-
     <!-- tableauAsset -->
-    <tableauAsset v-else />
+    <tableauAsset v-if="assetType.includes('Tableau')" />
+
+    <!-- nonBiAssets -->
+    <nonBiAsset v-else />
 </template>
 
 <script lang="ts">
     // Vue
-    import { defineComponent } from 'vue'
+    import { defineComponent, ref, inject, computed } from 'vue'
 
     // Components
     import nonBiAsset from '~/components/asset/assetProfile/tabs/overview/nonBiAsset/index.vue'
@@ -17,8 +17,18 @@
     export default defineComponent({
         components: { nonBiAsset, tableauAsset },
         setup() {
+            /** DATA */
+            const assetType = ref('')
+
+            /** INJECTIONS */
+            const assetDataInjection = inject('assetData')
+
+            /** COMPUTED */
+            const assetData = computed(() => assetDataInjection?.asset)
+            assetType.value = assetData.value.typeName
+
             return {
-                assetType: 'nonBiAsset',
+                assetType,
             }
         },
     })

497e41b13580b4543a6194bc1ffab83dd55e55a6
Shivansh Pahwa <shivanshpahwa24@gmail.com>
2021-08-27 18:06:21 +0530

    removed usage from uncollapsed panels

--- src/components/asset/assetProfile/tabs/overview/nonBiAsset/columnPreview/tabs/info/index.vue
+++ src/components/asset/assetProfile/tabs/overview/nonBiAsset/columnPreview/tabs/info/index.vue
@@ -122,7 +122,7 @@
                         localStorage.getItem('column_preview_info_tab') as any
                     ) as string[]
 
-                return ['columnDetails', 'linkedAsset', 'usage']
+                return ['columnDetails', 'linkedAsset']
             }
             function setUserDefaultCollapseOrderInInfoTab(
                 activeKeyOrder: string[]

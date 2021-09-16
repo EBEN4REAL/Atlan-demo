dd2cd0a2280e6a6682bfdfdd6449541c1b0fb14d
Shivansh Pahwa <shivanshpahwa24@gmail.com>
2021-09-17 02:19:54 +0530

    inline description changes

--- src/components/common/sidebar/description.vue
+++ src/components/common/sidebar/description.vue
@@ -1,69 +1,39 @@
 <template>
     <div class="mb-3 text-xs text-gray-500">
         <p class="mb-1 text-xs">Description</p>
-        <a-popover
-            v-model:visible="isCompleted"
-            placement="left"
-            trigger="click"
-            @visibleChange="handleVisibleChange"
-            :destroyTooltipOnHide="true"
+        <div
+            v-if="
+                (description && description !== '') || showEditableDescription
+            "
         >
-            <template #content>
-                <div class="">
-                    <a-textarea
-                        v-model:value="descriptionInput"
-                        autofocus
-                        placeholder="Add an asset description"
-                        show-count
-                        :maxlength="140"
-                        :rows="4"
-                        style="width: 280px"
-                        @change="handleTextAreaUpdate"
-                    >
-                    </a-textarea>
-                    <div class="flex justify-end w-full mt-4 space-x-4">
-                        <a-button class="px-4" @click="handleCancel"
-                            >Cancel</a-button
-                        >
-                        <a-button
-                            type="primary"
-                            class="px-4"
-                            :loading="isLoading"
-                            @click="handleUpdate"
-                            >Update</a-button
-                        >
-                    </div>
-                </div>
-            </template>
-            <div class="inline-block text-sm cursor-pointer text-gray">
-                <p v-if="description" class="mb-0">
-                    {{ description }}
-                </p>
-                <div v-else>
-                    <div
-                        class="flex items-center cursor-pointer  text-primary hover:text-primary hover:underline"
-                    >
-                        <!-- <span class="flex items-center text-xs">
-                            <fa icon="fal plus" />
-                        </span> -->
-                        <span class="text-xs">Update description</span>
-                    </div>
-                </div>
-            </div>
-        </a-popover>
+            <div
+                id="description-sideabr"
+                class="inline-block text-sm cursor-pointer text-gray"
+                contenteditable
+                @blur="handleDescriptionEdit"
+                @keydown.enter="endEditDescription"
+                @keydown.esc="cancelEditDescription"
+                v-text="description"
+            ></div>
+        </div>
+        <!-- <span v-if="description" class="mb-0">
+                {{ description }}
+            </span>-->
+
+        <span
+            v-if="
+                (!description || description === '') && !showEditableDescription
+            "
+            class="text-xs cursor-pointer text-primary hover:underline"
+            @click="handleAddDescriptionClick"
+        >
+            Add description
+        </span>
     </div>
 </template>
 
 <script lang="ts">
-    import { useMagicKeys } from '@vueuse/core'
-    import {
-        defineComponent,
-        nextTick,
-        PropType,
-        ref,
-        toRefs,
-        watch,
-    } from 'vue'
+    import { defineComponent, PropType, toRefs, ref, watch } from 'vue'
     import updateDescription from '~/composables/asset/updateDescription'
     import { assetInterface } from '~/types/assets/asset.interface'
 
@@ -77,57 +47,40 @@
         emits: ['update:selectedAsset'],
         setup(props, { emit }) {
             const { selectedAsset } = toRefs(props)
-            const {
-                isLoading,
-                update,
-                handleCancel,
-                isReady,
-                state,
-                description,
-                isCompleted,
-            } = updateDescription(selectedAsset)
+            const { update, handleCancel, description } =
+                updateDescription(selectedAsset)
 
-            const handleUpdate = () => {
+            const showEditableDescription = ref<boolean>(false)
+
+            const handleDescriptionEdit = (e: any) => {
+                description.value = e.target.innerText
+                showEditableDescription.value = false
                 update()
             }
 
-            const descriptionInput = ref(description.value)
-            const handleVisibleChange = () => {
-                descriptionInput.value = description.value
-                if (descriptionInput?.value) {
-                    nextTick(() => {
-                        descriptionInput?.value?.$el?.focus()
-                    })
-                }
-            }
-            const handleTextAreaUpdate = (e: any) => {
-                description.value = e.target.value
+            const endEditDescription = () => {
+                document.getElementById('description-sidebar').blur()
             }
 
-            const keys = useMagicKeys()
-            const esc = keys.Escape
+            const cancelEditDescription = () => {
+                handleCancel()
+            }
 
-            watch(esc, (v) => {
-                if (v) {
-                    handleCancel()
-                }
-            })
+            const handleAddDescriptionClick = () => {
+                showEditableDescription.value = true
+            }
 
             watch(description, () => {
                 emit('update:selectedAsset', selectedAsset.value)
             })
 
             return {
-                handleTextAreaUpdate,
-                handleUpdate,
-                handleCancel,
-                handleVisibleChange,
-                descriptionInput,
-                isReady,
-                state,
                 description,
-                isCompleted,
-                isLoading,
+                handleDescriptionEdit,
+                endEditDescription,
+                cancelEditDescription,
+                showEditableDescription,
+                handleAddDescriptionClick,
             }
         },
     })

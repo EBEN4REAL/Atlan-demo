d769e7fd08d8eeb56c45007af8b9516d19a68989
Ebuka Ezeh <ebuka@atlan.com>
2021-09-24 10:11:45 +0100

    lint

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,20 @@
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
 
 export default {
-  // input: 'node_modules/xml2js/lib/xml2js.js',
-  // output: {
-  //   dir: 'src/cjs',
-  //   format: 'iife',
-  //   exports: 'named',
-  //   name: 'xml2js',
-  // },
-  plugins: [
-    resolve(),
-    commonjs({
-        exclude: [/./],
-        transformMixedEsModules: true,
-    defaultIsModuleExports: true
-  })],
-};
+    // input: 'node_modules/xml2js/lib/xml2js.js',
+    // output: {
+    //   dir: 'src/cjs',
+    //   format: 'iife',
+    //   exports: 'named',
+    //   name: 'xml2js',
+    // },
+    plugins: [
+        resolve(),
+        commonjs({
+            exclude: [/./],
+            transformMixedEsModules: true,
+            defaultIsModuleExports: true,
+        }),
+    ],
+}

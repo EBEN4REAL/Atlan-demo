d2be26d09fc68516464a7ec893a34992ad023dc1
Aayush Dongre <aayushdongre4@gmail.com>
2021-09-28 19:16:09 +0530

    feat: composables to load saved queries

--- src/types/insights/savedQuery.interface.ts
+++ src/types/insights/savedQuery.interface.ts
@@ -1,6 +1,42 @@
+import { Components } from '~/api/atlas/client'
 export interface SavedQueryInterface {
     id: string
     label: string
     editor: string
     result: string
+}
+
+export type Attributes =  {
+    assetStatus?: string
+    assetStatusMessage?: string
+    assetStatusUpdatedBy?: string
+    ownerUsers?: string
+    ownerGroups?: string
+    assetStatusUpdatedAt?: number
+    columnCount?: number
+    dataType?: string
+    description?: string
+    meanings: { displayText: string }[]
+    name: string
+    qualifiedName: string
+    userDescription?: string
+    rowCount?: number
+    __createdBy: string
+    __customAttributes: string
+    __guid: string
+    __modificationTimestamp: number
+    __timestamp: number
+    __modifiedBy: string
+    __state: string
+}
+
+
+export type Folder = Omit<Components.Schemas.AtlasEntityHeader, 'attributes' | 'typeName'> & {
+    typeName: 'Folder';
+    attributes: Attributes
+}
+
+export type SavedQuery = Omit<Components.Schemas.AtlasEntityHeader, 'attributes' | 'typeName'> & {
+    typeName: 'Query';
+    attributes: Attributes
 }

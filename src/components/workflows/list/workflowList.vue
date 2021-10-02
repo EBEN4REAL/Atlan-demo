abc70c9dc812b3aba8eca8f5ad0fcec409c2cfac
nobal <nobal@atlan.com>
2021-10-02 17:27:25 +0530

    Setup workflow base layout

---
+++ src/components/workflows/list/workflowList.vue
@@ -0,0 +1,160 @@
+<template>
+    <VirtualList
+        :class="{ 'animate-pulse': isLoading }"
+        :data="list"
+        data-key="guid"
+        variable-height
+    >
+        <template #default="{ item }">
+            <ListItem
+                :item="item"
+                :is-selected="item.guid === selectedAssetId"
+                :score="score[item.guid]"
+                :projection="projection"
+                @click="handlePreview(item)"
+            ></ListItem>
+        </template>
+        <template #footer>
+            <div
+                v-if="isLoadMore || isLoading"
+                class="flex items-center justify-center"
+            >
+                <button
+                    :disabled="isLoading"
+                    class="
+                        flex
+                        items-center
+                        justify-between
+                        py-2
+                        transition-all
+                        duration-300
+                        bg-white
+                        rounded-full
+                        text-primary
+                    "
+                    :class="isLoading ? 'px-2 w-9' : 'px-5 w-32'"
+                    @click="$emit('loadMore')"
+                >
+                    <template v-if="!isLoading">
+                        <p
+                            class="
+                                m-0
+                                mr-1
+                                overflow-hidden
+                                text-sm
+                                transition-all
+                                duration-300
+                                overflow-ellipsis
+                                whitespace-nowrap
+                            "
+                        >
+                            Load more
+                        </p>
+                        <AtlanIcon icon="ArrowDown" />
+                    </template>
+                    <svg
+                        v-else
+                        class="w-5 h-5 text-primary animate-spin"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                    >
+                        <circle
+                            class="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="currentColor"
+                            stroke-width="4"
+                        ></circle>
+                        <path
+                            class="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                        ></path>
+                    </svg>
+                </button>
+            </div>
+        </template>
+    </VirtualList>
+</template>
+
+<script lang="ts">
+    import { defineComponent, SetupContext, ref, toRefs, watch } from 'vue'
+    import WorkflowItem from './workflowItem.vue'
+    import VirtualList from '~/utils/library/virtualList/virtualList.vue'
+
+    export default defineComponent({
+        name: 'AssetList',
+        components: {
+            ListItem,
+            VirtualList,
+        },
+        props: {
+            list: {
+                type: Array,
+                required: false,
+                default() {
+                    return [
+                        
+                    ]
+                },
+            },
+            score: {
+                type: Object,
+                required: false,
+                default() {
+                    return {}
+                },
+            },
+            projection: {
+                type: Array,
+                required: false,
+                default() {
+                    return []
+                },
+            },
+            isLoading: {
+                type: Boolean,
+                required: true,
+                default: () => false,
+            },
+            isLoadMore: {
+                type: Boolean,
+                required: true,
+                default: () => false,
+            },
+            autoSelect: {
+                type: Boolean,
+                required: false,
+                default: () => false,
+            },
+            typename: {
+                type: String,
+            },
+        },
+        emits: ['preview', 'loadMore', 'update:autoSelect'],
+        setup(props, { emit }) {
+            const { list, autoSelect, typename } = toRefs(props)
+            const selectedAssetId = ref('')
+            let shouldReSelect = false
+            function handlePreview(item: any) {
+                selectedAssetId.value = item.guid
+                emit('preview', item)
+            }
+
+            // select first asset automatically conditionally acc to  autoSelect prop
+
+            watch(
+                list,
+                () => {
+                    if (autoSelect.value) {
+                        if (list.value.length) handlePreview(list.value[0])
+                    } else emit('update:autoSelect', true)
+                },
+                { immediate: true }
+            )
+
+            return { handlePreview, selectedAssetId, list }
+        },
+    })
+</script>

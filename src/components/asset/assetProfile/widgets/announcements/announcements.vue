10c1aa94c4434d8d31f68f62b4505e63f005201a
Shivansh Pahwa <shivanshpahwa24@gmail.com>
2021-10-05 04:05:22 +0530

    loading state, margin, paddings added

--- src/components/asset/assetProfile/widgets/announcements/announcements.vue
+++ src/components/asset/assetProfile/widgets/announcements/announcements.vue
@@ -1,7 +1,13 @@
 <template>
-    <div class="flex justify-between w-full">
+    <div
+        v-if="isLoading"
+        class="flex items-center justify-center text-sm leading-none"
+    >
+        <a-spin size="small"></a-spin>
+    </div>
+    <div v-else class="flex justify-between w-full">
         <div class="flex-grow">
-            <div v-if="editable">
+            <div v-if="editable" class="p-2 shadow">
                 <a-dropdown
                     placement="bottomLeft"
                     :trigger="['click']"
@@ -27,24 +33,32 @@
                     </template>
 
                     <div
-                        class="flex items-center flex-none px-2 py-1 align-middle rounded cursor-pointer  bg-primary-light"
+                        class="flex items-center px-2 py-1 align-middle rounded cursor-pointer  max-w-min"
+                        :class="
+                            bannerType === 'information'
+                                ? 'information-bg'
+                                : bannerType === 'issue'
+                                ? 'issue-bg'
+                                : bannerType === 'warning'
+                                ? 'warning-bg'
+                                : ''
+                        "
                     >
-                        <span class="svg-icon">
-                            <component
-                                :is="announcementObject?.icon"
-                                class="w-auto h-4"
-                            />
-                        </span>
+                        <component
+                            :is="announcementObject?.icon"
+                            class="w-auto h-4"
+                        />
 
-                        <p class="mb-0 ml-2">{{ announcementObject?.label }}</p>
+                        <span class="mb-0 ml-2">
+                            {{ announcementObject?.label }}
+                        </span>
                     </div>
                 </a-dropdown>
                 <a-input
-                    ref="titleBar"
+                    :ref="titleBar"
                     v-model:value="announcementHeader"
                     placeholder="Add Announcement Header..."
                     class="text-lg font-bold text-gray-700 border-0 shadow-none outline-none "
-                    :class="$style.titleInput"
                 />
                 <a-textarea
                     v-model:value="announcementDescription"
@@ -57,41 +71,104 @@
             <div v-else>
                 <div v-if="!bannerMessage || bannerMessage === ''">
                     <div
-                        class="flex items-center flex-none px-2 py-1 align-middle rounded cursor-pointer  bg-primary-light"
+                        class="flex items-center px-2 py-1 align-middle rounded cursor-pointer  max-w-min"
+                        :class="
+                            bannerType === 'information'
+                                ? 'information-bg'
+                                : bannerType === 'issue'
+                                ? 'issue-bg'
+                                : bannerType === 'warning'
+                                ? 'warning-bg'
+                                : ''
+                        "
                     >
-                        <span class="svg-icon">
-                            <component
-                                :is="announcementObject?.icon"
-                                class="w-auto h-4"
-                            />
-                        </span>
-
-                        <p class="mb-0 ml-2">{{ announcementObject?.label }}</p>
-                    </div>
-
-                    <div
-                        class="text-lg font-bold text-gray-500 border-0 shadow-none outline-none "
-                    >
-                        Add Announcement Header...
-                    </div>
-                    <div
-                        class="text-gray-500 border-0 shadow-none outline-none"
-                    >
-                        Add description...
-                    </div>
-                </div>
-                <div v-else class="flex items-center">
-                    <div>
                         <component
                             :is="announcementObject?.icon"
                             class="w-auto h-4"
                         />
+
+                        <span class="mb-0 ml-2">
+                            {{ announcementObject?.label }}
+                        </span>
+                    </div>
+
+                    <div
+                        class="px-3 py-1 text-lg font-bold text-gray-500 border-0 shadow-none outline-none "
+                    >
+                        Add Announcement Header...
+                    </div>
+                    <div
+                        class="px-3 py-1 text-gray-500 border-0 shadow-none outline-none "
+                    >
+                        Add description...
+                    </div>
+                </div>
+                <div
+                    v-else
+                    class="flex p-4 border rounded"
+                    :class="
+                        bannerType === 'information'
+                            ? 'information-bg information-border'
+                            : bannerType === 'issue'
+                            ? 'issue-bg issue-border'
+                            : bannerType === 'warning'
+                            ? 'warning-bg warning-border'
+                            : ''
+                    "
+                >
+                    <div>
+                        <component
+                            :is="announcementObject?.icon"
+                            class="w-auto h-4 mt-1 mr-4"
+                        />
                     </div>
                     <div>
-                        <span class="text-lg font-bold text-gray-700"
-                            >Announcement Header</span
-                        >
-                        <span class="text-gray-500">{{ bannerMessage }}</span>
+                        <div class="mb-1 text-lg font-bold text-gray-700">
+                            Announcement Header
+                        </div>
+                        <div class="mb-2 text-gray-500">
+                            {{ bannerMessage }}
+                        </div>
+                        <div class="flex items-center">
+                            <a-popover>
+                                <template #content>
+                                    <OwnerInfoCard
+                                        :user="
+                                            asset?.attributes?.bannerUpdatedBy
+                                        "
+                                    />
+                                </template>
+                                <div
+                                    class="
+                                        flex
+                                        items-center
+                                        mr-2.5
+                                        cursor-pointer
+                                    "
+                                >
+                                    <avatar
+                                        class="mr-2"
+                                        :image-url="
+                                            KeyMaps.auth.avatar.GET_AVATAR({
+                                                username:
+                                                    asset?.attributes
+                                                        ?.bannerUpdatedBy,
+                                            })
+                                        "
+                                        :allow-upload="false"
+                                        :avatar-name="
+                                            asset?.attributes?.bannerUpdatedBy
+                                        "
+                                        avatar-size="small"
+                                        :avatar-shape="'circle'"
+                                    />
+                                    {{ asset?.attributes?.bannerUpdatedBy }}
+                                </div></a-popover
+                            >
+                            <span class="text-xs text-gray-500">{{
+                                timeAgo(asset?.attributes?.bannerUpdatedAt)
+                            }}</span>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -119,12 +196,26 @@
 </template>
 
 <script lang="ts">
-    import { defineComponent, ref, PropType, Ref, computed, toRefs } from 'vue'
+    import { useTimeAgo } from '@vueuse/core'
+    import {
+        defineComponent,
+        ref,
+        PropType,
+        Ref,
+        computed,
+        toRefs,
+        nextTick,
+    } from 'vue'
     import { assetInterface } from '~/types/assets/asset.interface'
     import AnnouncementList from '~/constant/announcement'
     import addAnnouncement from '~/composables/asset/addAnnouncement'
+    import Pill from '~/components/UI/pill/pill.vue'
+    import { KeyMaps } from '~/api/keyMap'
+    import Avatar from '~/components/common/avatar.vue'
+    import OwnerInfoCard from '~/components/discovery/preview/hovercards/ownerInfo.vue'
 
     export default defineComponent({
+        components: { Pill, Avatar, OwnerInfoCard },
         props: {
             asset: {
                 type: Object as PropType<assetInterface>,
@@ -180,6 +271,8 @@
                 announcementType.value = announcement.id
             }
 
+            const timeAgo = (time: string | number) => useTimeAgo(time).value
+
             return {
                 editable,
                 AnnouncementList,
@@ -195,24 +288,35 @@
                 handleTextAreaUpdate,
                 bannerMessage,
                 bannerType,
+                timeAgo,
+                KeyMaps,
+                isCompleted,
+                titleBar,
             }
         },
     })
 </script>
 
-<style lang="less" module>
-    .input {
-        :global(.ant-input:focus
-                .ant-input:hover
-                .ant-input::selection
-                .focus-visible) {
-            @apply shadow-none outline-none border-0 border-transparent border-r-0 bg-blue-600 !important;
-        }
-        :global(.ant-input) {
-            @apply shadow-none outline-none px-0 border-0 !important;
-        }
+<style lang="less" scoped>
+    .information-bg {
+        @apply bg-primary-light;
+    }
+    .issue-bg {
+        background-color: rgba(249, 220, 210, 1);
+    }
+    .warning-bg {
+        background-color: rgba(255, 239, 208, 1);
+    }
+    .information-border {
+        @apply border-primary;
+    }
+    .issue-border {
+        border-color: rgba(207, 89, 46, 1);
+    }
+    .warning-border {
+        border-color: rgba(255, 177, 25, 1);
     }
     :global(.ant-input::-webkit-input-placeholder) {
-        @apply font-bold text-gray-500 !important;
+        @apply text-gray-500 !important;
     }
 </style>

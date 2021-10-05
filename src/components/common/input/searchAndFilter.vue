ad7bf79f8a50f73a973d6a64c97296c0ce286fa0
Sarbik Betal <sarbikb55@gmail.com>
2021-10-06 03:55:30 +0530

    Added new searchbar design

--- src/components/common/input/searchAndFilter.vue
+++ src/components/common/input/searchAndFilter.vue
@@ -1,43 +1,55 @@
 <template>
-    <div class="flex items-center justify-between">
-        <a-input
+    <div
+        class="flex items-center justify-between py-1 transition duration-300 border border-gray-300 rounded shadow  hover:shadow-md focus-within:ring-2 focus-within:border-primary focus-within:shadow-none"
+    >
+        <AtlanIcon icon="Search" class="pl-2 pr-1 text-gray-500" />
+        <input
             ref="searchBar"
-            :value="value"
             :placeholder="placeholder"
-            :size="size"
-            :class="$style.searchbar"
-            @change="$emit('change', $event)"
-            @update:value="$emit('update:value', $event)"
-            :allowClear="true"
-        >
-            <template #prefix>
-                <AtlanIcon icon="Search" />
-            </template>
-            <template #suffix>
-                <a-popover
-                    v-if="$slots.filter"
-                    trigger="click"
-                    placement="bottomRight"
-                >
-                    <template #content>
-                        <slot name="filter" />
-                    </template>
+            v-model="value"
+            type="text"
+            class="flex-grow text-sm focus:outline-none"
+            @keyup.esc="$event.target.blur()"
+        />
+        <div class="flex-none h-7 w-7">
+            <button v-if="value?.length" class="text-gray-500 hover:text-gray">
+                <AtlanIcon
+                    icon="Cancel"
+                    class="h-3 m-2"
+                    @click="clearInput"
+                    @keyup.enter="clearInput"
+                />
+            </button>
+        </div>
 
-                    <div class="px-3 py-1 border rounded">
-                        <AtlanIcon
-                            :icon="dot ? 'FilterDot' : 'Filter'"
-                            class="w-4 h-4"
-                        />
-                        <slot name="buttonAggregation" />
-                    </div>
-                </a-popover>
+        <a-popover v-if="$slots.filter" trigger="click" placement="bottomRight">
+            <template #content>
+                <slot name="filter" />
             </template>
-        </a-input>
+
+            <button
+                class="p-1 mr-2 transition-colors rounded hover:bg-gray-light"
+            >
+                <AtlanIcon
+                    :icon="dot ? 'FilterDot' : 'Filter'"
+                    class="w-4 h-4"
+                />
+                <slot name="buttonAggregation" />
+            </button>
+        </a-popover>
     </div>
 </template>
 
 <script lang="ts">
-    import { defineComponent, nextTick, onMounted, Ref, ref, toRefs } from 'vue'
+    import {
+        computed,
+        defineComponent,
+        nextTick,
+        onMounted,
+        Ref,
+        ref,
+        toRefs,
+    } from 'vue'
 
     export default defineComponent({
         name: 'SearchAndFilter',
@@ -49,47 +61,34 @@
             value: { type: String },
         },
         emits: ['update:value', 'change'],
-        setup(props) {
-            const { autofocus } = toRefs(props)
+        setup(props, { emit }) {
+            const { autofocus, value: val } = toRefs(props)
             const searchBar: Ref<null | HTMLInputElement> = ref(null)
+            const value = computed({
+                get: () => val.value,
+                set: (newVal) => {
+                    emit('update:value', newVal)
+                    emit('change', newVal)
+                },
+            })
+
+            function clearInput() {
+                emit('update:value', '')
+                emit('change', '')
+            }
+
             onMounted(async () => {
                 if (autofocus.value) {
                     await nextTick()
                     searchBar.value?.focus()
                 }
             })
+
             return {
+                value,
                 searchBar,
+                clearInput,
             }
         },
     })
 </script>
-
-<style lang="less" module>
-    .searchbar {
-        @apply bg-transparent border-t-0 border-l-0 border-r-0  border-b border-gray-300 rounded-sm !important;
-
-        @apply outline-none;
-        transition: border 500ms ease-out;
-
-        :global(.ant-input) {
-            @apply bg-transparent !important;
-        }
-
-        &:global(.ant-input-affix-wrapper-focused) {
-            @apply border-primary border-b border-t-0 border-l-0 border-r-0 !important;
-            box-shadow: none !important;
-        }
-
-        &:hover {
-            @apply border-primary border-b border-t-0 border-l-0 border-r-0 !important;
-        }
-        &:focus {
-            @apply border-primary border-b border-t-0 border-l-0 border-r-0 !important;
-            box-shadow: none !important;
-        }
-        ::placeholder {
-            @apply text-gray-500 opacity-80;
-        }
-    }
-</style>

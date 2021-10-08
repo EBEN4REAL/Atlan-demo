030d79be61f58c4573aceb7fa8d927f465e659c8
Shubhankar Khare <shubhankar@ATMAC050.local>
2021-10-08 12:17:14 +0530

    feat: popular gtc & recently viewed gtc

---
+++ src/components/glossary/utils/getEntityStatusIcon.ts
@@ -0,0 +1,13 @@
+const getEntityStatusIcon = (type: String, assetStatus: String): String => {
+    if (
+        assetStatus === undefined ||
+        assetStatus === '' ||
+        assetStatus === 'is_null'
+    )
+        return `${type?.charAt(0).toUpperCase()}${type?.slice(1)}`
+
+    return `${type?.charAt(0).toUpperCase()}${type?.slice(1)}${assetStatus
+        ?.charAt(0)
+        .toUpperCase()}${assetStatus?.slice(1)}`
+}
+export default getEntityStatusIcon
--- src/pages/glossary/index.vue
+++ src/pages/glossary/index.vue
@@ -1,17 +1,23 @@
 <template>
     <div class="flex flex-col items-center w-full pt-5 mb-5">
         <SearchWidget />
+        <PopularTerms />
+        <RecentlyViewed />
     </div>
 </template>
 <script lang="ts">
     import { defineComponent, ref } from 'vue'
     // components
     import SearchWidget from '@/glossary/home/searchWidget.vue'
+    import PopularTerms from '@/glossary/home/popularTerms.vue'
+    import RecentlyViewed from '@/glossary/home/recentlyViewedGtc.vue'
     // composables
 
     export default defineComponent({
         components: {
             SearchWidget,
+            PopularTerms,
+            RecentlyViewed,
         },
         setup() {},
     })

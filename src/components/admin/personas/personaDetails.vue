68b0ba9b997c8df795c1c36e54ca46de07bfd6df
Sarbik Betal <sarbikb55@gmail.com>
2021-10-11 03:30:03 +0530

    Hardcoded available scopes and binded scope UI with API response
    (cherry picked from commit fa46252639ec1d7333e523ad81b2ffe267de0a31)

--- src/components/admin/personas/personaDetails.vue
+++ src/components/admin/personas/personaDetails.vue
@@ -1,26 +1,69 @@
 <template>
     <template v-if="selectedPersona">
         <MinimalTab v-model:active="activeTabKey" :data="tabConfig" />
-        <span class="px-5 pt-3 text-xl text-gray">Scopes</span>
-        <div class="px-5 py-2 overflow-y-auto">
-            <div class="mb-3" v-for="scope in scopeList">
-                <span class="block mb-1 text-base text-gray-500 capitalize">{{
-                    scope.type
-                }}</span>
-                <a-checkbox-group :name="scope.type" :options="scope.scopes" />
-            </div>
+        <div class="px-5 pt-4 pb-2 overflow-y-auto">
+            <a-form
+                :key="selectedPersona.id"
+                layout="vertical"
+                :wrapper-col="{ span: 12 }"
+                :model="selectedPersona"
+            >
+                <a-form-item label="Name" name="displayName" required>
+                    <a-input
+                        v-model:value="selectedPersona.displayName"
+                        placeholder="Persona Name"
+                    />
+                </a-form-item>
+                <a-form-item label="Description" name="description">
+                    <a-textarea
+                        v-model:value="selectedPersona.description"
+                        showCount
+                        :maxlength="140"
+                        :auto-size="{ minRows: 1, maxRows: 3 }"
+                    />
+                </a-form-item>
+                <a-form-item label="Admins" name="admins">
+                    {{ selectedPersona.admins }}
+                </a-form-item>
+                <a-form-item label="Created On" name="createdAt">
+                    {{ selectedPersona.createdAt }}
+                </a-form-item>
+                <a-divider />
+
+                <a-form-item label="Scopes">
+                    <div class="mb-3" v-for="scope in scopeList">
+                        <span
+                            class="block mb-1 text-sm text-gray-500 capitalize"
+                            >{{ scope.type }}</span
+                        >
+                        <a-checkbox-group
+                            :value="
+                                selectedPersona.metadataPolicies[0]?.actions
+                            "
+                            :name="scope.type"
+                            :options="scope.scopes"
+                        />
+                    </div>
+                </a-form-item>
+            </a-form>
+            <a-divider />
         </div>
     </template>
     <div v-else class="flex items-center justify-center h-full">
-        <a-spin v-if="scopeListLoading" />
-        <span v-else class="mx-auto"
-            >Add a new persona or select one to edit it</span
-        >
+        <span class="mx-auto">Add a new persona or select one to edit it</span>
     </div>
 </template>
 
 <script lang="ts">
-    import { computed, defineComponent, PropType, ref } from 'vue'
+    import {
+        computed,
+        defineComponent,
+        PropType,
+        Ref,
+        ref,
+        toRefs,
+        watch,
+    } from 'vue'
     import useScopeService from '~/services/heracles/composables/scopes'
     import SearchAndFilter from '@/common/input/searchAndFilter.vue'
     import MinimalTab from '@/UI/minimalTab.vue'
@@ -35,9 +78,25 @@
                 required: false,
             },
         },
-        setup() {
-            const { scopeList, isLoading: scopeListLoading } =
-                useScopeService().listScopes()
+        emits: ['update:persona'],
+        setup(props, { emit }) {
+            // Persona related stuff
+            const { persona } = toRefs(props)
+
+            const selectedPersona: Ref<IPersona | undefined> = ref(undefined)
+
+            watch(
+                persona,
+                () => {
+                    selectedPersona.value = { ...persona.value }
+                },
+                { immediate: true }
+            )
+
+            // const selectedPersona = computed({
+            //     get: () => localPerson.value,
+            //     set: (val) => emit('update:persona', val),
+            // })
 
             const activeTabKey = ref('ov')
             const tabConfig = [
@@ -45,9 +104,10 @@
                 { key: 'ug', label: 'Users & Groups' },
             ]
 
+            const { scopeList } = useScopeService().listScopes()
+
             return {
                 scopeList,
-                scopeListLoading,
                 activeTabKey,
                 tabConfig,
             }
--- src/types/accessPolicies/personas.ts
+++ src/types/accessPolicies/personas.ts
@@ -1,9 +1,14 @@
+export interface MetadataPolicies {
+    actions: string[]
+    assets: string[]
+}
+
 export interface IPersona {
     id: string
     displayName: string
     description?: string
     personaName: string
-    metadataPolicies: []
+    metadataPolicies: MetadataPolicies[]
     orgPolicies: []
     roles: []
     updatedAt: string

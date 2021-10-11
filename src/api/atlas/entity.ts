327ba385144d16c2b76449a486403fb8b78bb29c
Varun <varun@atlan.com>
2021-10-11 18:19:59 +0530

    Metastore Path Changes

--- src/api/atlas/entity.ts
+++ src/api/atlas/entity.ts
@@ -1,25 +1,29 @@
+import { AxiosRequestConfig } from 'axios'
+import useSWRV, { IConfig } from 'swrv'
+import { reactive, ref, Ref, toRef, toRefs } from 'vue'
+import { Components } from './client'
+import { fetcherPost, getAPIPath, getAxiosClient } from '~/api'
 
-import { AxiosRequestConfig } from "axios";
-import useSWRV, { IConfig, } from "swrv";
-import { reactive, ref, Ref, toRef, toRefs } from "vue";
-import { Components } from "./client";
-import { fetcherPost, getAPIPath, getAxiosClient } from "~/api";
-
-const serviceAlias = "metastore";
-
+const serviceAlias = 'meta'
 
 export const URL = {
-  BulkUpdate: "/entity/bulk"
+    BulkUpdate: '/entity/bulk',
 }
 
-const BulkUpdate = (body?: Components.Schemas.AtlasEntitiesWithExtInfo,
-  options?: AxiosRequestConfig) => {
-  const data = getAxiosClient().post(getAPIPath(serviceAlias, URL.BulkUpdate), body, {
-    ...options,
-  });
-  return data;
+const BulkUpdate = (
+    body?: Components.Schemas.AtlasEntitiesWithExtInfo,
+    options?: AxiosRequestConfig
+) => {
+    const data = getAxiosClient().post(
+        getAPIPath(serviceAlias, URL.BulkUpdate),
+        body,
+        {
+            ...options,
+        }
+    )
+    return data
 }
 
 export const Entity = {
-  BulkUpdate,
-};
+    BulkUpdate,
+}
--- src/api/atlas/search.ts
+++ src/api/atlas/search.ts
@@ -1,45 +1,49 @@
-import { AxiosRequestConfig } from "axios";
-import useSWRV, { IConfig, } from "swrv";
-import { reactive, ref, Ref, toRef, toRefs } from "vue";
-import { Components } from "./client";
-import { fetcherPost, getAPIPath, getAxiosClient } from "~/api";
-import { SearchParameters } from "~/types/atlas/attributes";
+import { AxiosRequestConfig } from 'axios'
+import useSWRV, { IConfig } from 'swrv'
+import { reactive, ref, Ref, toRef, toRefs } from 'vue'
+import { Components } from './client'
+import { fetcherPost, getAPIPath, getAxiosClient } from '~/api'
+import { SearchParameters } from '~/types/atlas/attributes'
 
-const serviceAlias = "metastore";
+const serviceAlias = 'meta'
 
-const Basic = (
-  body?: SearchParameters,
-  options?: AxiosRequestConfig,
-) => getAxiosClient().post(
-    getAPIPath(serviceAlias, "/search/basic"),
-    body,
-    options
-  );
-
+const Basic = (body?: SearchParameters, options?: AxiosRequestConfig) =>
+    getAxiosClient().post(
+        getAPIPath(serviceAlias, '/search/basic'),
+        body,
+        options
+    )
 
 const BasicSearch = (
-  body?: Components.Schemas.SearchParameters,
-  options?: AxiosRequestConfig,
-  config?: IConfig,
-  skip = true
+    body?: Components.Schemas.SearchParameters,
+    options?: AxiosRequestConfig,
+    config?: IConfig,
+    skip = true
 ) => {
-  const { data, error, mutate, isValidating } = useSWRV(skip ? [getAPIPath(serviceAlias, "/search/basic"), body, options, skip] : null, fetcherPost, config);
-  const response = data as Ref<Components.Schemas.AtlasSearchResult>
-  return {
-    response,
-    error,
-    mutate,
-    isValidating
-  }
-};
-
-
-
-export const Search = {
-  Basic,
-  BasicSearch,
-};
-function useSwrvState(data: Ref<any>, error: Ref<any>, isValidating: Ref<boolean>): { state: any; STATES: any; } {
-  throw new Error("Function not implemented.");
+    const { data, error, mutate, isValidating } = useSWRV(
+        skip
+            ? [getAPIPath(serviceAlias, '/search/basic'), body, options, skip]
+            : null,
+        fetcherPost,
+        config
+    )
+    const response = data as Ref<Components.Schemas.AtlasSearchResult>
+    return {
+        response,
+        error,
+        mutate,
+        isValidating,
+    }
 }
 
+export const Search = {
+    Basic,
+    BasicSearch,
+}
+function useSwrvState(
+    data: Ref<any>,
+    error: Ref<any>,
+    isValidating: Ref<boolean>
+): { state: any; STATES: any } {
+    throw new Error('Function not implemented.')
+}

4d17e2d019799fbb6cbddaa37ae2e94486ae3bc7
Shivansh Pahwa <shivanshpahwa24@gmail.com>
2021-10-15 22:40:55 +0530

    made data types uppercase

--- src/components/asset/assetProfile/tabs/overview/nonBiAsset/overviewTable.vue
+++ src/components/asset/assetProfile/tabs/overview/nonBiAsset/overviewTable.vue
@@ -1,56 +1,29 @@
 <template>
-    <div class="sampleData">
-        <!-- Table -->
-        <!--  <div class="relative border rounded border-gray-light h-96">
-            <a-table
-                :columns="tableColumns"
-                :data-source="results"
-                :pagination="false"
-                :scroll="{ x: true, y: 300 }"
-                :loading="isLoading"
-                class="absolute left-0 w-full"
-                size="small"
-            >
-            </a-table>
-             <a-table
-                :data-source="results"
-                :loading="isLoading"
-                :pagination="false"
-                :scroll="{ y: 300 }"
-                class="absolute left-0 w-full"
-                size="small"
-            >
-                <a-table-column
-                    v-for="col in tableColumns"
-                    :key="col.dataIndex"
-                    :data-index="col.dataIndex"
-                >
-                    <template #title>
-                        <div class="flex">
-                            <component
-                                :is="images[col.columnType]"
-                                class="w-4 h-4 mr-3"
-                            ></component>
-                            <span class="truncate">{{ col.columnTitle }}</span>
-                        </div>
-                    </template>
-
-                    <template #customRender="{ text }">
-                        {{ text }}
-                    </template>
-                </a-table-column>
-            </a-table> 
-        </div>-->
-        <table>
+    <div
+        v-if="isLoading"
+        class="flex items-center justify-center mt-4 text-sm leading-none"
+    >
+        <a-spin size="small" class="mr-2 leading-none"></a-spin
+        ><span>Getting sample data</span>
+    </div>
+    <div v-else class="w-full p-0 m-0 border rounded border-gray-light">
+        <table class="relative block w-full p-0 m-0 overflow-auto h-80">
             <thead>
                 <tr>
-                    <th v-for="(col, index) in tableColumns" :key="index">
+                    <th
+                        v-for="(col, index) in tableColumns"
+                        :key="index"
+                        class="sticky top-0 px-4 py-2 text-sm font-normal text-gray-700 bg-gray-100 border  border-gray-light"
+                    >
                         <div class="flex">
                             <component
                                 :is="images[col.data_type]"
                                 class="w-4 h-4 mr-3"
                             ></component>
-                            <Tooltip :tooltip-text="col.title" />
+                            <Tooltip
+                                :tooltip-text="col.title"
+                                classes="cursor-pointer"
+                            />
                         </div>
                     </th>
                 </tr>
@@ -58,8 +31,15 @@
 
             <tbody>
                 <tr v-for="(row, index) in results" :key="index">
-                    <td v-for="(rowData, index) in row" :key="index">
-                        <Tooltip :tooltip-text="rowData" />
+                    <td
+                        v-for="(rowData, index) in row"
+                        :key="index"
+                        class="px-4 py-2 text-xs text-gray-700 bg-white border  border-gray-light"
+                    >
+                        <Tooltip
+                            :tooltip-text="rowData"
+                            classes="cursor-pointer"
+                        />
                     </td>
                 </tr>
             </tbody>
@@ -152,8 +132,6 @@
                 tableColumns,
                 results,
                 isLoading,
-                data,
-                getDataType,
                 images,
             }
         },
@@ -161,54 +139,17 @@
 </script>
 
 <style lang="less" scoped>
-    :global(.ant-table-thead tr th) {
-        @apply font-normal text-gray-700 !important;
-    }
-    :global(.ant-table) {
-        @apply border border-gray-light !important;
-    }
-    :global(.ant-table-tbody tr td) {
-        @apply text-gray-500 !important;
-    }
-    :global(.ant-table-thead) {
-        @apply bg-gray-100 !important;
-    }
-    :global(.ant-table-cell-fix-left) {
-        @apply bg-gray-100 border-r border-gray-light !important;
-    }
-</style>
-
-<style lang="less" scoped>
-    .sampleData {
-        @apply w-full border border-gray-light rounded m-0 p-0;
-    }
     table {
-        @apply w-full block overflow-auto h-80 m-0 p-0;
+        td,
+        th {
+            max-width: 150px;
+            min-width: 150px;
+        }
 
-        thead {
-            @apply sticky top-0;
-            th {
-                @apply font-normal text-gray-700 px-4 py-2 text-sm bg-gray-100 border border-gray-light;
-                width: 150px;
-                min-width: 150px;
-            }
-        }
-        tbody {
-            td {
-                @apply text-gray-700 px-4 py-2 text-xs bg-white border border-gray-light;
-                width: 150px;
-                min-width: 150px;
-            }
-        }
-        td:first-child {
-            @apply sticky left-0 border-r border-gray-light;
+        td:first-child,
+        th:first-child {
             width: 50px;
             min-width: 50px;
         }
-        /*  th:first-child {
-            
-            width: 50px;
-            min-width: 50px;
-        } */
     }
 </style>

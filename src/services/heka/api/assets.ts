2e60eee2e3a4701e0221e66752c65db8eb07cd11
Shivansh Pahwa <shivanshpahwa24@gmail.com>
2021-10-15 22:53:35 +0530

    heka api for sample data added

--- src/services/heka/api/assets.ts
+++ src/services/heka/api/assets.ts
@@ -1,9 +1,16 @@
 import { KeyMaps } from '../heka_keyMaps'
-import { useAPI } from '~/api/useAPI'
+import { useAPIAsyncState } from '~/api/useAPI'
+import { RequestAttributes } from '~/types/atlas/requests'
 
-const GetSampleData = (body: Record<string, any>) => useAPI(KeyMaps.asset.PREVIEW_TABLE(), 'POST', {
-    body,
-})
+const GetSampleData = (body: Record<string, any>) => {
+    const { data, error, mutate, isLoading } = useAPIAsyncState<{
+        records: RequestAttributes[]
+    }>(KeyMaps.asset.PREVIEW_TABLE, 'POST', {
+        body,
+    })
+
+    return { data, error, mutate, isLoading }
+}
 
 export const Assets = {
     GetSampleData,

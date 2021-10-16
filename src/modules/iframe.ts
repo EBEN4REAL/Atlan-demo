d9f85306ac37f0a89d15be723358bf45a1b9ec0b
Varun <varun@atlan.com>
2021-10-17 02:30:19 +0530

    Delete iframe.ts

--- src/modules/iframe.ts
+++
@@ -1,7 +0,0 @@
-import VueFriendlyIframe from 'vue-friendly-iframe'
-
-import { UserModule } from '~/types/vitessg'
-
-export const install: UserModule = ({ app }) => {
-    app.use(VueFriendlyIframe)
-}

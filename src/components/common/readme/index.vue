57e80d0f6d9d64c282385760b9b75eb64bd7b6a1
Aayush Dongre <aayushdongre4@gmail.com>
2021-10-19 03:34:07 +0530

    fix: readme saving fix

--- src/components/common/readme/index.vue
+++ src/components/common/readme/index.vue
@@ -191,14 +191,14 @@
         setup(props) {
             const editable = ref(false)
             const editor = ref()
-            const editorContent = ref('')
+            const editorContent = ref(entity?.attributes?.readme?.attributes?.description ?? '')
             const showTemplatesModal = ref(false)
             const templateName = ref('')
             const newTemplateName = ref('')
             const templateNameDropdown = ref(false)
             const { entity } = toRefs(props)
             const readmeDescription = computed(
-                () => props.entity?.attributes?.readme?.attributes?.description
+                () => entity?.attributes?.readme?.attributes?.description
             )
 
             const templateList = ref([
@@ -216,13 +216,14 @@
 
             const onUpdate = (content: string, json: Record<string, any>) => {
                 editorContent.value = content
+                console.log(content, editorContent.value)
             }
 
             const onCancel = () => {
                 if (editor.value) {
                     editor.value.resetEditor()
                 }
-                editorContent.value = editor.value.getEditorContent()?.content
+                editorContent.value = readmeDescription.value
 
                 editable.value = false
             }
@@ -250,7 +251,7 @@
                 // if (!editorContent.value || editorContent.value === '<p></p>') {
                 //     showTemplatesModal.value = true
                 // }
-                editorContent.value = readmeDescription.value
+                // editorContent.value = readmeDescription.value
             }
 
             // const newTemplate = () => {
@@ -262,12 +263,18 @@
             }
             const handleSave = () => {
                 editable.value = false
-                if (readmeDescription.value?.length) {
+                console.log(editorContent.value)
+                if (readmeDescription.value?.length || readmeDescription.value === '') {
                     const { isCompleted, isLoading, update } = useUpdateReadme(
-                        props?.entity?.attributes?.readme,
+                        entity?.attributes?.readme,
                         editorContent.value
                     )
                     update()
+                    watch(isCompleted, (completed) => {
+                        if(completed) {
+                            entity.attributes.readme.attributes.description = editorContent.value
+                        }
+                    })
                 } else {
                     const { createReadme } = useCreateReadme(
                         entity,

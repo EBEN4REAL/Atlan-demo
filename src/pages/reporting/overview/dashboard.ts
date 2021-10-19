a42e56951c853c2c26a2199d0df4f7808ac819da
Varun <varun@atlan.com>
2021-10-19 21:38:02 +0530

    Dashboard Widgets
    - Classifications
    - Terms

--- src/pages/reporting/overview/dashboard.ts
+++ src/pages/reporting/overview/dashboard.ts
@@ -494,7 +494,7 @@ export const Metadata = {
         },
         {
             id: 'created-assets',
-            label: 'Top 5 classifications',
+            label: 'Top 5 Creators',
             info: 'Asset Types',
             showHeader: true,
             class: 'col-span-6 border border-light px-5 py-3 rounded h-64',
@@ -507,6 +507,164 @@ export const Metadata = {
                             terms: {
                                 field: '__createdBy',
                                 size: 5,
+                            },
+                        },
+                    },
+                },
+                graphType: 'bar',
+                graphOptions: {
+                    indexAxis: 'y',
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    plugins: {
+                        legend: {
+                            display: false,
+                        },
+                        datalabels: {
+                            backgroundColor: '#fff',
+
+                            borderColor: '#f4f6fd',
+                            borderRadius: 25,
+                            borderWidth: 0.5,
+                            color: '#5277d7',
+                            // backgroundColor: function (context) {
+                            //     return context.dataset.backgroundColor
+                            // },
+
+                            clamp: true,
+                            padding: {
+                                top: 0,
+                                bottom: 0,
+                            },
+
+                            formatter: Math.round,
+                            offset: 20,
+
+                            anchor: 'center',
+                        },
+                        title: {
+                            display: false,
+                        },
+                    },
+                    scales: {
+                        x: {
+                            grid: {
+                                display: false,
+                                drawBorder: false,
+                            },
+                            ticks: {
+                                display: false,
+                            },
+                        },
+                        y: {
+                            grid: {
+                                display: false,
+                            },
+                            ticks: {
+                                display: true,
+                            },
+                        },
+                    },
+                },
+                dataOptions: {
+                    aggregationKey: 'group_by_traits',
+                },
+            },
+        },
+        {
+            id: 'terms-assets',
+            label: 'Top 5 terms',
+            info: 'Asset Types',
+            showHeader: true,
+            class: 'col-span-6 border border-light px-5 py-3 rounded h-64',
+            component: 'graph',
+            componentData: {
+                query: {
+                    size: 0,
+                    aggs: {
+                        group_by_traits: {
+                            terms: {
+                                field: '__meanings',
+                                size: 20,
+                            },
+                        },
+                    },
+                },
+                graphType: 'bar',
+                graphOptions: {
+                    indexAxis: 'y',
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    plugins: {
+                        legend: {
+                            display: false,
+                        },
+                        datalabels: {
+                            backgroundColor: '#fff',
+
+                            borderColor: '#f4f6fd',
+                            borderRadius: 25,
+                            borderWidth: 0.5,
+                            color: '#5277d7',
+                            // backgroundColor: function (context) {
+                            //     return context.dataset.backgroundColor
+                            // },
+
+                            clamp: true,
+                            padding: {
+                                top: 0,
+                                bottom: 0,
+                            },
+
+                            formatter: Math.round,
+                            offset: 20,
+
+                            anchor: 'center',
+                        },
+                        title: {
+                            display: false,
+                        },
+                    },
+                    scales: {
+                        x: {
+                            grid: {
+                                display: false,
+                                drawBorder: false,
+                            },
+                            ticks: {
+                                display: false,
+                            },
+                        },
+                        y: {
+                            grid: {
+                                display: false,
+                            },
+                            ticks: {
+                                display: true,
+                            },
+                        },
+                    },
+                },
+                dataOptions: {
+                    aggregationKey: 'group_by_traits',
+                },
+            },
+        },
+        {
+            id: 'certificate-assets',
+            label: 'Top 5 terms',
+            info: 'Asset Types',
+            showHeader: true,
+            class: 'col-span-6 border border-light px-5 py-3 rounded h-64',
+            component: 'graph',
+            componentData: {
+                query: {
+                    size: 0,
+                    aggs: {
+                        group_by_traits: {
+                            terms: {
+                                field: 'Asset.certificateStatus',
+                                size: 20,
                             },
                         },
                     },

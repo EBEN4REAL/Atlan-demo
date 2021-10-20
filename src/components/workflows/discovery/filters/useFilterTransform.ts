c9ae535a57d4fcb849313ae1c14e045bc811e260
Izuorah Dubem kizito <dubemizuus@gmail.com>
2021-10-20 15:05:32 +0100

    added workflows source sample filter to new workflows page

--- src/components/workflows/discovery/filters/useFilterTransform.ts
+++ src/components/workflows/discovery/filters/useFilterTransform.ts
@@ -1,9 +1,13 @@
 export const transformFacets = (facetsFilters) => {
   const output = []
-  const { owners } = facetsFilters
+  const { owners, workflowType } = facetsFilters
   // further filters would be transformed, labelled and pushed in here
   if (owners?.userValue?.length > 0) {
     const temp = owners.userValue.map(x => ({ labels: { $elemMatch: { "workflows.argoproj.io/creator": x } } }))
+    output.push(...temp)
+  }
+  if (workflowType?.checked?.length > 0) {
+    const temp = workflowType.checked.map(x => ({ labels: { $elemMatch: { "com.atlan.orchestration/source": x } } }))
     output.push(...temp)
   }
   return output

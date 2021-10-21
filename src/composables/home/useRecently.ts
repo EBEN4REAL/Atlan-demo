407d614dba824603ab0b8617f04417d0adfe39f7
AtlanKautzar <kautzar@atlan.com>
2021-10-21 16:12:52 +0700

    recently term

--- src/composables/home/useRecently.ts
+++ src/composables/home/useRecently.ts
@@ -4,20 +4,30 @@ import useIndexSearch from '~/services/atlas/discovery/useIndexSearch'
 
 
 export default function useRecentlyData() {
-    const query = bodybuilder()
-      .aggregation("term", "__typeName.keyword", 
-        {
-          size: 10
-        })
-        .size(10)
-      .build()
+    // const query = bodybuilder().sort("__timestamp", {sort: "asc"}).build()
+    const query = bodybuilder().filter("term", "__typeName.keyword", "AtlasGlossaryTerm").sort("__timestamp").build()
+    // const query = bodybuilder().sort("__timestamp", {sort: "asc"}).size(20).build()
     const { replaceBody, body, isReady, error, data } = useIndexSearch(
       {dsl: query},
       '',
       true
     )
     onMounted(() => {
-      replaceBody({dsl: { query }})
+      replaceBody({dsl: query, "attributes":["_modificationTimestamp","_timestamp"]})
+      // replaceBody({dsl: {
+      //   size: 2,
+      //   query: {
+      //     bool: {
+      //       filter: [
+      //         {
+      //           terms: {
+      //             "__typeName.keyword": ["Table"]
+      //           }
+      //         }
+      //       ]
+      //     }
+      //   }
+      // } })
       console.log('RUNNNNNNNNN')
     })
 

38d093a62664dff80ffad3e1079c8dcdc4194666
AtlanKautzar <kautzar@atlan.com>
2021-10-21 18:04:18 +0700

    fix: filter range timestamp

--- src/composables/home/useRecently.ts
+++ src/composables/home/useRecently.ts
@@ -4,16 +4,28 @@ import useIndexSearch from '~/services/atlas/discovery/useIndexSearch'
 
 
 export default function useRecentlyData() {
-    const query = bodybuilder().filter("term", "__typeName.keyword", "AtlasGlossaryTerm").sort("__timestamp").build()
+    const currentDate = new Date();
+    const sevenDaysAgo = currentDate.getDate() - 7;
+    currentDate.setDate(sevenDaysAgo);
+    const lastSevenDaysTimestamp = new Date(currentDate).getTime()
+    const currentTimestamp = new Date().getTime()
+
+    const query = bodybuilder().orFilter(
+      'bool', 
+      (b) => b.filter("term", "__typeName.keyword", "AtlasGlossaryTerm")
+      .filter("range", "__timestamp", {
+        gte: lastSevenDaysTimestamp,
+        lt: currentTimestamp
+      }))
+      .sort("__timestamp").build()
+
     const { replaceBody, body, isReady, error, data } = useIndexSearch(
       {dsl: query},
       '',
       true
     )
     onMounted(() => {
-      replaceBody({dsl: query, "attributes":["_modificationTimestamp","_timestamp"]})
-      
-      console.log('RUNNNNNNNNN')
+      replaceBody({dsl: query, "attributes":["_modificationTimestamp", "_timestamp", "__createdBy"]})
     })
 
   return {

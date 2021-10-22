ead3bdc9f1c3849bfaaba6a229784e28d2dccaaa
AtlanKautzar <kautzar@atlan.com>
2021-10-22 08:36:49 +0700

    remove subquery

--- src/composables/home/useHomeDSL.ts
+++ src/composables/home/useHomeDSL.ts
@@ -4,14 +4,12 @@ import useIndexSearch from '~/services/atlas/discovery/useIndexSearch'
 import { getRecentTimestamp } from '~/utils/date'
 
 export function useRecentTerms() {
-    const query = bodybuilder().andFilter(
-      'bool', 
-      (b) => b
+    const query = bodybuilder()
       .filter("term", "__typeName.keyword", "AtlasGlossaryTerm")
       .filter("range", "__timestamp", {
         gte: getRecentTimestamp().lastSevenDaysTimestamp,
         lt: getRecentTimestamp().currentTimestamp
-      }))
+      })
       .sort("__timestamp").build()
 
     const { replaceBody, body, isReady, error, data } = useIndexSearch(

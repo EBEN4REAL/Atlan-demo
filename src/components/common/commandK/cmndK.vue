c95fc6a1176586858a31a06eb58a17895b21cba2
Shubhankar Khare <shubhankar@ATMAC050.local>
2021-10-26 01:38:51 +0530

    feat: initial setup with shortcut keys

---
+++ src/components/common/commandK/cmndK.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <a-modal :visible="isVisible" @cancel="$emit('closeModal')">
+            <template #title> searchBar goes here </template>
+            list goes here
+        </a-modal>
+    </div>
+</template>
+<script lang="ts">
+    import {
+        defineComponent,
+        ref,
+        onMounted,
+        watch,
+        computed,
+        toRefs,
+    } from 'vue'
+    import { useMagicKeys, whenever } from '@vueuse/core'
+    export default defineComponent({
+        name: 'CommandK',
+        components: {},
+        props: {
+            isCmndKVisible: {
+                type: Boolean,
+                required: true,
+                default: false,
+            },
+        },
+        emits: ['closeModal'],
+        setup(props) {
+            const { isCmndKVisible } = toRefs(props)
+            const isVisible = computed(() => isCmndKVisible.value)
+
+            return {
+                isVisible,
+            }
+        },
+    })
+</script>

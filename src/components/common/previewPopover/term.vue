b45a1c3361fbd62064a7f4354bb8647ed4ea5d0b
Aayush Dongre <aayushdongre4@gmail.com>
2021-11-01 15:45:42 +0530

    feat: init term popover

---
+++ src/components/common/previewPopover/term.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script lang="ts">
+    import { computed, defineComponent, PropType, ref, watch, Ref, toRefs } from 'vue'
+
+    import { Term } from '~/types/glossary/glossary.interface'
+
+    export default defineComponent({
+        name: 'TermPopover',
+        components: {
+
+        },
+        props: {
+            term: {
+                type: Object as PropType<Term>,
+                required: false,
+                default: true,
+            },
+        },
+        setup(props, { emit }) {
+           const term = toRefs(props)
+
+            return {
+                term
+            }
+        },
+    })
+</script>
+
+
+<style lang="less" module>
+
+</style>

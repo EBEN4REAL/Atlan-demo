284ebb97735f18789477a9be0c70f4c9d5e648d6
alii13 <shekhaliul44@gmail.com>
2021-11-10 15:19:29 +0530

    feat: minor fixes

--- src/components/insights/common/connector/connector.vue
+++ src/components/insights/common/connector/connector.vue
@@ -14,7 +14,7 @@
             :data-test-id="'conector'"
             @select="selectNode"
         >
-            <template #title="node">
+            <!-- <template #title="node">
                 <div class="flex items-center" v-if="node?.img">
                     <img :src="node.img" class="w-auto h-3 mr-2" />
                     <span class="">{{
@@ -28,7 +28,7 @@
                     />
                     <span class="">{{ node.name }}</span>
                 </div>
-            </template>
+            </template> -->
 
             <template #suffixIcon>
                 <AtlanIcon icon="ChevronDown" class="h-4 -mt-0.5 -ml-0.5" />

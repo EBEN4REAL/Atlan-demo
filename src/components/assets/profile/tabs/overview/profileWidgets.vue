e534337ab8583d75367cdb281fd900739c497258
Shivansh Pahwa <shivanshpahwa24@gmail.com>
2021-11-11 03:30:53 +0530

    sidebar widgets added in table summary

--- src/components/assets/profile/tabs/overview/profileWidgets.vue
+++ src/components/assets/profile/tabs/overview/profileWidgets.vue
@@ -8,34 +8,45 @@
                     : 'summary-grid-2'
             "
         >
+            <SQL
+                v-if="
+                    asset.typeName == 'View' ||
+                    asset.typeName == 'MaterialisedView'
+                "
+                :sql="definition(asset)"
+            >
+                <div class="flex flex-col text-sm cursor-pointer">
+                    <span class="mb-2 text-sm text-gray-500">Definition</span>
+                    <span class="text-primary">SQL</span>
+                </div>
+            </SQL>
             <RowInfoHoverCard
                 v-if="
                     asset.typeName == 'Table' ||
                     asset.typeName == 'TablePartition'
                 "
-                :row-count="rows"
-                :size-bytes="size"
-                :source-updated-at="sourceUpdated"
-                :source-updated-at-raw="sourceUpdatedRaw"
-                :source-created-at="sourceCreated"
-                :source-created-at-raw="sourceCreatedRaw"
+                :image="getConnectorImage(asset)"
+                :row-count="rowCount(asset)"
+                :size-bytes="sizeBytes(asset)"
+                :source-updated-at="sourceUpdatedAt(asset)"
+                :source-updated-at-raw="sourceUpdatedAt(asset, true)"
+                :source-created-at="sourceCreatedAt(asset)"
+                :source-created-at-raw="sourceCreatedAt(asset, true)"
             >
                 <div class="flex flex-col text-sm cursor-pointer">
                     <span class="mb-1 text-sm text-gray-500">Rows</span>
-                    <span class="text-gray-700">{{ rows }}</span>
+                    <span class="text-gray-700">{{ rowCount(asset) }}</span>
                 </div>
             </RowInfoHoverCard>
             <div class="flex flex-col text-sm">
                 <span class="mb-1 text-sm text-gray-500">Columns</span>
-                <span class="text-gray-700">{{ cols }}</span>
+                <span class="text-gray-700">{{ columnCount(asset) }}</span>
             </div>
             <div class="max-w-screen-md">
-                <Description
-                    v-if="asset.guid"
-                    :selected-asset="asset"
-                    :edit-permission="editPermission"
-                    @update:selected-asset="mutateAsset"
-                />
+                <div class="flex flex-col">
+                    <p class="mb-1 text-sm text-gray-500">Description</p>
+                    <Description v-model="localDescription" />
+                </div>
             </div>
             <div
                 :class="
@@ -45,48 +56,49 @@
                         : ''
                 "
             >
-                <Status
-                    v-if="asset.guid"
+                <CertificationPopover
                     :selected-asset="asset"
-                    :edit-permission="editPermission"
-                    @update:selected-asset="mutateAsset"
-                />
+                    placement="bottom"
+                >
+                    <Certificate :selected-asset="asset" />
+                </CertificationPopover>
             </div>
 
-            <Owners
-                v-if="asset.guid"
-                :selected-asset="asset"
-                :edit-permission="editPermission"
-                @update:selected-asset="mutateAsset"
-            />
+            <div v-if="asset.guid" class="flex flex-col">
+                <p class="mb-1 text-sm text-gray-500">Owners</p>
+                <Owners v-model="localOwners" @change="handleOwnersChange" />
+            </div>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-    // Vue
-    import {
-        defineComponent,
-        // watch,
-        // ref,
-        // inject,
-        computed,
-        PropType,
-        toRefs,
-    } from 'vue'
+    import { defineComponent, watch, ref, PropType, toRefs } from 'vue'
 
     // Components
-    import Description from '@common/sidebar/description.vue'
-    import Status from '@common/sidebar/status.vue'
-    import Owners from '@common/sidebar/owners.vue'
-    import RowInfoHoverCard from '@/discovery/preview/hovercards/rowInfo.vue'
+    import SQL from '@/assets/preview/popover/sql.vue'
+    import RowInfoHoverCard from '@/assets/preview/popover/rowInfo.vue'
+    import Description from '@/common/input/description/index.vue'
+    import Owners from '@/common/input/owner/index.vue'
+    import Certificate from '@/common/input/certificate/index.vue'
+    import CertificationPopover from '@/assets/preview/popover/certification.vue'
+
     // Composables
     import useAssetInfo from '~/composables/asset/useAssetInfo'
+    import updateAsset from '~/composables/discovery/updateAsset'
+
     // Types
     import { assetInterface } from '~/types/assets/asset.interface'
 
     export default defineComponent({
-        components: { RowInfoHoverCard, Description, Status, Owners },
+        components: {
+            RowInfoHoverCard,
+            Description,
+            Certificate,
+            Owners,
+            SQL,
+            CertificationPopover,
+        },
 
         props: {
             asset: {
@@ -108,40 +120,62 @@
                 sizeBytes,
                 sourceUpdatedAt,
                 sourceCreatedAt,
+                ownerGroups,
+                ownerUsers,
+                definition,
+                description,
+                getConnectorImage,
             } = useAssetInfo()
 
-            const rows = computed(() =>
-                asset.value ? rowCount(asset.value, true) : '~'
-            )
-            const size = computed(() =>
-                asset.value ? sizeBytes(asset.value, false) : '~'
-            )
-            const cols = computed(() =>
-                asset.value ? columnCount(asset.value, true) : '~'
-            )
-            const sourceUpdated = computed(() =>
-                asset.value ? sourceUpdatedAt(asset.value) : ''
-            )
-            const sourceUpdatedRaw = computed(() =>
-                asset.value ? sourceUpdatedAt(asset.value, true) : ''
-            )
-            const sourceCreated = computed(() =>
-                asset.value ? sourceCreatedAt(asset.value) : ''
-            )
-            const sourceCreatedRaw = computed(() =>
-                asset.value ? sourceCreatedAt(asset.value, true) : ''
-            )
-            const mutateAsset = (updatedAsset: assetInterface) => {}
+            const entity = ref({
+                guid: asset.value.guid,
+                typeName: asset.value.typeName,
+                attributes: {
+                    name: asset.value.attributes?.name,
+                    qualifiedName: asset.value.attributes?.qualifiedName,
+                    tenantId: 'default',
+                },
+            })
+
+            const body = ref({
+                entities: [],
+            })
+
+            const { mutate, isLoading } = updateAsset(body)
+
+            const localDescription = ref(description(asset.value))
+
+            watch(localDescription, () => {
+                entity.value.attributes.userDescription = localDescription.value
+                body.value.entities = [entity.value]
+                mutate()
+            })
+
+            const localOwners = ref({
+                ownerUsers: ownerUsers(asset.value),
+                ownerGroups: ownerGroups(asset.value),
+            })
+
+            const handleOwnersChange = () => {
+                entity.value.attributes.ownerUsers =
+                    localOwners.value?.ownerUsers
+                entity.value.attributes.ownerGroups =
+                    localOwners.value?.ownerGroups
+                body.value.entities = [entity.value]
+                mutate()
+            }
+
             return {
-                asset,
-                rows,
-                cols,
-                sourceUpdated,
-                sourceUpdatedRaw,
-                sourceCreated,
-                sourceCreatedRaw,
-                size,
-                mutateAsset,
+                rowCount,
+                columnCount,
+                sizeBytes,
+                sourceUpdatedAt,
+                sourceCreatedAt,
+                getConnectorImage,
+                definition,
+                localDescription,
+                localOwners,
+                handleOwnersChange,
             }
         },
     })

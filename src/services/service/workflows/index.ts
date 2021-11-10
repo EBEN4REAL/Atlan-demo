8d5c3bdc26aec2aaf6e330a4f3faa9657cb365f9
AtlanKautzar <kautzar@atlan.com>
2021-11-10 20:58:58 +0700

    Auto stash before merge of "feat-refractor-wfs" and "origin/feat-refractor-wfs"

--- src/services/service/workflows/index.ts
+++ src/services/service/workflows/index.ts
@@ -22,174 +22,180 @@ const getWorkflows = ({ immediate, options, params }) =>
         map.WORKFLOW,
         'GET',
         {
+            options,
             params,
         },
         { options, asyncOptions: {immediate} }
     )
 
-// const getWorkflowTemplates = ({ pathVariables, immediate, options, params }) =>
-//     useAPIAsyncState(
-//         KeyMaps.workflow.WORKFLOW_TEMPLATE,
-//         'GET',
-//         {
-//             options,
-//             pathVariables,
-//             params,
-//         },
-//         { immediate }
-//     )
-
-// const getWorkflowConfigMap = ({ pathVariables, immediate, options, params }) =>
-//     useAPIAsyncState(
-//         KeyMaps.workflow.WORKFLOW_CONFIG_MAP,
-//         'GET',
-//         {
-//             pathVariables,
-//             options,
-//             params,
-//         },
-//         { immediate }
-//     )
-
-// const updateWorkflowByName = (name, body, { immediate, options }) =>
-//     useAPIAsyncState(
-//         KeyMaps.workflow.WORKFLOW_UPDATE_BY_NAME,
-//         'POST',
-//         {
-//             options,
-//             body,
-//             pathVariables: {
-//                 name,
-//             },
-//         },
-//         { immediate }
-//     )
-
-// // ! depreciated, use filter on /workflowtemplate
-// const getWorkflowTemplateByName = ({ immediate, options, pathVariables }) =>
-//     useAPIAsyncState(
-//         KeyMaps.workflow.WORKFLOW_TEMPLATE_NAME,
-//         'GET',
-//         {
-//             pathVariables,
-//             options,
-//         },
-//         { immediate }
-//     )
-
-// const retryRun = (pathVariables) =>
-//     useAPIAsyncState(KeyMaps.workflow.RETRY_RUN, 'POST', {
-//         pathVariables,
-//     })
-
-// const stopRun = (pathVariables) =>
-//     useAPIAsyncState(KeyMaps.workflow.STOP_RUN, 'POST', {
-//         pathVariables,
-//     })
-
-// const getRunList = (pathVariables, { options, params }) =>
-//     useAPISWRV(KeyMaps.workflow.WORKFLOW_RUN, 'GET', 'getRunList', {
-//         params,
+// const getWorkflows = ({ immediate, options, params }) =>
+// useAPIAsyncState(
+//     KeyMaps.workflow.WORKFLOW,
+//     'GET',
+//     {
 //         options,
-//         pathVariables,
-//     })
+//         params,
+//     },
+//     { immediate }
+// )
 
-// const getArchivedRunList = (pathVariables, { options, params }) =>
-//     useAPISWRV(
-//         KeyMaps.workflow.ARCHIVED_WORKFLOW_RUN,
-//         'GET',
-//         'getArchivedRunList',
-//         {
-//             params,
-//             options,
-//             pathVariables,
-//         }
-//     )
+const getWorkflowTemplates = ({ pathVariables, immediate, options, params }) =>
+    useAPI(
+        map.WORKFLOW_TEMPLATE,
+        'GET',
+        {
+            options,
+            pathVariables,
+            params,
+        },
+        { immediate }
+    )
 
-// const deleteWorkflowByName = ({ pathVariables, immediate, options }) =>
-//     useAPIAsyncState(
-//         KeyMaps.workflow.DELETE_WORKFLOW,
-//         'POST',
-//         {
-//             options,
-//             pathVariables,
-//         },
-//         { immediate }
-//     )
+const getWorkflowConfigMap = ({ pathVariables, immediate, options, params }) =>
+    useAPI(
+        map.WORKFLOW_CONFIG_MAP,
+        'GET',
+        {
+            pathVariables,
+            options,
+            params,
+        },
+        { immediate }
+    )
 
-// const runWorkflowByName = ({ body, immediate, options }) =>
-//     useAPIAsyncState(
-//         KeyMaps.workflow.RUN_WORKFLOW,
-//         'POST',
-//         {
-//             body,
-//             options,
-//         },
-//         { immediate }
-//     )
+const updateWorkflowByName = (name, body, { immediate, options }) =>
+    useAPI(
+        map.WORKFLOW_UPDATE_BY_NAME,
+        'POST',
+        {
+            options,
+            body,
+            pathVariables: {
+                name,
+            },
+        },
+        { immediate }
+    )
 
-// const createWorkflow = ({ params, body, immediate, options }) =>
-//     useAPIAsyncState(
-//         KeyMaps.workflow.CREATE_WORKFLOW,
-//         'POST',
-//         {
-//             params,
-//             body,
-//             options,
-//         },
-//         { immediate }
-//     )
+// ! depreciated, use filter on /workflowtemplate
+const getWorkflowTemplateByName = ({ immediate, options, pathVariables }) =>
+    useAPI(
+        map.WORKFLOW_TEMPLATE_NAME,
+        'GET',
+        {
+            pathVariables,
+            options,
+        },
+        { immediate }
+    )
 
-// const deleteSchedule = ({ pathVariables }) =>
-//     useAPIAsyncState(KeyMaps.workflow.DELETE_SCHEDULE, 'POST', {
-//         pathVariables,
-//     })
+const retryRun = (pathVariables) =>
+    useAPI(map.RETRY_RUN, 'POST', {
+        pathVariables,
+    })
 
-// const updateSchedule = ({ body, pathVariables }) =>
-//     useAPIAsyncState(KeyMaps.workflow.UPDATE_SCHEDULE, 'POST', {
-//         body,
-//         pathVariables,
-//     })
+const stopRun = (pathVariables) =>
+    useAPI(map.STOP_RUN, 'POST', {
+        pathVariables,
+    })
 
-// const addSchedule = ({ body }) =>
-//     useAPIAsyncState(KeyMaps.workflow.SCHEDULES, 'POST', { body })
+const getRunList = (pathVariables, { options, params }) =>
+    useAPISWRV(map.WORKFLOW_RUN, 'GET', 'getRunList', {
+        params,
+        options,
+        pathVariables,
+    })
 
-// const getSchedules = ({ params }) =>
-//     useAPISWRV(KeyMaps.workflow.SCHEDULES, 'GET', 'getSchedules', { params })
+const getArchivedRunList = (pathVariables, { options, params }) =>
+    useAPI(map.ARCHIVED_WORKFLOW_RUN, 'GET', 'getArchivedRunList', {
+        params,
+        options,
+        pathVariables,
+    })
 
-// const getArchivedRunLogs = (id, { immediate, options, params }) =>
-//     useAPIAsyncState(
-//         KeyMaps.workflow.ARCHIVED_WORKFLOW_RUN_LOGS,
-//         'GET',
-//         {
-//             options,
-//             params,
-//             pathVariables: {
-//                 id,
-//                 isArchived: true, // TODO
-//             },
-//         },
-//         { immediate }
-//     )
+const deleteWorkflowByName = ({ pathVariables, immediate, options }) =>
+    useAPI(
+        map.DELETE_WORKFLOW,
+        'POST',
+        {
+            options,
+            pathVariables,
+        },
+        { immediate }
+    )
+
+const runWorkflowByName = ({ body, immediate, options }) =>
+    useAPI(
+        map.RUN_WORKFLOW,
+        'POST',
+        {
+            body,
+            options,
+        },
+        { immediate }
+    )
+
+const createWorkflow = ({ params, body, immediate, options }) =>
+    useAPI(
+        map.CREATE_WORKFLOW,
+        'POST',
+        {
+            params,
+            body,
+            options,
+        },
+        { immediate }
+    )
+
+const deleteSchedule = ({ pathVariables }) =>
+    useAPI(map.DELETE_SCHEDULE, 'POST', {
+        pathVariables,
+    })
+
+const updateSchedule = ({ body, pathVariables }) =>
+    useAPI(map.UPDATE_SCHEDULE, 'POST', {
+        body,
+        pathVariables,
+    })
+
+const addSchedule = ({ body }) => useAPI(map.SCHEDULES, 'POST', { body })
+
+const getSchedules = ({ params }) =>
+    useAPI(map.SCHEDULES, 'GET', 'getSchedules', { params })
+
+const getArchivedRunLogs = (id, { immediate, options, params }) =>
+    useAPI(
+        map.ARCHIVED_WORKFLOW_RUN_LOGS,
+        'GET',
+        {
+            options,
+            params,
+            pathVariables: {
+                id,
+                isArchived: true, // TODO
+            },
+        },
+        { immediate }
+    )
 
 export const Workflows = {
     URL,
-    // List,
-    // retryRun,
-    // stopRun,
-    // getRunList,
-    // getArchivedRunList,
-    // updateWorkflowByName,
+    List,
+    retryRun,
+    stopRun,
+    getRunList,
+    getArchivedRunList,
+    updateWorkflowByName,
     getWorkflows,
-    // createWorkflow,
-    // deleteWorkflowByName,
-    // runWorkflowByName,
-    // getWorkflowTemplates,
-    // getWorkflowConfigMap,
-    // getWorkflowTemplateByName,
-    // deleteSchedule,
-    // updateSchedule,
-    // addSchedule,
-    // getSchedules,
-    // getArchivedRunLogs,
+    createWorkflow,
+    deleteWorkflowByName,
+    runWorkflowByName,
+    getWorkflowTemplates,
+    getWorkflowConfigMap,
+    getWorkflowTemplateByName,
+    deleteSchedule,
+    updateSchedule,
+    addSchedule,
+    getSchedules,
+    getArchivedRunLogs,
 }

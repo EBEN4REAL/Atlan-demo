6b78772c9f612182202ce685d1a36fc49dffc18c
Varun <varun@atlan.com>
2021-11-11 08:23:26 +0530

    Remove Health and Persona from Admin
    - Commented out Enums and Custom Metadata - doesnt build

--- src/pages/admin/enums.vue
+++ src/pages/admin/enums.vue
@@ -1,5 +1,5 @@
 <template>
-    <ExplorerLayout
+    <!--  <ExplorerLayout
         v-if="permissions.list"
         title="Enumerations"
         sub-title="Create & manage Enumerations"
@@ -32,65 +32,65 @@
         />
         <span v-else>No Enum Selected</span>
     </ExplorerLayout>
-    <NoAcces v-else />
+    <NoAcces v-else /> -->
 </template>
 
 <script lang="ts">
-    import { defineComponent, ref, computed } from 'vue'
-    import { useHead } from '@vueuse/head'
+    // import { defineComponent, ref, computed } from 'vue'
+    // import { useHead } from '@vueuse/head'
 
-    import useEnums from '@/admin/enums/composables/useEnums'
-    import EnumList from '@/admin/enums/enumList.vue'
-    import EnumDetails from '@/admin/enums/enumDetails.vue'
-    import AddEnumModal from '@/admin/enums/addEnumModal.vue'
-    import AtlanBtn from '@/UI/button.vue'
-    import ExplorerLayout from '@/admin/explorerLayout.vue'
-    import SearchAndFilter from '@/common/input/searchAndFilter.vue'
-    import { useAccessStore } from '~/services/access/accessStore'
-    import NoAcces from '@/common/secured/access.vue'
-    import useAuth from '~/composables/auth/useAuth'
+    // import useEnums from '@/admin/enums/composables/useEnums'
+    // import EnumList from '@/admin/enums/enumList.vue'
+    // import EnumDetails from '@/admin/enums/enumDetails.vue'
+    // import AddEnumModal from '@/admin/enums/addEnumModal.vue'
+    // import AtlanBtn from '@/UI/button.vue'
+    // import ExplorerLayout from '@/admin/explorerLayout.vue'
+    // import SearchAndFilter from '@/common/input/searchAndFilter.vue'
+    // import { useAccessStore } from '~/services/access/accessStore'
+    // import NoAcces from '@/common/secured/access.vue'
+    // import useAuth from '~/composables/auth/useAuth'
 
-    export default defineComponent({
-        components: {
-            EnumList,
-            EnumDetails,
-            AddEnumModal,
-            ExplorerLayout,
-            SearchAndFilter,
-            AtlanBtn,
-            NoAcces,
-        },
-        setup() {
-            useHead({
-                title: 'Enums',
-            })
-            const accessStore = useAccessStore()
-            const permissions = computed(() => ({
-                list: accessStore.checkPermission('LIST_BUSINESS_METADATA'),
-                create: accessStore.checkPermission('CREATE_BUSINESS_METADATA'),
-            }))
-            const { enumListData, selectedId, selectedEnum, addToList } =
-                useEnums()
-            const addModalVisible = ref(false)
+    // export default defineComponent({
+    //     components: {
+    //         EnumList,
+    //         EnumDetails,
+    //         AddEnumModal,
+    //         ExplorerLayout,
+    //         SearchAndFilter,
+    //         AtlanBtn,
+    //         NoAcces,
+    //     },
+    //     setup() {
+    //         useHead({
+    //             title: 'Enums',
+    //         })
+    //         const accessStore = useAccessStore()
+    //         const permissions = computed(() => ({
+    //             list: accessStore.checkPermission('LIST_BUSINESS_METADATA'),
+    //             create: accessStore.checkPermission('CREATE_BUSINESS_METADATA'),
+    //         }))
+    //         const { enumListData, selectedId, selectedEnum, addToList } =
+    //             useEnums()
+    //         const addModalVisible = ref(false)
 
-            function toggleAddModal(state: boolean) {
-                addModalVisible.value = state
-            }
+    //         function toggleAddModal(state: boolean) {
+    //             addModalVisible.value = state
+    //         }
 
-            const { isAccess } = useAuth()
+    //         const { isAccess } = useAuth()
 
-            return {
-                enumListData,
-                addModalVisible,
-                selectedId,
-                selectedEnum,
-                permissions,
-                toggleAddModal,
-                addToList,
-                isAccess,
-            }
-        },
-    })
+    //         return {
+    //             enumListData,
+    //             addModalVisible,
+    //             selectedId,
+    //             selectedEnum,
+    //             permissions,
+    //             toggleAddModal,
+    //             addToList,
+    //             isAccess,
+    //         }
+    //     },
+    // })
 </script>
 <route lang="yaml">
 meta:

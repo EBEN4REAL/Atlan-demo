267e2447a55171b3e59ccb4aee28819becee956b
AtlanKautzar <kautzar@atlan.com>
2021-11-11 11:43:39 +0700

    fix import keymap

--- src/composables/workflow/fetchWorkflowList.ts
+++ src/composables/workflow/fetchWorkflowList.ts
@@ -1,26 +1,34 @@
-import useSWRV from 'swrv';
-import swrvState from '../utils/swrvState';
-import { Workflows, URL } from '@services/argo/api/workflow';
+import useSWRV from 'swrv'
+import { Workflows, URL } from '@services/argo/api/workflow'
+import swrvState from '../utils/swrvState'
 
-export default function fetchWorkflowList(cachekey: string, dependent: any, paramsdefault: any, isArchive: boolean) {
-    const { data, error, mutate, isValidating } = useSWRV([cachekey, paramsdefault?.value, {}], () => {
-        if (dependent.value && isArchive) {
-            return Workflows.ArchivedList(paramsdefault.value);
-        }
-        if (dependent.value) {
-            return Workflows.List(paramsdefault.value);
-        }
-        
+export default function fetchWorkflowList(
+    cachekey: string,
+    dependent: any,
+    paramsdefault: any,
+    isArchive: boolean
+) {
+    const { data, error, mutate, isValidating } = useSWRV(
+        [cachekey, paramsdefault?.value, {}],
+        () => {
+            if (dependent.value && isArchive) {
+                return Workflows.ArchivedList(paramsdefault.value)
+            }
+            if (dependent.value) {
+                return Workflows.List(paramsdefault.value)
+            }
+
             return {}
-        
-    }, {
-        revalidateOnFocus: false,
-        dedupingInterval: 1,
-    });
+        },
+        {
+            revalidateOnFocus: false,
+            dedupingInterval: 1,
+        }
+    )
 
-    const phase = (item: any) => item?.status?.phase;
+    const phase = (item: any) => item?.status?.phase
 
-    const { state, STATES } = swrvState(data, error, isValidating);
+    const { state, STATES } = swrvState(data, error, isValidating)
     return {
         data,
         error,

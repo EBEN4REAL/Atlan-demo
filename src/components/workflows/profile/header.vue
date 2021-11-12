bd142b246c28e64c4e3fc8a86c000f9ebd7111a3
AtlanKautzar <kautzar@atlan.com>
2021-11-12 18:03:11 +0700

    fix if empty

--- src/components/workflows/profile/header.vue
+++ src/components/workflows/profile/header.vue
@@ -35,30 +35,32 @@
                 <div>
                     <h3 class="text-xl capitalize">
                         {{ workflow?.name }}
-                        <span
+                        <!-- <span
                             class="px-2 py-1 text-xs border border-transparent border-gray-200 rounded shadow-none hover:border-gray-300"
                             @click="$router.back()"
                         >
                             Scheduled
-                        </span>
+                        </span> -->
                     </h3>
                 </div>
                 <div class="flex items-center gap-x-3">
                     <div
                         class="flex items-center text-sm text-gray-500 gap-x-1"
                     >
-                        <AtlanIcon icon="User" />
+                        <AtlanIcon v-if="creator?.first_name" icon="User" />
                         <span>{{ creator?.first_name }}</span>
                     </div>
-                    <div style="color: rgb(196, 196, 196)">•</div>
+                    <div v-if="latRun" style="color: rgb(196, 196, 196)">•</div>
                     <div class="flex text-sm text-gray-500">
-                        <span class="text-gray-500"> {{latRun}}</span>
+                        <span class="text-gray-500"> {{ latRun }}</span>
                     </div>
                     <template v-if="totalRun !== 0">
-                      <div style="color: rgb(196, 196, 196)">•</div>
-                      <div class="flex text-sm text-gray-500">
-                          <span class="text-gray-500">{{totalRun}} total runs</span>
-                      </div>
+                        <div style="color: rgb(196, 196, 196)">•</div>
+                        <div class="flex text-sm text-gray-500">
+                            <span class="text-gray-500"
+                                >{{ totalRun }} total runs</span
+                            >
+                        </div>
                     </template>
                 </div>
             </div>
@@ -76,9 +78,7 @@
     import { defineComponent, watch, ref, toRefs } from 'vue'
     import { useTimeAgo } from '@vueuse/core'
     import UtilityButtons from '@/workflows/shared/utilityButtons.vue'
-    import {
-        getArchivedRunList,
-    } from '~/composables/workflow/useWorkflowList'
+    import { getArchivedRunList } from '~/composables/workflow/useWorkflowList'
 
     export default defineComponent({
         components: {
@@ -87,34 +87,31 @@
         props: {
             workflow: { type: Object, default: () => {} },
             id: {
-              type: String, 
-              required:true
+                type: String,
+                required: true,
             },
             creator: {
-              type: Object,
-              requred: true
+                type: Object,
+                requred: true,
+            },
+        },
+        setup(props) {
+            const totalRun = ref(0)
+            const latRun = ref('')
+            const { id } = toRefs(props)
+            const { archivedList } = getArchivedRunList(id.value)
+            watch(archivedList, (newVal) => {
+                totalRun.value = newVal.filter_record
+                if (newVal?.records?.length > 0) {
+                    const lastRun = newVal.records[newVal?.records.length - 1]
+                    latRun.value = useTimeAgo(lastRun.finished_at).value
+                }
+            })
+            return {
+                totalRun,
+                latRun,
             }
         },
-        setup (props) {
-          const totalRun = ref(0)
-          const latRun = ref("")
-          const {id} = toRefs(props)
-          const {
-            archivedList,
-          } = getArchivedRunList(id.value) 
-          watch(archivedList, (newVal) => {
-            totalRun.value = newVal.filter_record
-            if(newVal?.records?.length > 0){
-              const lastRun = newVal.records[newVal?.records.length - 1]
-              latRun.value = useTimeAgo(lastRun.finished_at).value
-            
-            }
-          })
-          return {
-            totalRun,
-            latRun
-          }
-        }
         // emits: ['openLogs'],
     })
 </script>

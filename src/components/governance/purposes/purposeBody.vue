31173cb7c31fa3087658f4061e7b05eacf9072c6
alii13 <shekhaliul44@gmail.com>
2021-11-14 22:07:25 +0530

    feat: basic setup for purposes

---
+++ src/components/governance/purposes/purposeBody.vue
@@ -0,0 +1,284 @@
+<template>
+    <template v-if="selectedPersonaDirty">
+        <MinimalTab v-model:active="activeTabKey" :data="tabConfig">
+            <template #label="t">
+                <div class="flex items-center">
+                    <span
+                        class="text-base"
+                        :class="
+                            activeTabKey === t?.data?.key
+                                ? 'text-gray-700'
+                                : 'text-gray-500'
+                        "
+                        >{{ t?.data?.label }}</span
+                    >
+                    <div
+                        class="px-1 py-0.5 ml-2 text-sm font-bold rounded"
+                        v-if="t?.data?.key === 'policies'"
+                        :class="
+                            activeTabKey === t?.data?.key
+                                ? 'text-primary bg-primary-light'
+                                : 'text-gray-500 bg-gray-100'
+                        "
+                    >
+                        {{
+                            selectedPersonaDirty?.metadataPolicies?.length ??
+                            0 + selectedPersonaDirty?.datapolicies?.length ??
+                            0
+                        }}
+                    </div>
+                    <div
+                        class="px-1 py-0.5 ml-2 text-sm font-bold rounded"
+                        v-if="t?.data?.key === 'users'"
+                        :class="
+                            activeTabKey === t?.data?.key
+                                ? 'text-primary bg-primary-light'
+                                : 'text-gray-500 bg-gray-100'
+                        "
+                    >
+                        {{
+                            selectedPersonaDirty?.users?.length ??
+                            0 + selectedPersonaDirty?.groups?.length ??
+                            0
+                        }}
+                    </div>
+                </div>
+            </template>
+        </MinimalTab>
+
+        <div class="px-4 overflow-y-auto">
+            <PurposeMeta
+                v-if="activeTabKey === 'details'"
+                class="pb-2"
+                :persona="persona"
+            />
+            <div v-else-if="activeTabKey === 'policies'" class="mt-2">
+                <template
+                    v-for="(
+                        policy, idx
+                    ) in selectedPersonaDirty.metadataPolicies"
+                    :key="idx"
+                >
+                    <!-- Render it if the policy is being edited -->
+                    <MetadataPolicy
+                        v-if="policyEditMap.metadataPolicies[policy.id!]"
+                        class="px-5"
+                        :policy="policy"
+                        @delete="deletePolicyUI('meta', policy.id!)"
+                        @save="savePolicyUI('meta', policy.id!)"
+                        @cancel="discardPolicy('meta', policy.id!)"
+                    />
+
+                    <PolicyCard
+                        v-else
+                        class="px-5"
+                        :policy="policy"
+                        type="meta"
+                        @edit="setEditFlag('meta', policy.id!)"
+                    />
+                </template>
+
+                <template
+                    v-for="(policy, idx) in selectedPersonaDirty.dataPolicies"
+                    :key="idx"
+                >
+                    <!-- Render it if the policy is being edited -->
+                    <DataPolicy
+                        v-if="policyEditMap.dataPolicies[policy.id!]"
+                        class="px-5"
+                        :policy="policy"
+                        @delete="deletePolicyUI('data', policy.id!)"
+                        @save="savePolicyUI('data', policy.id!)"
+                        @cancel="discardPolicy('data', policy.id!)"
+                    />
+                    <!-- ^^^ FIXME: Add implemmentation for @save and @cancel ^^^-->
+                    <PolicyCard
+                        v-else
+                        class="px-5"
+                        :policy="policy"
+                        type="data"
+                        @edit="setEditFlag('data', policy.id!)"
+                    />
+                </template>
+                <div
+                    v-if="
+                        !selectedPersonaDirty.metadataPolicies?.length &&
+                        !selectedPersonaDirty.dataPolicies?.length
+                    "
+                    class="flex flex-col items-center justify-center mt-8"
+                >
+                    <component :is="NewPolicyIllustration"></component>
+                    <span class="text-2xl font-bold text-gray">
+                        Create Policies</span
+                    >
+                </div>
+                <a-dropdown trigger="click">
+                    <AtlanBtn
+                        class="flex-none mx-auto mt-6"
+                        color="primary"
+                        padding="compact"
+                        size="sm"
+                        @click.prevent
+                    >
+                        <template #prefix>
+                            <AtlanIcon icon="Add" />
+                        </template>
+                        Add new policy
+                        <template #suffix>
+                            <AtlanIcon icon="ChevronDown" class="text-white" />
+                        </template>
+                    </AtlanBtn>
+
+                    <template #overlay>
+                        <a-menu>
+                            <a-menu-item
+                                v-for="(
+                                    option, index
+                                ) in addPolicyDropdownConfig"
+                                :key="index"
+                                @click="option.handleClick()"
+                            >
+                                <div class="flex items-center">
+                                    <AtlanIcon
+                                        v-if="option.icon"
+                                        :icon="option.icon"
+                                    />
+                                    <span class="pl-2 text-sm">{{
+                                        option.title
+                                    }}</span>
+                                </div>
+                            </a-menu-item>
+                        </a-menu>
+                    </template>
+                </a-dropdown>
+            </div>
+            <PurposeUsersGroups
+                v-else-if="activeTabKey === 'users'"
+                class="pt-6 pb-2"
+                v-model:persona="persona"
+            />
+        </div>
+    </template>
+</template>
+
+<script lang="ts">
+    import { defineComponent, PropType, toRefs, watch } from 'vue'
+    import { message } from 'ant-design-vue'
+
+    import MinimalTab from '@/UI/minimalTab.vue'
+    import AtlanBtn from '@/UI/button.vue'
+
+    import NewPolicyIllustration from '~/assets/images/illustrations/new_policy.svg'
+
+    import PolicyCard from './policies/collapsedPolicyCard.vue'
+    import PurposeUsersGroups from './users/purposeUsersGroups.vue'
+    import MetadataPolicy from './policies/metadataPolicyItem.vue'
+    import DataPolicy from './policies/dataPolicyItem.vue'
+    import PurposeMeta from './overview/purposeMeta.vue'
+    import { IPersona } from '~/types/accessPolicies/personas'
+    import {
+        selectedPersonaDirty,
+        addPolicy,
+        deletePolicy,
+        policyEditMap,
+        setEditFlag,
+        removeEditFlag,
+        savePolicy,
+        discardPolicy,
+        PolicyType,
+    } from './composables/useEditPurpose'
+    import { activeTabKey, tabConfig } from './composables/usePersonaTabs'
+
+    export default defineComponent({
+        name: 'PurposeBody',
+        components: {
+            MinimalTab,
+            PolicyCard,
+            MetadataPolicy,
+            DataPolicy,
+            AtlanBtn,
+            PurposeMeta,
+            PurposeUsersGroups,
+        },
+        props: {
+            persona: {
+                type: Object as PropType<IPersona>,
+                required: true,
+            },
+        },
+        setup() {
+            const addPolicyDropdownConfig = [
+                {
+                    title: 'Metadata Policy',
+                    icon: 'Settings',
+                    handleClick: () => addPolicy('meta'),
+                },
+                {
+                    title: 'Data Policy',
+                    icon: 'Queries',
+                    handleClick: () => addPolicy('data'),
+                },
+            ]
+
+            async function savePolicyUI(type: PolicyType, id: string) {
+                const messageKey = Date.now()
+                message.loading({
+                    content: 'Saving policy',
+                    duration: 0,
+                    key: messageKey,
+                })
+                try {
+                    await savePolicy(type, id)
+                    message.success({
+                        content: 'Policy saved',
+                        duration: 1.5,
+                        key: messageKey,
+                    })
+                } catch (error) {
+                    message.error({
+                        content: 'Failed to save policy',
+                        duration: 1.5,
+                        key: messageKey,
+                    })
+                }
+            }
+
+            async function deletePolicyUI(type: PolicyType, id: string) {
+                const messageKey = Date.now()
+                message.loading({
+                    content: 'Deleting policy',
+                    duration: 0,
+                    key: messageKey,
+                })
+                try {
+                    await deletePolicy(type, id)
+                    message.success({
+                        content: 'Policy deleted',
+                        duration: 1.5,
+                        key: messageKey,
+                    })
+                } catch (error) {
+                    message.error({
+                        content: 'Failed to delete policy',
+                        duration: 1.5,
+                        key: messageKey,
+                    })
+                }
+            }
+
+            return {
+                activeTabKey,
+                tabConfig,
+                selectedPersonaDirty,
+                addPolicyDropdownConfig,
+                policyEditMap,
+                setEditFlag,
+                removeEditFlag,
+                savePolicyUI,
+                deletePolicyUI,
+                discardPolicy,
+                NewPolicyIllustration,
+            }
+        },
+    })
+</script>
---
+++ src/components/governance/purposes/purposeHeader.vue
@@ -0,0 +1,160 @@
+<template>
+    <div v-if="persona" class="flex flex-col px-5">
+        <CreationModal
+            v-model:visible="isEditing"
+            ok-text="Save"
+            title="Edit Persona"
+            @cancel="discardPersona"
+            @ok="saveEditedPersona"
+        >
+            <div class="flex flex-col items-stretch pb-4 gap-y-1">
+                <input
+                    v-model="selectedPersonaDirty!.displayName"
+                    placeholder="Untitled persona"
+                    type="text"
+                    class="text-lg font-bold text-gray-700 clean-input"
+                    @keyup.esc="$event?.target?.blur()"
+                />
+
+                <textarea
+                    v-model="selectedPersonaDirty!.description"
+                    class="text-sm text-gray-500 clean-input"
+                    maxlength="140"
+                    rows="2"
+                    placeholder="Add description..."
+                    @keyup.esc="$event?.target?.blur()"
+                />
+            </div>
+        </CreationModal>
+        <div class="flex mb-2 gap-x-2">
+            <div style="width: 90%">
+                <div class="mb-1 text-xl font-bold truncate text-gray">
+                    {{ persona.displayName }}
+                </div>
+                <div class="flex mb-0 text-sm text-gray-500">
+                    <span class="truncate">{{ persona.description }}</span>
+                </div>
+            </div>
+            <Dropdown class="ml-auto" :options="personaActions"></Dropdown>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent, PropType, computed, toRefs, ref } from 'vue'
+    import { message } from 'ant-design-vue'
+
+    import CreationModal from '@/admin/common/addModal.vue'
+
+    import { IPersona } from '~/types/accessPolicies/personas'
+    import {
+        isEditing,
+        savePersona,
+        discardPersona,
+        selectedPersonaDirty,
+        deletePersonaById,
+    } from './composables/useEditPurpose'
+
+    import Dropdown from '@/UI/dropdown.vue'
+    import { reFetchList } from './composables/usePersonaList'
+
+    export default defineComponent({
+        name: 'PurposeHeader',
+        components: { Dropdown, CreationModal },
+        props: {
+            persona: {
+                type: Object as PropType<IPersona>,
+                required: true,
+            },
+        },
+        setup(props, { emit }) {
+            const { persona } = toRefs(props)
+            const personaActions = computed(() => [
+                {
+                    title: 'Edit',
+                    icon: 'Edit',
+                    handleClick: () => {
+                        isEditing.value = true
+                    },
+                },
+                {
+                    title: 'Delete',
+                    icon: 'Trash',
+                    class: 'text-red-700',
+                    handleClick: async () => {
+                        const msgId = Date.now()
+                        message.loading({
+                            content: `Deleting ${persona.value.displayName}`,
+                            duration: 0,
+                            key: msgId,
+                        })
+                        try {
+                            await deletePersonaById(persona.value.id!)
+                            message.success({
+                                content: 'Persona deleted',
+                                duration: 1.5,
+                                key: msgId,
+                            })
+                        } catch (error) {
+                            message.error({
+                                content: 'Failed to delete persona',
+                                duration: 1.5,
+                                key: msgId,
+                            })
+                        }
+                    },
+                },
+            ])
+
+            async function saveEditedPersona() {
+                const messageKey = Date.now()
+                message.loading({
+                    content: 'Working on it...',
+                    duration: 0,
+                    key: messageKey,
+                })
+
+                try {
+                    await savePersona({
+                        ...persona.value,
+                        displayName: selectedPersonaDirty.value?.displayName,
+                        description: selectedPersonaDirty.value?.description,
+                    })
+
+                    message.success({
+                        content: `${persona.value?.displayName} persona updated`,
+                        duration: 1.5,
+                        key: messageKey,
+                    })
+
+                    isEditing.value = false
+                    reFetchList()
+                } catch (error) {
+                    message.error({
+                        content: 'Failed to update persona',
+                        duration: 1.5,
+                        key: messageKey,
+                    })
+                }
+            }
+
+            return {
+                personaActions,
+                isEditing,
+                saveEditedPersona,
+                discardPersona,
+                selectedPersonaDirty,
+            }
+        },
+    })
+</script>
+
+<style lang="less" scoped>
+    .clean-input {
+        @apply block bg-transparent border-0 shadow-none outline-none;
+
+        &:focus {
+            @apply outline-none;
+        }
+    }
+</style>

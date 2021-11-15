2f7c88a4aacf57ae188cb52971f3e828bbe7c46a
alii13 <shekhaliul44@gmail.com>
2021-11-15 13:05:35 +0530

    fix: search bug in asset select drawer

--- src/components/insights/explorers/schema/composables/useBody.ts
+++ src/components/insights/explorers/schema/composables/useBody.ts
@@ -1,6 +1,42 @@
 /* eslint-disable default-case */
 import bodybuilder from 'bodybuilder'
 
+function addQueryTextFilter(base: any, queryText) {
+    base.orQuery('match', 'name', {
+        query: queryText,
+        boost: 40,
+        analyzer: 'search_synonyms',
+    })
+    base.orQuery('match', 'name', {
+        query: queryText,
+        boost: 40,
+    })
+
+    base.orQuery('match', 'name', {
+        query: queryText,
+        operator: 'AND',
+        boost: 40,
+    })
+    base.orQuery('match', 'name.keyword', {
+        query: queryText,
+        boost: 100,
+    })
+    base.orQuery('match_phrase', 'name', {
+        query: queryText,
+        boost: 70,
+    })
+    base.orQuery('wildcard', 'name.keyword', {
+        value: `${queryText}*`,
+    })
+    base.orQuery('match', 'description', {
+        query: queryText,
+    })
+    base.orQuery('match', 'userDescription', {
+        query: queryText,
+    })
+    base.orQuery('match', 'name.stemmed', { query: queryText })
+    base.queryMinimumShouldMatch(1)
+}
 export function useBody(
     sort: any,
     appliedFilters: any,
@@ -11,42 +47,6 @@ export function useBody(
 ) {
     const base = bodybuilder()
 
-    if (queryText) {
-        base.orQuery('match', 'name', {
-            query: queryText,
-            boost: 40,
-            analyzer: 'search_synonyms',
-        })
-        base.orQuery('match', 'name', {
-            query: queryText,
-            boost: 40,
-        })
-
-        base.orQuery('match', 'name', {
-            query: queryText,
-            operator: 'AND',
-            boost: 40,
-        })
-        base.orQuery('match', 'name.keyword', {
-            query: queryText,
-            boost: 100,
-        })
-        base.orQuery('match_phrase', 'name', {
-            query: queryText,
-            boost: 70,
-        })
-        base.orQuery('wildcard', 'name.keyword', {
-            value: `${queryText}*`,
-        })
-        base.orQuery('match', 'description', {
-            query: queryText,
-        })
-        base.orQuery('match', 'userDescription', {
-            query: queryText,
-        })
-        base.orQuery('match', 'name.stemmed', { query: queryText })
-        base.queryMinimumShouldMatch(1)
-    }
     appliedFilters.forEach((filter, i) => {
         if (i == 0) {
             const ob = Object.keys(filter.term)
@@ -54,8 +54,18 @@ export function useBody(
         }
         if (i == 1) {
             if (Array.isArray(typeName))
-                base.filter('terms', '__typeName.keyword', typeName)
-            else base.filter('term', '__typeName.keyword', typeName)
+                base.filter('terms', '__typeName.keyword', [
+                    'Database',
+                    'Schema',
+                    'Table',
+                    'Column',
+                ])
+            else {
+                /* Only use queryText when searching for database/ topmost parent, children won't be filtered using query text */
+                if (typeName === 'Database' && queryText)
+                    addQueryTextFilter(base, queryText)
+                base.filter('term', '__typeName.keyword', typeName)
+            }
         }
     })
 

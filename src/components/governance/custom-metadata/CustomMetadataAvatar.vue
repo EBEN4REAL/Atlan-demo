7406789125b98a2a186a3a73e19609a184a08c79
Izuorah Dubem kizito <dubemizuus@gmail.com>
2021-11-17 05:43:23 +0100

    CM Avatar Refactor
    - created reusable component for CM avatar
    - created composable for CM avatar update
    - Created Composable for TypeDef update (WIP)

---
+++ src/components/governance/custom-metadata/CustomMetadataAvatar.vue
@@ -0,0 +1,76 @@
+<template>
+    <div
+        class="relative bg-gray-100 rounded cursor-pointer"
+        :style="{ width: size, height: size }"
+    >
+        <div
+            v-if="metadata?.options?.imageId || metadata?.options?.emoji"
+            class="flex items-center justify-center w-full h-full"
+            @click="popOverVisible = !popOverVisible"
+        >
+            <div v-if="metadata?.options?.logoType === 'image'">
+                <img
+                    :src="imageUrl"
+                    alt=""
+                    class="object-cover w-full"
+                    :style="{ height: size }"
+                />
+            </div>
+            <span
+                v-else
+                class="self-center block leading-none"
+                :style="{ fontSize: `calc(${size} - 2px)` }"
+            >
+                {{ metadata?.options?.emoji }}
+            </span>
+        </div>
+        <div
+            v-else
+            class="flex items-center justify-center w-full h-full"
+            @click="popOverVisible = !popOverVisible"
+        >
+            <a-spin v-if="isUpdating" />
+            <AtlanIcon v-else icon="NoAvatar" />
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent, toRefs } from 'vue'
+
+    // Emoji
+    import data from 'emoji-mart-vue-fast/data/twitter.json'
+    import { Picker, EmojiIndex } from 'emoji-mart-vue-fast/src'
+    import 'emoji-mart-vue-fast/css/emoji-mart.css'
+
+    import useCustomMetadataAvatar from './composables/useCustomMetadataAvatar'
+
+    const emojiIndex = new EmojiIndex(data)
+
+    export default defineComponent({
+        props: {
+            metadata: {
+                required: true,
+                type: Object,
+                default: () => {},
+            },
+            size: {
+                type: String,
+                default: '28px',
+            },
+            isUpdating: {
+                type: Boolean,
+                default: false,
+            },
+        },
+        setup(props) {
+            const { metadata } = toRefs(props)
+
+            const { imageUrl } = useCustomMetadataAvatar(metadata)
+
+            return {
+                imageUrl,
+            }
+        },
+    })
+</script>

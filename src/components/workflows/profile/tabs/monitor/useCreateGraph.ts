cdc31a5beb4811d5f53e95adb5c86d68c32d9d84
Ebuka Ezeh <ebuka@atlan.com>
2021-11-18 10:56:18 +0100

    fix: Remove dagre layout from global

--- src/components/workflows/profile/tabs/monitor/useCreateGraph.ts
+++ src/components/workflows/profile/tabs/monitor/useCreateGraph.ts
@@ -1,5 +1,6 @@
 import { ref } from 'vue'
 import { SimpleNodeView } from './view.js'
+import { DagreLayout } from '../../../../common/assets/profile/tabs/lineage/dagreLayout/dagre'
 
 export default function useCreateGraph(
     graph,
@@ -10,7 +11,7 @@ export default function useCreateGraph(
 
     /* Build Graph Canvas */
     const { Graph } = window.X6
-    const { DagreLayout } = window.layout
+    // const { DagreLayout } = window.layout
 
     graph.value = new Graph({
         async: true, // Saves resources and enhances performance.

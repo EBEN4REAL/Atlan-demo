a97cd655e7bace36ff9d0e874267f351ea38cb84
Milek Agrawal <milek@atlan.com>
2021-11-18 19:06:30 +0530

    Add new update entity api

--- src/services/meta/insights/key.ts
+++ src/services/meta/insights/key.ts
@@ -5,6 +5,7 @@ export const CREATE_SAVED_QUERY = 'CREATE_SAVED_QUERY'
 export const CREATE_QUERY_FOLDER = 'CREATE_QUERY_FOLDER'
 export const GET_SAVED_QUERY = 'GET_SAVED_QUERY'
 export const UPDATE_SAVED_QUERY = 'UPDATE_SAVED_QUERY'
+export const UPDATE_SAVED_FOLDER = 'UPDATE_SAVED_FOLDER'
 export const DELETE_ENTITY = 'DELETE_ENTITY'
 
 export const map = {
@@ -13,7 +14,8 @@ export const map = {
     [CREATE_QUERY_FOLDER]: () => getAPIPath('meta', '/entity'),
     [GET_SAVED_QUERY]: ({ guid }: PathParams) =>
         getAPIPath('meta', `/entity/guid/${guid}`),
-    [UPDATE_SAVED_QUERY]: () => getAPIPath('meta', `/entity`),
+    [UPDATE_SAVED_QUERY]: () => getAPIPath('meta', `/entity?replaceClassifications=true`),
+    [UPDATE_SAVED_FOLDER]: () => getAPIPath('meta', `/entity?replaceClassifications=true`),
     [DELETE_ENTITY]: ({ guid }: Record<string, any>) =>
         getAPIPath('meta', `/entity/bulk?guid=${guid}`),
 }

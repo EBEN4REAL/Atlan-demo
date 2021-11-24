ace0a92cd34d41deefa5cc57d05a02a76edd9931
shivanshpahwa24 <shivanshpahwa24@gmail.com>
2021-11-25 01:57:26 +0530

    info tab cleanup

--- src/components/common/assets/preview/info/index.vue
+++ src/components/common/assets/preview/info/index.vue
@@ -305,9 +305,7 @@
         Ref,
         reactive,
     } from 'vue'
-    import { whenever } from '@vueuse/core'
     import SavedQuery from '@common/hovercards/savedQuery.vue'
-    import { message } from 'ant-design-vue'
     import AnnouncementWidget from '@/common/widgets/announcement/index.vue'
     import SQL from '@/common/popover/sql.vue'
     import useAssetInfo from '~/composables/discovery/useAssetInfo'
@@ -318,14 +316,9 @@
     import Certificate from '@/common/input/certificate/index.vue'
     import Classification from '@/common/input/classification/index.vue'
     import Terms from '@/common/input/terms/index.vue'
-    import updateAsset from '~/composables/discovery/updateAsset'
-    import useSetClassifications from '~/composables/discovery/useSetClassifications'
-    import { useCurrentUpdate } from '~/composables/discovery/useCurrentUpdate'
-    import whoami from '~/composables/user/whoami'
-    import confetti from '~/utils/confetti'
     import Shortcut from '@/common/popover/shortcut.vue'
-
     import Connection from './connection.vue'
+    import updateAssetAttributes from '~/composables/discovery/updateAssetAttributes'
 
     export default defineComponent({
         name: 'AssetDetails',
@@ -355,13 +348,28 @@
             const actions = inject('actions')
             const selectedAsset = inject('selectedAsset')
             const switchTab = inject('switchTab')
-            const isConfetti = ref(false)
 
             const sampleDataVisible = ref<boolean>(false)
 
             const showSampleDataModal = () => {
                 sampleDataVisible.value = true
             }
+
+            const {
+                entity,
+                isLoading,
+                localName,
+                localDescription,
+                localCertificate,
+                localOwners,
+                localClassifications,
+                handleChangeName,
+                handleChangeDescription,
+                handleOwnersChange,
+                handleChangeCertificate,
+                handleClassificationChange,
+                isLoadingClassification,
+            } = updateAssetAttributes(selectedAsset)
 
             const {
                 title,
@@ -396,261 +404,6 @@
                 certificateUpdatedBy,
                 isGTC,
             } = useAssetInfo()
-
-            const entity = ref({
-                guid: selectedAsset.value.guid,
-                typeName: selectedAsset.value.typeName,
-                attributes: {
-                    name: selectedAsset.value.attributes?.name,
-                    qualifiedName:
-                        selectedAsset.value.attributes?.qualifiedName,
-                    tenantId: 'default',
-                },
-            })
-
-            const guid = ref()
-
-            const {
-                asset,
-                mutate: mutateUpdate,
-                isReady: isUpdateReady,
-            } = useCurrentUpdate({
-                id: guid,
-            })
-
-            if (
-                [
-                    'AtlasGlossary',
-                    'AtlasGlossaryTerm',
-                    'AtlasGlossaryCategory',
-                ].includes(entity.value.typeName)
-            ) {
-                entity.value.relationshipAttributes = {
-                    anchor: {
-                        typeName: 'AtlasGlossary',
-                        guid: getAnchorGuid(selectedAsset.value),
-                    },
-                }
-            }
-
-            const body = ref({
-                entities: [],
-            })
-
-            const { mutate, isLoading, isReady, error } = updateAsset(body)
-
-            const localName = ref(title(selectedAsset?.value))
-            const localDescription = ref(description(selectedAsset?.value))
-            const localCertificate = ref({
-                certificateStatus: certificateStatus(selectedAsset.value),
-                certificateUpdatedAt: certificateUpdatedAt(selectedAsset.value),
-                certificateUpdatedBy: certificateUpdatedBy(selectedAsset.value),
-                certificateStatusMessage: certificateStatusMessage(
-                    selectedAsset.value
-                ),
-            })
-            const localOwners = ref({
-                ownerUsers: ownerUsers(selectedAsset.value),
-                ownerGroups: ownerGroups(selectedAsset.value),
-            })
-
-            const localClassifications = ref(
-                classifications(selectedAsset.value)
-            )
-
-            const currentMessage = ref('')
-
-            const nameRef = ref(null)
-            const descriptionRef = ref(null)
-
-            // Name Change
-            const handleChangeName = () => {
-                if (title(selectedAsset?.value) !== localName.value) {
-                    entity.value.attributes.name = localName.value
-                    body.value.entities = [entity.value]
-                    currentMessage.value = 'Name has been updated'
-                    mutate()
-                }
-            }
-
-            // Description Change
-            const handleChangeDescription = () => {
-                if (
-                    description(selectedAsset?.value) !== localDescription.value
-                ) {
-                    entity.value.attributes.userDescription =
-                        localDescription.value
-                    body.value.entities = [entity.value]
-                    currentMessage.value = 'Description has been updated'
-                    mutate()
-                }
-            }
-
-            // Owners Change
-            const handleOwnersChange = () => {
-                let isChanged = false
-                if (
-                    entity.value.attributes.ownerUsers?.sort().toString() !==
-                    localOwners.value?.ownerUsers.sort().toString()
-                ) {
-                    entity.value.attributes.ownerUsers =
-                        localOwners.value?.ownerUsers
-                    isChanged = true
-                }
-                if (
-                    entity.value.attributes.ownerGroups?.sort().toString() !==
-                    localOwners.value?.ownerGroups.sort().toString()
-                ) {
-                    entity.value.attributes.ownerGroups =
-                        localOwners.value?.ownerGroups
-                    isChanged = true
-                }
-
-                if (isChanged) {
-                    body.value.entities = [entity.value]
-                    currentMessage.value = 'Owners has been updated'
-                    mutate()
-                }
-            }
-
-            // error handling
-            whenever(error, () => {
-                if (title(selectedAsset?.value) !== localName.value) {
-                    localName.value = title(selectedAsset?.value)
-                    nameRef.value?.handleReset(localName.value)
-                }
-                if (
-                    description(selectedAsset?.value) !== localDescription.value
-                ) {
-                    localDescription.value = description(selectedAsset?.value)
-                    descriptionRef.value?.handleReset(localDescription.value)
-                }
-                message.error('Something went wrong. Please try again')
-            })
-
-            whenever(isReady, () => {
-                message.success(currentMessage.value)
-                guid.value = selectedAsset.value.guid
-                rainConfettis()
-                mutateUpdate()
-            })
-
-            const updateList = inject('updateList')
-            whenever(isUpdateReady, () => {
-                if (
-                    asset.value.typeName !== 'AtlasGlossary' &&
-                    asset.value.typeName !== 'AtlasGlossaryCategory' &&
-                    asset.value.typeName !== 'AtlasGlossaryTerm'
-                ) {
-                    updateList(asset.value)
-                }
-            })
-
-            const classificationBody = ref({
-                guidHeaderMap: {
-                    [selectedAsset.value.guid]: {
-                        classifications: localClassifications.value,
-                    },
-                },
-            })
-
-            const {
-                mutate: mutateClassification,
-                isLoading: isLoadingClassification,
-                isReady: isReadyClassification,
-                error: isErrorClassification,
-            } = useSetClassifications(classificationBody)
-
-            const arrayEquals = (a, b) =>
-                Array.isArray(a) &&
-                Array.isArray(b) &&
-                a.length === b.length &&
-                a.every((val, index) =>
-                    b.map((i) => i.typeName).includes(val.typeName)
-                )
-
-            const handleClassificationChange = () => {
-                console.log(classifications(selectedAsset.value))
-                console.log(localClassifications.value)
-                if (
-                    !arrayEquals(
-                        classifications(selectedAsset.value),
-                        localClassifications.value
-                    )
-                ) {
-                    classificationBody.value = {
-                        guidHeaderMap: {
-                            [selectedAsset.value.guid]: {
-                                ...entity.value,
-                                classifications: localClassifications.value,
-                            },
-                        },
-                    }
-                    currentMessage.value = 'Classifications have been updated'
-                    mutateClassification()
-                }
-            }
-
-            whenever(isReadyClassification, () => {
-                message.success(currentMessage.value)
-                guid.value = selectedAsset.value.guid
-                mutateUpdate()
-            })
-
-            whenever(isErrorClassification, () => {
-                message.error('Something went wrong. Please try again')
-            })
-
-            const { username } = whoami()
-            const handleChangeCertificate = () => {
-                if (
-                    localCertificate.value.certificateStatus !==
-                        certificateStatus(selectedAsset.value) ||
-                    localCertificate.value.certificateStatusMessage !==
-                        certificateStatusMessage(selectedAsset.value)
-                ) {
-                    if (
-                        localCertificate.value.certificateStatus === 'VERIFIED'
-                    ) {
-                        isConfetti.value = true
-                    } else {
-                        isConfetti.value = false
-                    }
-
-                    entity.value.attributes.certificateStatus =
-                        localCertificate.value.certificateStatus
-
-                    entity.value.attributes.certificateStatusMessage =
-                        localCertificate.value.certificateStatusMessage
-                    body.value.entities = [entity.value]
-                    currentMessage.value = 'Certificate has been updated'
-                    mutate()
-                }
-            }
-
-            const animationPoint = ref(null)
-            const rainConfettis = () => {
-                const config = {
-                    angle: 45,
-                    startVelocity: 10,
-                    spread: 200,
-                    elementCount: 100,
-                    colors: [
-                        '#2251cc',
-                        '#2251cc',
-                        '#82b54b',
-                        '#e94a3f',
-                        '#faa040',
-                    ],
-                    width: '0.3rem',
-                    height: '0.3rem',
-                }
-                if (isConfetti.value) {
-                    if (animationPoint) {
-                        confetti(animationPoint.value, config)
-                    }
-                }
-            }
 
             const isSelectedAssetHaveRowsAndColumns = (selectedAsset) => {
                 if (

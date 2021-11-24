973756a9b34cad43e924d7ad00ee250bbe330251
Milek Agrawal <milek@atlan.com>
2021-11-24 10:31:21 +0530

    fix color

--- src/components/insights/playground/index.vue
+++ src/components/insights/playground/index.vue
@@ -5,7 +5,7 @@
             fullSreenState ? 'height: calc( 100vh - 40px )' : 'height:100vh'
         "
     >
-        <div class="relative flex flex-col">
+        <div class="relative flex flex-col bg-gray-light">
             <div class="flex w-full text-gray">
                 <a-tabs
                     v-model:activeKey="activeInlineTabKey"

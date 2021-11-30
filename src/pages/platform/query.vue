c92fde2b1a49a62e5d1fc578a07970a25f44ed3b
abstrekt <saammmmmmy@gmail.com>
2021-11-30 11:57:45 +0530

    add /platform

---
+++ src/pages/platform/query.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="w-full h-full overflow-hidden">
+        <iframe
+            ref="iframeRef"
+            :src="iframeUrl"
+            class="w-full h-full"
+            style="position: relative; left: 0px; top: 0px"
+        >
+            <p>Your browser does not support iframes.</p>
+        </iframe>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent, reactive, toRefs } from 'vue'
+
+    export default defineComponent({
+        name: 'QueryPage',
+        setup() {
+            const state = reactive({
+                iframeUrl:
+                    '/api/observe/d/E_7xL0mnz/heka-dashboard?orgId=1&refresh=10s',
+            })
+
+            return {
+                ...toRefs(state),
+            }
+        },
+    })
+</script>
+
+<style scoped></style>

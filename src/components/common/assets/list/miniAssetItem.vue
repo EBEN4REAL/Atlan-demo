dcb162c97c4938639f8bfb617b89606f7437f3a7
AtlanKautzar <kautzar@atlan.com>
2021-12-03 17:59:51 +0700

    truncate text

--- src/components/common/assets/list/miniAssetItem.vue
+++ src/components/common/assets/list/miniAssetItem.vue
@@ -51,13 +51,13 @@
                             />
                         </a-tooltip>
                         <div
-                            class="text-sm tracking-wider text-gray-500 uppercase truncate max-40"
+                            class="text-sm tracking-wider text-gray-500 uppercase truncate  max-40"
                         >
                             {{ assetTypeLabel(item) || item.typeName }}
                         </div>
                         <div
                             v-if="item.typeName?.toLowerCase() === 'table'"
-                            class="flex flex-wrap items-center ml-2 text-sm text-gray-500 gap-x-2"
+                            class="flex flex-wrap items-center ml-2 text-sm text-gray-500  gap-x-2"
                         >
                             <div class="dot" />
                             <a-tooltip placement="bottomLeft">
@@ -99,7 +99,7 @@
                                     ?.toLowerCase()
                                     .includes('glossary')
                             "
-                            class="flex flex-wrap items-center ml-2 text-sm text-gray-500 gap-x-2"
+                            class="flex flex-wrap items-center ml-2 text-sm text-gray-500  gap-x-2"
                         >
                             <div v-if="item.attributes.name" class="dot" />
                             <a-tooltip
@@ -127,7 +127,7 @@
                             v-if="
                                 item.typeName?.toLowerCase().includes('query')
                             "
-                            class="flex flex-wrap items-center ml-2 text-sm text-gray-500 gap-x-2"
+                            class="flex flex-wrap items-center ml-2 text-sm text-gray-500  gap-x-2"
                         >
                             <div v-if="item.attributes.name" class="dot" />
                             <a-tooltip
@@ -165,7 +165,7 @@
                         </div>
                         <div
                             v-if="item.typeName?.toLowerCase() === 'column'"
-                            class="flex flex-wrap items-center ml-2 text-sm text-gray-500 gap-x-2"
+                            class="flex flex-wrap items-center ml-2 text-sm text-gray-500  gap-x-2"
                         >
                             <div v-if="tableName(item)" class="dot" />
                             <a-tooltip
@@ -180,7 +180,9 @@
                                         icon="ViewGray"
                                         class="mr-1 mb-0.5 icon-blue-color"
                                     />
-                                    <div class="tracking-tight text-gray-500">
+                                    <div
+                                        class="tracking-tight text-gray-500 truncate  max-40"
+                                    >
                                         {{ tableName(item) }}
                                     </div>
                                 </div>
@@ -195,7 +197,7 @@
                                     item.typeName?.toLowerCase()
                                 )
                             "
-                            class="flex flex-wrap items-center ml-2 text-sm text-gray-500 gap-x-2"
+                            class="flex flex-wrap items-center ml-2 text-sm text-gray-500  gap-x-2"
                         >
                             <div class="dot" />
                             <a-tooltip
@@ -289,7 +291,7 @@
                         <div class="flex items-center">
                             <div
                                 v-if="categories(item)?.length > 0"
-                                class="flex items-center mr-3 text-sm text-gray-500 gap-x-1"
+                                class="flex items-center mr-3 text-sm text-gray-500  gap-x-1"
                             >
                                 in
                                 <div
@@ -328,7 +330,7 @@
                             </div>
                             <div
                                 v-if="parentCategory(item)"
-                                class="flex items-center mr-3 text-sm text-gray-500 gap-x-1"
+                                class="flex items-center mr-3 text-sm text-gray-500  gap-x-1"
                             >
                                 in
                                 <div
@@ -574,7 +576,7 @@
                     <div class="flex items-center overflow-hidden">
                         <router-link
                             :to="assetURL(item)"
-                            class="flex-shrink mb-0 mr-1 font-bold truncate cursor-pointer text-md text-primary hover:underline max-90"
+                            class="flex-shrink mb-0 mr-1 font-bold truncate cursor-pointer  text-md text-primary hover:underline max-90"
                         >
                             {{ title(item) }}
                         </router-link>
@@ -833,10 +835,10 @@
             stroke: #5277d7;
         }
     }
-    .max-40{
+    .max-40 {
         max-width: 40%;
     }
-    .max-90{
+    .max-90 {
         max-width: 90%;
     }
 </style>

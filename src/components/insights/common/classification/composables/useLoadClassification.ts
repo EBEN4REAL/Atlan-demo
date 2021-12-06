6bb87947159c1e7e3e3c8545b55759428dc66c93
Milek Agrawal <milek@atlan.com>
2021-12-07 05:16:16 +0530

    Add Aggregation in classification filter

---
+++ src/components/insights/common/classification/composables/useLoadClassification.ts
@@ -0,0 +1,115 @@
+import { Ref, ref } from 'vue'
+
+import { useAPIPromise } from '~/services/api/useAPIPromise'
+import {map} from '~/services/meta/search/key'
+
+import {
+    InternalAttributes,
+    BasicSearchAttributes,
+    SavedQueryAttributes,
+} from '~/constant/projection'
+
+interface useLoadClassificationProps {
+    connector: Ref<string | undefined>,
+}
+
+const useLoadClassification = ({
+    connector,
+}: useLoadClassificationProps) => {
+
+    const attributes = [
+        'name',
+        'displayName',
+        'typeName',
+        'dataType',
+        'description',
+        'userDescription',
+        'certificateStatus',
+        'ownerUsers',
+        'ownerGroups',
+        'classifications',
+
+        'connectorName',
+        'connectionId',
+        'connectionQualifiedName',
+        'parentFolderQualifiedName',
+        'defaultSchemaQualifiedName',
+        'defaultDatabaseQualifiedName',
+        'parentFolder',
+        'columns', //TODO: queries
+        'folder',
+        'compiledQuery',
+        'rawQuery',
+        ...InternalAttributes,
+        ...BasicSearchAttributes,
+        ...SavedQueryAttributes,
+    ]
+    const body = ref()
+
+    const refreshBody = () => {
+        body.value = {
+            dsl: {
+                query: {
+                    bool: {
+                        must: [
+                            {
+                                term: {
+                                    "__state": "ACTIVE"
+                                }
+                            }
+                        ]
+                    }
+                },
+                aggs: {
+                    group_by_direct_classifications: {
+                        terms: {
+                            field: "__traitNames",
+                            size: 50
+                        }
+                    },
+                    group_by_propagated_classifications: {
+                        terms: {
+                            field: "__propagatedTraitNames",
+                            size: 50
+                        }
+                    }
+                }
+            },
+            attributes
+        }
+
+        if (connector.value) {
+
+            body.value.dsl.query.bool.must.push(
+                {
+                    term: {
+                        "connectionName": `${connector.value}`
+                    }
+                }
+            )
+        }
+    }
+
+    refreshBody()
+
+    const getAssetCountOnClassification = (type) => {
+        refreshBody()
+        body.value.dsl.query.bool.must.push(
+            {
+                term: {
+                    "__typeName.keyword": type
+                }
+            }
+        )
+        return useAPIPromise(map.INDEX_SEARCH(), 'POST', {
+            body
+        })
+    }
+
+
+    return {
+        getAssetCountOnClassification
+    }
+}
+
+export default useLoadClassification

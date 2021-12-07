9f1f68d9fae4262b4328eb6a6cd663848d9f51c9
Tanya Chawla <tanya@ATMAC091.local>
2021-12-07 22:59:11 +0530

    wip expiry dropdown

--- src/components/admin/apikeys/apiKeyDrawer.vue
+++ src/components/admin/apikeys/apiKeyDrawer.vue
@@ -105,14 +105,123 @@
                         </template>
                     </a-popover>
                 </div>
-                <div class="mb-4">
-                    <div class="mb-2 mr-2 text-gray-500">Validity</div>
-                    <a-date-picker
-                        v-model:value="validityDate"
-                        format="YYYY-MM-DD"
-                        :allowClear="true"
-                        :disabled-date="disabledDate"
-                    ></a-date-picker>
+                <div class="mb-4" v-if="!apiKeyDirty.id">
+                    <div class="mb-2 mr-2 text-gray-500">Expiry</div>
+                    <a-dropdown :trigger="['click']">
+                        <a-button
+                            class="rounded focus:ring-2 focus:border-primary"
+                            style="min-width: 168px"
+                        >
+                            <div class="flex items-center">
+                                <div class="flex flex-1">
+                                    {{
+                                        validityOptions.find(
+                                            (v) => v.value === validity
+                                        )
+                                            ? validityOptions.find(
+                                                  (v) => v.value === validity
+                                              ).label
+                                            : 'Custom'
+                                    }}
+                                </div>
+                                <div class="ml-4 text-gray-700">
+                                    <AtlanIcon
+                                        icon="ChevronDown"
+                                        class="h-4 -mt-0.5 -ml-0.5"
+                                    />
+                                </div>
+                            </div>
+                        </a-button>
+                        <template #overlay>
+                            <a-menu>
+                                <a-menu-item key="0" class="hover:bg-white">
+                                    <a-radio-group
+                                        v-model:value="validity"
+                                        class="w-full"
+                                    >
+                                        <div
+                                            class="
+                                                flex flex-col
+                                                w-full
+                                                px-1
+                                                text-sm text-gray-700
+                                                hover:text-primary
+                                            "
+                                        >
+                                            <template
+                                                v-for="item in validityOptions"
+                                                :key="item.label"
+                                            >
+                                                <div class="w-full px-0 py-1">
+                                                    <a-radio
+                                                        :value="item.value"
+                                                        @click="
+                                                            showDatePicker = false
+                                                        "
+                                                    >
+                                                        <div>
+                                                            {{ item.label }}
+                                                        </div>
+                                                    </a-radio>
+                                                </div>
+                                            </template>
+                                            <div
+                                                @click.stop="() => {}"
+                                                class="
+                                                    flex flex-col
+                                                    items-center
+                                                    py-2
+                                                    pb-0.5
+                                                    border-t border-300
+                                                    hover:text-primary
+                                                "
+                                            >
+                                                <div
+                                                    class="
+                                                        flex
+                                                        items-center
+                                                        justify-start
+                                                        w-full
+                                                        mb-2
+                                                    "
+                                                >
+                                                    <a-radio
+                                                        @click="
+                                                            showDatePicker = true
+                                                        "
+                                                        :value="'custom'"
+                                                        >Custom</a-radio
+                                                    >
+                                                </div>
+                                                <div
+                                                    :class="
+                                                        showDatePicker
+                                                            ? ''
+                                                            : 'hidden'
+                                                    "
+                                                >
+                                                    <a-date-picker
+                                                        v-model:value="
+                                                            validityDate
+                                                        "
+                                                        format="YYYY-MM-DD"
+                                                        :allowClear="true"
+                                                        :disabled-date="
+                                                            disabledDate
+                                                        "
+                                                    ></a-date-picker>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </a-radio-group>
+                                </a-menu-item>
+                            </a-menu>
+                        </template>
+                    </a-dropdown>
+                </div>
+                <div v-else>
+                    <div class="mb-2 mr-2 text-gray-500">Expiry</div>
+                    <div>{{ apiKeyExpiryDate }}</div>
                 </div>
             </div>
             <div
@@ -320,10 +429,26 @@ export default defineComponent({
         const addPersonaPopoverVisible = ref(false)
         const isDeletePopoverVisible = ref(false)
         const { generatedAPIKey } = useVModels(props, emit)
-        const validityDate = ref('')
+        const validityDate = ref(dayjs())
         const showDeletePopover = () => {
             isDeletePopoverVisible.value = true
         }
+        const showDatePicker = ref(false)
+        const validity = ref('')
+        const validityOptions = ref([
+            {
+                label: 'Never',
+                value: 'never',
+            },
+            {
+                label: '1 Week',
+                value: 'one-week',
+            },
+            {
+                label: '1 Month',
+                value: 'one-month',
+            },
+        ])
         watch(
             () => props.deleteAPIKeyLoading,
             (newVal, prevVal) => {
@@ -337,7 +462,18 @@ export default defineComponent({
                 nameEmptyOnSubmit.value = true
                 return
             }
-            //calculate validity seconds from validityDate
+            //calculate validity seconds from validityDate in case of custom
+            if (validity.value === 'never') {
+                const validityUnixEpoch =
+                    dayjs().unix() + DEFAULT_VALIDITY_IN_SECONDS
+                // getting dayjs obj from the calculated unix epoch to pass in datepicker
+                validityDate.value = dayjs.unix(validityUnixEpoch)
+            } else if (validity.value === 'one-week') {
+                validityDate.value = dayjs().add(1, 'week')
+            } else if (validity.value === 'one-month') {
+                validityDate.value = dayjs().add(1, 'month')
+            }
+            // if custom, make no changes to validity date
             const modifiedDate = validityDate.value.endOf('day')
             const validityInSeconds = modifiedDate.diff(dayjs(), 's')
             if (props?.apiKey?.id)
@@ -369,17 +505,39 @@ export default defineComponent({
                     props?.apiKey?.validitySeconds
                 ) {
                     // adding validity in seconds to created timestamp unix epoch to find the date till which the api key is valid
-                    const validityUnixEpoch =
-                        dayjs(
-                            props?.apiKey?.rawKey?.attributes?.createdAt
-                        ).unix() + parseInt(props.apiKey.validitySeconds)
-                    // getting dayjs obj from the calculated unix epoch to pass in datepicker
-                    validityDate.value = dayjs.unix(validityUnixEpoch)
+                    // const validityUnixEpoch =
+                    //     dayjs(
+                    //         props?.apiKey?.rawKey?.attributes?.createdAt
+                    //     ).unix() + parseInt(props.apiKey.validitySeconds)
+                    // // getting dayjs obj from the calculated unix epoch to pass in datepicker
+                    // validityDate.value = dayjs.unix(validityUnixEpoch)
+                    // if (
+                    //     validityDate.value.diff(
+                    //         dayjs(props?.apiKey?.rawKey?.attributes?.createdAt),
+                    //         'week'
+                    //     ) === 1
+                    // ) {
+                    //     validity.value = 'one-week'
+                    // } else if (
+                    //     validityDate.value.diff(
+                    //         dayjs(props?.apiKey?.rawKey?.attributes?.createdAt),
+                    //         'month'
+                    //     ) === 1
+                    // ) {
+                    //     validity.value = 'one-month'
+                    // } else if (
+                    //     parseInt(props.apiKey.validitySeconds) <
+                    //     DEFAULT_VALIDITY_IN_SECONDS
+                    // ) {
+                    //     validity.value = 'custom'
+                    //     showDatePicker.value = true
+                    // } else validity.value = 'never'
                 } else {
                     const validityUnixEpoch =
                         dayjs().unix() + DEFAULT_VALIDITY_IN_SECONDS
                     // getting dayjs obj from the calculated unix epoch to pass in datepicker
                     validityDate.value = dayjs.unix(validityUnixEpoch)
+                    validity.value = 'never'
                 }
             },
             { immediate: true, deep: true }
@@ -398,7 +556,6 @@ export default defineComponent({
                     },
                     'en-GB'
                 )
-                console.log(createDate)
                 const data = generatedAPIKey.value.attributes.accessToken
                 const filename = `${generatedAPIKey.value.attributes.displayName}_${createDate}_atlan api key.txt`
                 const type = 'text/plain'
@@ -440,6 +597,9 @@ export default defineComponent({
             validityDate,
             disabledDate,
             dayjs,
+            validityOptions,
+            validity,
+            showDatePicker,
         }
     },
 })

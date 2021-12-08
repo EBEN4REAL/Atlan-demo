5995d2fc104fb4aad4cbed56dd9cc8fb01015bb4
shivanshpahwa24 <shivanshpahwa24@gmail.com>
2021-12-08 20:27:50 +0530

    handle drawer open only on first load

--- src/components/common/assets/profile/tabs/columns/columnWidget.vue
+++ src/components/common/assets/profile/tabs/columns/columnWidget.vue
@@ -127,9 +127,9 @@
                 padding="compact"
             >
                 {{ pagination.current }} of
-                <span v-if="Math.ceil(pagination.total)">{{
-                    Math.ceil(pagination.total)
-                }}</span>
+                <span v-if="Math.ceil(pagination.total)"
+                    >{{ Math.ceil(pagination.total) }}
+                </span>
 
                 <div
                     v-else-if="isValidating"
@@ -210,6 +210,8 @@
             const columnsList: Ref<assetInterface[]> = ref([])
             const columnFromUrl: Ref<assetInterface[]> = ref([])
 
+            const openDrawerOnLoad = ref<boolean>(false)
+
             const {
                 selectedAsset,
                 certificateStatus,
@@ -258,6 +260,7 @@
 
             const {
                 freshList: list,
+                list: combinedList,
                 isLoading,
                 quickChange,
                 totalCount,
@@ -396,7 +399,7 @@
                         })
                         const fetchKey = computed(() => {
                             if (
-                                list.value.some(
+                                combinedList.value.some(
                                     (item) => item.guid === column.value
                                 )
                             ) {
@@ -418,32 +421,30 @@
                         watch([urlColumnList], () => {
                             columnFromUrl.value = urlColumnList.value
                             filterColumnsList()
-
-                            columnsList.value?.forEach((singleRow) => {
-                                if (singleRow.guid === column.value) {
-                                    openColumnSidebar(
-                                        singleRow.attributes.order
-                                    )
-                                }
-                            })
-
-                            nextTick(() => {
-                                scrollToElement()
-                            })
                         })
+                        if (fetchKey.value === null) {
+                            filterColumnsList()
+                        }
+                    } else {
                         filterColumnsList()
+                    }
+                }
+            )
 
+            watch(
+                () => [...columnsList.value],
+                () => {
+                    if (!openDrawerOnLoad.value) {
                         columnsList.value?.forEach((singleRow) => {
                             if (singleRow.guid === column.value) {
                                 openColumnSidebar(singleRow.attributes.order)
                             }
                         })
+                        openDrawerOnLoad.value = true
 
                         nextTick(() => {
                             scrollToElement()
                         })
-                    } else {
-                        filterColumnsList()
                     }
                 }
             )

1e62eeb0d09c66caca5da53d2db71656b4ed2857
Milek Agrawal <milek@atlan.com>
2021-12-10 01:40:06 +0530

    add vqb column picker, filter selector

---
+++ src/components/insights/playground/editor/vqb/panels/filter/filterSelector/index.vue
@@ -0,0 +1,283 @@
+<template>
+    <div
+        ref="container"
+        @click="setFocus"
+        @focusout="handleContainerBlur"
+        @mouseover="handleMouseOver"
+        @mouseout="handleMouseOut"
+        tabindex="0"
+        class="relative flex items-center group"
+        :class="[
+            isAreaFocused
+                ? ' border-primary-focus border-2 '
+                : 'border-gray-300 border border-plus',
+            ,
+            'flex flex-wrap items-center  rounded box-shadow selector-height px-3',
+            !columnName ? ' cursor-not-allowed disable-bg' : '',
+        ]"
+        @click.stop="() => {}"
+    >
+        <template v-if="selectedFilter?.name">
+            <!-- chips -->
+            <div class="flex items-center">
+                <span class="mb-0 ml-1 text-sm text-gray-700">
+                    {{ filterName }}
+                </span>
+            </div>
+        </template>
+
+        <a-input
+            v-if="!selectedFilter?.name"
+            :disabled="!columnName"
+            :placeholder="placeholder"
+            :contenteditable="false"
+            :class="[
+                'w-full p-0  border-none shadow-none outline-none text-sm  focus-none',
+                !columnName ? $style.custom_input : '',
+            ]"
+        />
+        <div class="absolute right-2">
+            <AtlanIcon icon="ChevronDown" class="w-4 h-4" />
+        </div>
+        <div
+            v-if="isAreaFocused"
+            @click.stop="() => {}"
+            :style="`width: 100%;top:${topPosShift}px`"
+            :class="[
+                'absolute z-10 pb-2 overflow-auto bg-white rounded custom-shadow position',
+            ]"
+        >
+            <div
+                :class="['flex  justify-center overflow-auto']"
+                style="max-height: 250px"
+            >
+                <div class="w-full">
+                    <template
+                        v-for="(item, index) in dropdownOption"
+                        :key="item.value + index"
+                        v-if="dropdownOption?.length !== 0"
+                    >
+                        <div
+                            class="flex items-center justify-between w-full px-4 h-9 hover:bg-primary-light"
+                            @click="onCheckChange(item)"
+                            :class="
+                                selectedFilter.name === item.key
+                                    ? 'bg-primary-light'
+                                    : 'bg-white'
+                            "
+                        >
+                            <span>{{ item.label }}</span>
+                            <AtlanIcon
+                                icon="Check"
+                                class="text-primary"
+                                v-if="selectedFilter.name === item.key"
+                            />
+                        </div>
+                    </template>
+
+                    <span
+                        class="flex items-center justify-center w-full mt-4 text-sm text-center text-gray-400"
+                        v-if="dropdownOption.length == 0"
+                    >
+                        No functions found!
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import {
+        computed,
+        watch,
+        defineComponent,
+        ref,
+        nextTick,
+        onMounted,
+        inject,
+        PropType,
+        ComputedRef,
+        toRefs,
+    } from 'vue'
+    import { useFilter } from '~/components/insights/playground/editor/vqb/composables/useFilter'
+    import { useVModels } from '@vueuse/core'
+
+    // import useBody from './useBody'
+
+    export default defineComponent({
+        name: 'Sub panel',
+        components: {},
+        // emits: ['checkChange', 'filterChange'],
+        props: {
+            selectedFilter: {
+                type: Object,
+                required: true,
+            },
+            columnName: {
+                type: String,
+                required: true,
+            },
+            columnType: {
+                type: String,
+                required: true,
+            },
+        },
+
+        setup(props, { emit }) {
+            const { columnName, columnType } = toRefs(props)
+
+            const { selectedFilter } = useVModels(props)
+
+            const { filterList } = useFilter()
+
+            const inputRef = ref()
+            const selectAll = ref(false)
+            const topPosShift = ref(0)
+            const isAreaFocused = ref(false)
+            const container = ref()
+            const clickPos = ref({ left: 0, top: 0 })
+            const setFocus = () => {
+                if (!columnType.value) return
+                // inputChange()
+                isAreaFocused.value = true
+                // nextTick(() => {
+                //     if (columnType.value) inputRef?.value?.focus()
+                // })
+            }
+
+            const handleContainerBlur = (event) => {
+                // if the blur was because of outside focus
+                // currentTarget is the parent element, relatedTarget is the clicked element
+                if (!container.value.contains(event.relatedTarget)) {
+                    isAreaFocused.value = false
+                }
+            }
+
+            const inputChange = () => {
+                console.log('called')
+                if (topPosShift.value !== container.value?.offsetHeight) {
+                    topPosShift.value = container.value?.offsetHeight
+                }
+            }
+
+            let filtersList = ref({})
+            watch(
+                [columnType],
+                () => {
+                    // console.log('filters:', {
+                    //     type: columnType,
+                    //     filtersList,
+                    // })
+                    filtersList.value = filterList(columnType.value)
+                },
+                {
+                    immediate: true,
+                }
+            )
+            const placeholder = computed(() => {
+                if (columnName.value) {
+                    return `Select filter function`
+                }
+                return `Select a column first`
+            })
+            // const totalCount = computed(
+            //     () => aggregationList.value?.length || 0
+            // )
+
+            const dropdownOption = computed(() => {
+                let data = filtersList?.value?.length
+                    ? filtersList.value[0].functions.map((ls) => ({
+                          label: ls.name,
+                          value: ls.name,
+                          key: ls.key,
+                          type: ls.type,
+                          name: ls.name,
+                      }))
+                    : []
+                data.sort((x, y) => {
+                    if (x.label < y.label) return -1
+                    if (x.label > y.label) return 1
+                    return 0
+                })
+                return data
+            })
+
+            let filterName = ref('')
+            const onCheckChange = (checked) => {
+                // inputChange()
+                // console.log(checked)
+                selectedFilter.value = {
+                    ...selectedFilter.value,
+                    name: checked.key,
+                    type: checked.type,
+                    value: checked.value,
+                }
+                filterName.value = checked.name
+            }
+
+            onMounted(() => {
+                topPosShift.value = container.value?.offsetHeight
+                console.log(container.value)
+            })
+
+            const mouseOver = ref(false)
+            const handleMouseOver = () => {
+                if (!mouseOver.value) mouseOver.value = true
+            }
+            const handleMouseOut = () => {
+                if (mouseOver.value) mouseOver.value = false
+            }
+
+            return {
+                onCheckChange,
+                selectAll,
+                dropdownOption,
+                selectedFilter,
+                placeholder,
+                inputChange,
+                topPosShift,
+                inputRef,
+                clickPos,
+                container,
+                handleContainerBlur,
+                filterName,
+                setFocus,
+                isAreaFocused,
+                columnName,
+                columnType,
+                handleMouseOver,
+                handleMouseOut,
+                mouseOver,
+            }
+        },
+    })
+</script>
+<style lang="less" scoped>
+    .border-plus {
+        padding: 1px;
+    }
+    .border-minus {
+        padding: 0px;
+    }
+    .custom-shadow {
+        box-shadow: 0 2px 8px rgb(0 0 0 / 15%);
+    }
+    .selector-height {
+        min-height: 32px;
+    }
+    .position {
+        @apply right-0;
+    }
+    .box-shadow {
+        box-shadow: 0px 2px 5px 1px rgba(0, 0, 0, 0.05);
+    }
+    .disable-bg {
+        background-color: #fbfbfb;
+    }
+</style>
+<style lang="less" module>
+    .custom_input {
+        background-color: #fbfbfb !important;
+    }
+</style>
---
+++ src/components/insights/playground/editor/vqb/panels/filter/subpanel/index.vue
@@ -0,0 +1,168 @@
+<template>
+    <div :class="[' mx-3 mt-1 mb-4']">
+        <div class="">
+            <template
+                v-for="(subpanel, index) in subpanels"
+                :key="subpanel?.id + index"
+            >
+                <div
+                    class="flex items-center w-full mb-3"
+                    @mouseover="hoverItem = subpanel.id"
+                    @mouseout="hoverItem = null"
+                >
+                    <ColumnSelector
+                        style="width: 30%"
+                        v-model:selectedItem="subpanel.column"
+                        :tableQualfiedName="
+                            columnSubpanels[0]?.tableQualfiedName
+                        "
+                        @change="(val) => handleColumnChange(val, index)"
+                    />
+
+                    <FilterSelector
+                        class="ml-6"
+                        style="width: 30%"
+                        :columnName="subpanel?.column?.label"
+                        :columnType="subpanel?.column?.type"
+                        v-model:selectedFilter="subpanel.filter"
+                    />
+
+                    <AtlanIcon
+                        @click.stop="() => handleDelete(index)"
+                        icon="Close"
+                        class="w-6 h-6 ml-3 text-gray-500 mt-0.5 cursor-pointer"
+                        :class="`opacity-${
+                            hoverItem === subpanel.id ? 100 : 0
+                        }`"
+                    />
+                </div>
+            </template>
+        </div>
+
+        <span>
+            <div
+                class="items-center mt-3 cursor-pointer text-primary"
+                @click.stop="handleAddPanel"
+            >
+                <AtlanIcon icon="Add" class="w-4 h-4 mr-1 -mt-0.5" />
+                <span>Add another</span>
+            </div>
+        </span>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent, ref, watch, PropType, toRaw } from 'vue'
+    // import Pill from '~/components/UI/pill/pill.vue'
+    // import { useColumn } from '~/components/insights/playground/editor/vqb/composables/useColumn'
+    import FilterSelector from '../filterSelector/index.vue'
+    import { SubpanelColumn } from '~/types/insights/VQBPanelColumns.interface'
+    import { SubpanelFilter } from '~/types/insights/VQBPanelFilter.interface'
+    import { generateUUID } from '~/utils/helper/generator'
+    import { useVModels } from '@vueuse/core'
+    // import ColumnSelector from '../columnSelector/index.vue'
+    import ColumnSelector from '../../common/columnSelector/index.vue'
+
+    export default defineComponent({
+        name: 'Sub panel',
+        components: {
+            FilterSelector,
+            ColumnSelector,
+        },
+        props: {
+            expand: {
+                type: Boolean,
+                required: true,
+                default: false,
+            },
+            subpanels: {
+                type: Object as PropType<SubpanelFilter[]>,
+                required: true,
+                default: [],
+            },
+            columnSubpanels: {
+                type: Object as PropType<SubpanelColumn[]>,
+                required: true,
+                default: [],
+            },
+        },
+
+        setup(props, { emit }) {
+            const selectedAggregates = ref([])
+            const selectedColumn = ref({})
+
+            const { subpanels, columnSubpanels } = useVModels(props)
+            const columnName = ref('Hello World')
+            const columnType = ref('char')
+
+            watch(columnName, () => {
+                if (!columnName.value) {
+                    selectedAggregates.value = []
+                }
+            })
+
+            const checkChange = (checkedArr: string[]) => {
+                console.log('checked array: ', checkedArr)
+            }
+
+            const handleColumnChange = (val, index) => {
+                console.log('col change: ', val)
+
+                const copySubPanel = JSON.parse(
+                    JSON.stringify(toRaw(subpanels.value[0]))
+                )
+                copySubPanel.column = val
+                copySubPanel.filter = {}
+
+                subpanels.value[index] = copySubPanel
+                console.log(subpanels.value)
+            }
+
+            const handleAddPanel = () => {
+                const copySubPanels: SubpanelFilter[] = JSON.parse(
+                    JSON.stringify(toRaw(subpanels.value))
+                )
+                let uuid = generateUUID()
+                copySubPanels.push({
+                    id: uuid,
+                    column: {},
+                    filter: {},
+                })
+                subpanels.value = copySubPanels
+
+                // console.log('subpanels: ', copySubPanels)
+            }
+            const handleDelete = (index) => {
+                subpanels.value.splice(index, 1)
+            }
+
+            const changeColumn = (column) => {
+                console.log('columns: ', column)
+            }
+
+            let hoverItem = ref(null)
+
+            return {
+                selectedAggregates,
+                columnName,
+                columnType,
+                checkChange,
+                handleAddPanel,
+                handleDelete,
+                handleColumnChange,
+                columnSubpanels,
+                selectedColumn,
+                changeColumn,
+                hoverItem,
+            }
+        },
+    })
+</script>
+<style lang="less" scoped>
+    .border-shift-plus {
+        padding: 1px;
+    }
+    .border-shift-minus {
+        padding: 0px;
+    }
+</style>

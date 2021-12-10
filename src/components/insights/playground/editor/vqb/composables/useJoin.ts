25582dbd7b4c5e09cf38c0b23782399bd30b77c8
Milek Agrawal <milek@atlan.com>
2021-12-10 20:46:10 +0530

    add join interface and list

---
+++ src/components/insights/playground/editor/vqb/composables/useJoin.ts
@@ -0,0 +1,24 @@
+export function useJoin() {
+
+    let list = [
+        {
+            type: 'inner_join',
+            name: 'Inner Join'
+        },
+        {
+            type: 'outer_join',
+            name: 'Outer Join'
+        },
+        {
+            type: 'left_join',
+            name: 'Left Join'
+        }, 
+        {
+            type: 'right_join',
+            name: 'Right Join'
+        }
+    ]
+
+    return {list};
+    
+}

c9408626508953f7066e178b48cad9c4f3299b65
Milek Agrawal <milek@atlan.com>
2021-12-10 20:46:40 +0530

    add joins selector

---
+++ src/components/insights/playground/editor/vqb/panels/join/subpanel/index.vue
@@ -0,0 +1,151 @@
+<template>
+    <div :class="[' mx-3 mt-1 mb-4']">
+        <div class="">
+            <template
+                v-for="(subpanel, index) in subpanels"
+                :key="subpanel?.id + index"
+            >
+                <div
+                    class="flex items-center w-full mb-3"
+                    @mouseover="hoverItem = subpanel.id"
+                    @mouseout="hoverItem = null"
+                >
+                    <JoinSelector
+                        style="width: 300px"
+                        v-model:selectedJoinType="subpanel.joinType"
+                    />
+
+                    <AtlanIcon
+                        @click.stop="() => handleDelete(index)"
+                        icon="Close"
+                        class="w-6 h-6 text-gray-500 mt-0.5 cursor-pointer ml-auto"
+                        :class="`opacity-${
+                            hoverItem === subpanel.id ? 100 : 0
+                        }`"
+                    />
+                </div>
+            </template>
+        </div>
+
+        <span>
+            <div
+                class="items-center mt-3 cursor-pointer text-primary"
+                @click.stop="handleAddPanel"
+            >
+                <AtlanIcon icon="Add" class="w-4 h-4 mr-1 -mt-0.5" />
+                <span>Add condition</span>
+            </div>
+        </span>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent, ref, watch, PropType, toRaw } from 'vue'
+    // import Pill from '~/components/UI/pill/pill.vue'
+    // import { useColumn } from '~/components/insights/playground/editor/vqb/composables/useColumn'
+    import JoinSelector from '../joinSelector/index.vue'
+    import { SubpanelJoin } from '~/types/insights/VQBPanelJoins.interface'
+    import { generateUUID } from '~/utils/helper/generator'
+    import { useVModels } from '@vueuse/core'
+    // import ColumnSelector from '../columnSelector/index.vue'
+
+    export default defineComponent({
+        name: 'Sub panel',
+        components: {
+            JoinSelector,
+        },
+        props: {
+            expand: {
+                type: Boolean,
+                required: true,
+                default: false,
+            },
+            subpanels: {
+                type: Object as PropType<SubpanelJoin[]>,
+                required: true,
+                default: [],
+            },
+        },
+
+        setup(props, { emit }) {
+            const selectedAggregates = ref([])
+            const selectedColumn = ref({})
+
+            const { subpanels } = useVModels(props)
+            const columnName = ref('Hello World')
+            const columnType = ref('char')
+
+            watch(columnName, () => {
+                if (!columnName.value) {
+                    selectedAggregates.value = []
+                }
+            })
+
+            const checkChange = (checkedArr: string[]) => {
+                console.log('checked array: ', checkedArr)
+            }
+
+            const handleColumnChange = (val, index) => {
+                console.log('col change: ', val)
+
+                const copySubPanel = JSON.parse(
+                    JSON.stringify(toRaw(subpanels.value[index]))
+                )
+                copySubPanel.column = val
+                // copySubPanel.filter = {}
+
+                subpanels.value[index] = copySubPanel
+                // console.log(subpanels.value)
+            }
+
+            const handleAddPanel = () => {
+                const copySubPanels: SubpanelJoin[] = JSON.parse(
+                    JSON.stringify(toRaw(subpanels.value))
+                )
+                let uuid = generateUUID()
+                copySubPanels.push({
+                    id: uuid,
+                    columnsDataLeft: {},
+                    columnsDataRight: {},
+                    joinType: {
+                        type: 'inner_join',
+                        name: 'Inner Join',
+                    },
+                })
+                subpanels.value = copySubPanels
+
+                // console.log('subpanels: ', copySubPanels)
+            }
+            const handleDelete = (index) => {
+                subpanels.value.splice(index, 1)
+            }
+
+            const changeColumn = (column) => {
+                console.log('columns: ', column)
+            }
+
+            let hoverItem = ref(null)
+
+            return {
+                selectedAggregates,
+                columnName,
+                columnType,
+                checkChange,
+                handleAddPanel,
+                handleDelete,
+                handleColumnChange,
+                selectedColumn,
+                changeColumn,
+                hoverItem,
+            }
+        },
+    })
+</script>
+<style lang="less" scoped>
+    .border-shift-plus {
+        padding: 1px;
+    }
+    .border-shift-minus {
+        padding: 0px;
+    }
+</style>

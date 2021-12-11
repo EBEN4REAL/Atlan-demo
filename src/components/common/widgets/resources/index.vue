45097e0deb9810a7f7bb4df8d057c8a11aee5d75
shivanshpahwa24 <shivanshpahwa24@gmail.com>
2021-12-12 01:24:39 +0530

    menu options for resources added

--- src/components/common/widgets/resources/index.vue
+++ src/components/common/widgets/resources/index.vue
@@ -8,7 +8,7 @@
                 :asset="selectedAsset"
                 ><template #trigger>
                     <a-button
-                        class="text-gray-500 border border-transparent rounded shadow-none  hover:border-gray-400"
+                        class="text-gray-500 border border-transparent rounded shadow-none hover:border-gray-400"
                     >
                         <AtlanIcon icon="Add" /> </a-button
                 ></template>
@@ -65,102 +65,102 @@
 </template>
 
 <script lang="ts">
-// Vue
-import dayjs from 'dayjs'
-import relativeTime from 'dayjs/plugin/relativeTime'
+    // Vue
+    import dayjs from 'dayjs'
+    import relativeTime from 'dayjs/plugin/relativeTime'
 
-import {
-    defineComponent,
-    PropType,
-    computed,
-    toRefs,
-    defineAsyncComponent,
-} from 'vue'
-import { assetInterface } from '~/types/assets/asset.interface'
-import AddResources from './addResource.vue'
-import useAssetInfo from '~/composables/discovery/useAssetInfo'
-import AtlanButton from '~/components/UI/button.vue'
-import integrationStore from '~/store/integrations/index'
-import AtlanIcon from '../../icon/atlanIcon.vue'
-import SlackUserLoginTrigger from '@common/integrations/slack/slackUserLoginTriggerCard.vue'
-import {
-    isSlackLink,
-    getChannelAndMessageIdFromSlackLink,
-} from '~/composables/integrations/useSlack'
-import { UnfurlSlackMessage } from '~/composables/integrations/useIntegrations'
+    import {
+        defineComponent,
+        PropType,
+        computed,
+        toRefs,
+        defineAsyncComponent,
+    } from 'vue'
+    import { assetInterface } from '~/types/assets/asset.interface'
+    import AddResources from './addResource.vue'
+    import useAssetInfo from '~/composables/discovery/useAssetInfo'
+    import AtlanButton from '~/components/UI/button.vue'
+    import integrationStore from '~/store/integrations/index'
+    import AtlanIcon from '../../icon/atlanIcon.vue'
+    import SlackUserLoginTrigger from '@common/integrations/slack/slackUserLoginTriggerCard.vue'
+    import {
+        isSlackLink,
+        getChannelAndMessageIdFromSlackLink,
+    } from '~/composables/integrations/useSlack'
+    import { UnfurlSlackMessage } from '~/composables/integrations/useIntegrations'
 
-dayjs.extend(relativeTime)
+    dayjs.extend(relativeTime)
 
-export default defineComponent({
-    components: {
-        SlackUserLoginTrigger,
-        AddResources,
-        AtlanButton,
-        AtlanIcon,
-        slackLinkPreview: defineAsyncComponent(
-            () => import('./previews/slackLinkPreviewCard.vue')
-        ),
-        linkPreview: defineAsyncComponent(
-            () => import('./previews/linkPreviewCard.vue')
-        ),
-    },
-    props: {
-        selectedAsset: {
-            type: Object as PropType<assetInterface>,
-            required: true,
+    export default defineComponent({
+        components: {
+            SlackUserLoginTrigger,
+            AddResources,
+            AtlanButton,
+            AtlanIcon,
+            slackLinkPreview: defineAsyncComponent(
+                () => import('./previews/slackLinkPreviewCard.vue')
+            ),
+            linkPreview: defineAsyncComponent(
+                () => import('./previews/linkPreviewCard.vue')
+            ),
         },
-    },
-    setup(props) {
-        const timeAgo = (time: string) => dayjs().from(time, true)
-        const { links } = useAssetInfo()
-        const hasUserLevelSlackIntegration = true
-        const hasTenantLevelSlackIntegration = true
-        console.log('links', links)
+        props: {
+            selectedAsset: {
+                type: Object as PropType<assetInterface>,
+                required: true,
+            },
+        },
+        setup(props) {
+            const timeAgo = (time: string) => dayjs().from(time, true)
+            const { links } = useAssetInfo()
+            const hasUserLevelSlackIntegration = true
+            const hasTenantLevelSlackIntegration = true
+            console.log('links', links)
 
-        function getPreviewComponent(url) {
-            if (
-                isSlackLink(url) &&
-                hasUserLevelSlackIntegration &&
-                hasTenantLevelSlackIntegration
-            ) {
-                return 'slackLinkPreview'
+            function getPreviewComponent(url) {
+                if (
+                    isSlackLink(url) &&
+                    hasUserLevelSlackIntegration &&
+                    hasTenantLevelSlackIntegration
+                ) {
+                    return 'slackLinkPreview'
+                }
+                return 'linkPreview'
             }
-            return 'linkPreview'
-        }
 
-        const { selectedAsset } = toRefs(props)
+            const { selectedAsset } = toRefs(props)
 
-        const hasAtleastOneSlackLink = computed(() => {
-            const linkArr = links(selectedAsset.value)
-            const slackLink = linkArr.some((link) =>
-                isSlackLink(link?.attributes?.link)
-            )
-            return slackLink
-        })
+            const hasAtleastOneSlackLink = computed(() => {
+                const linkArr = links(selectedAsset.value)
+                const slackLink = linkArr.some((link) =>
+                    isSlackLink(link?.attributes?.link)
+                )
+                return slackLink
+            })
 
-        return {
-            links,
-            hasAtleastOneSlackLink,
-            hasUserLevelSlackIntegration,
-            isSlackLink,
-            timeAgo,
-            getPreviewComponent,
-            hasTenantLevelSlackIntegration,
-        }
-    },
-})
+            return {
+                links,
+                hasAtleastOneSlackLink,
+                hasUserLevelSlackIntegration,
+                isSlackLink,
+                timeAgo,
+                getPreviewComponent,
+                hasTenantLevelSlackIntegration,
+            }
+        },
+    })
 </script>
 <style lang="less" scoped>
-.slack-icon-avatar-overlay {
-    height: 1rem;
-    bottom: -3px;
-    right: -6px;
-    border-radius: 100px;
-    /* box-shadow: -1px 1px 4px white; */
-    background: white;
-    padding: 0.9px;
-}
-.min-w-link-left-col {
-    min-width: 2rem;
-}
+    .slack-icon-avatar-overlay {
+        height: 1rem;
+        bottom: -3px;
+        right: -6px;
+        border-radius: 100px;
+        /* box-shadow: -1px 1px 4px white; */
+        background: white;
+        padding: 0.9px;
+    }
+    .min-w-link-left-col {
+        min-width: 2rem;
+    }
 </style>

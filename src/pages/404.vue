23e34e550744543a5f86bfac0d6aa9df039f6e04
Nobal <nobal@atlan.com>
2021-12-12 12:51:44 -0500

    added 404

---
+++ src/pages/404.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container flex flex-col h-full">
+        <h1>404</h1>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue'
+    import { useRouter } from 'vue-router'
+    export default defineComponent({
+    });
+</script>
+
+<style lang="less">
+    .container {
+        display: flex;
+        width: 100vw !important;
+        align-items: center;
+    }
+</style>
+
+<route lang="yaml">
+meta:
+    layout: default
+    requiresAuth: true
+</route>
+

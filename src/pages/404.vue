2f41ab91771714ceae86aae6a796bd4f33f43217
Nobal <nobal@atlan.com>
2021-12-12 13:01:19 -0500

    404 page style

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,6 +1,11 @@
 <template>
-    <div class="container flex flex-col h-full">
-        <h1>404</h1>
+    <div class="flex justify-center h-full">
+        <div class="max-w-md py-4 px-8 bg-white rounded-lg my-20">
+        <div>
+            <h2 class="text-gray-800 text-3xl font-semibold"><span class="text-primary">404</span> Page not found</h2>
+            <!--p class="mt-2 text-gray-600">Please check the URL in the adress bar and try again.</p-->
+        </div>
+</div>
     </div>
 </template>
 

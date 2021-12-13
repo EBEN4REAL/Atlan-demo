a0eb18e958044560f60460fdc5d4bb5d4a07ada2
alii13 <shekhaliul44@gmail.com>
2021-12-14 00:09:32 +0530

    feat: shared & private collection separated

--- src/components/insights/explorers/queries/collection/collectionSelector.vue
+++ src/components/insights/explorers/queries/collection/collectionSelector.vue
@@ -7,34 +7,75 @@
         overlayClassName="collectionSelectPopover"
     >
         <template #content>
-            <div class="flex flex-col w-full">
-                <div class="px-2 py-2">
-                    <div
-                        v-for="(collectionArr, index) in [
-                            sharedCollections,
-                            privateCollections,
-                        ]"
-                        :key="index"
-                        :class="{ 'mb-3': index === 0 }"
+            <div class="flex flex-col w-full pt-1 pb-3">
+                <div class="px-3">
+                    <a-input
+                        v-model:value="queryText"
+                        class="h-8 mt-2 rounded"
+                        :class="$style.inputSearch"
+                        :placeholder="`Search in ${
+                            sharedCollections?.length +
+                            privateCollections?.length
+                        } Collections`"
+                        :allowClear="true"
                     >
-                        <span
-                            v-if="collectionArr?.length"
-                            class="text-xs font-bold text-gray-500"
-                            >{{ index === 0 ? 'Shared' : 'Private' }}</span
-                        >
+                        <!-- <template #suffix>
+                            <AtlanIcon icon="Search" color="#6F7590" />
+                        </template> -->
+                    </a-input>
+                </div>
+                <!-- FIXME: These should be using a same component -->
+                <div class="px-3 pt-3">
+                    <span class="text-xs font-bold text-gray-500">{{
+                        `Shared (${sharedCollections?.length ?? 0})`
+                    }}</span>
+
+                    <div class="mb-3 overflow-x-hidden overflow-y-auto h-44">
                         <div
-                            v-for="collection in collectionArr"
+                            v-if="sharedCollections?.length !== 0"
+                            v-for="collection in sharedCollections"
                             :key="collection.guid"
-                            class="flex items-center p-1 cursor-pointer hover:bg-primary-light grou"
+                            class="flex items-center p-1 cursor-pointer hover:bg-primary-light"
                             @click="handleChange(collection.guid)"
                         >
                             <AtlanIcon
-                                v-if="index === 0"
                                 icon="Group"
                                 class="self-center w-4 h-4 pr-1"
                             ></AtlanIcon>
+
+                            <div
+                                class="overflow-ellipsis group-hover:text-primary"
+                            >
+                                {{ collection.attributes.name }}
+                            </div>
+                        </div>
+                        <div
+                            v-else
+                            class="flex flex-col items-center justify-center w-full h-full"
+                        >
                             <AtlanIcon
-                                v-else
+                                icon="EmptySearchQuery"
+                                class="h-24 mr-8 -mt-4 no-svaved-query-icon text-primary"
+                            />
+                            <span class="mt-3 text-gray-500"
+                                >No shared collection found</span
+                            >
+                        </div>
+                    </div>
+
+                    <span class="text-xs font-bold text-gray-500">{{
+                        `Private (${privateCollections?.length ?? 0})`
+                    }}</span>
+
+                    <div class="overflow-x-hidden overflow-y-auto h-44">
+                        <div
+                            v-if="privateCollections?.length !== 0"
+                            v-for="collection in privateCollections"
+                            :key="collection.guid"
+                            class="flex items-center p-1 cursor-pointer hover:bg-primary-light"
+                            @click="handleChange(collection.guid)"
+                        >
+                            <AtlanIcon
                                 icon="User"
                                 class="self-center w-4 h-4 pr-1"
                             ></AtlanIcon>
@@ -44,14 +85,26 @@
                                 {{ collection.attributes.name }}
                             </div>
                         </div>
+                        <div
+                            v-else
+                            class="flex flex-col items-center justify-center w-full h-full"
+                        >
+                            <AtlanIcon
+                                icon="EmptySearchQuery"
+                                class="h-24 mr-8 -mt-4 no-svaved-query-icon text-primary"
+                            />
+                            <span class="mt-3 text-gray-500"
+                                >No prive collection found</span
+                            >
+                        </div>
                     </div>
                 </div>
             </div>
         </template>
-        <div class="flex items-center cursor-pointer hover:text-primary">
+        <div class="flex items-center w-full cursor-pointer hover:text-primary">
             <AtlanIcon icon="Group" class="self-center h-4 mr-1"></AtlanIcon>
             <p class="text-sm truncate">
-                {{ selectedCollection?.attributes.name }}
+                {{ selectedCollection?.attributes?.name }}
             </p>
             <AtlanIcon
                 icon="ChevronDown"
@@ -76,6 +129,7 @@
 
 <script lang="ts">
     import {
+        nextTick,
         computed,
         defineComponent,
         ref,
@@ -88,6 +142,7 @@
         watch,
     } from 'vue'
     import AtlanIcon from '~/components/common/icon/atlanIcon.vue'
+    import { useTimeoutFn } from '@vueuse/core'
     import {
         SavedQuery,
         QueryCollection,
@@ -95,17 +150,20 @@
     import { activeInlineTabInterface } from '~/types/insights/activeInlineTab.interface'
     import { isCollectionPrivate } from '~/components/insights/explorers/queries/composables/useQueryCollection'
     import { useAuthStore } from '~/store/auth'
+    import SearchAndFilter from '@/common/input/searchAndFilter.vue'
 
     export default defineComponent({
         name: 'CollectionSelector',
         emits: ['update:data'],
-        components: { AtlanIcon },
+        components: { AtlanIcon, SearchAndFilter },
         setup(props, { emit }) {
             // store
             const authStore = useAuthStore()
+            const inputRef = ref()
             // variables
             const selectedValue = ref()
             const isVisible = ref(false)
+            const queryText = ref('')
             const queryCollections = inject('queryCollections') as ComputedRef<
                 QueryCollection[] | undefined
             >
@@ -127,14 +185,31 @@
             })
 
             const sharedCollections = computed(() =>
-                queryCollections.value?.filter(
-                    (coll) => !isCollectionPrivate(coll, username)
-                )
+                queryCollections.value?.filter((coll) => {
+                    if (queryText) {
+                        return (
+                            !isCollectionPrivate(coll, username) &&
+                            coll?.displayText
+                                ?.toLowerCase()
+                                .includes(queryText.value?.toLowerCase())
+                        )
+                    }
+                    return !isCollectionPrivate(coll, username)
+                })
             )
             const privateCollections = computed(() =>
-                queryCollections.value?.filter((coll) =>
-                    isCollectionPrivate(coll, username)
-                )
+                queryCollections.value?.filter((coll) => {
+                    if (queryText) {
+                        return (
+                            isCollectionPrivate(coll, username) &&
+                            coll?.displayText
+                                ?.toLowerCase()
+                                .includes(queryText.value?.toLowerCase())
+                        )
+                    }
+
+                    return isCollectionPrivate(coll, username)
+                })
             )
 
             function handleChange(collectionId: string) {
@@ -168,13 +243,14 @@
                 }
             })
 
-            onMounted(() => {
+            onMounted(async () => {
                 if (!queryCollectionsLoading.value) {
                     selectDefaultValue()
                 }
-                // changeDisplayText()
             })
+
             return {
+                queryText,
                 handleChange,
                 queryCollections,
                 selectedValue,
@@ -188,12 +264,25 @@
         },
     })
 </script>
-//
+
 <style lang="less">
-    //     .collectionSelectPopover {
-    //         .ant-popover-inner-content {
-    //             width: 250px !important;
-    //         }
-    //     }
-    //
+    .collectionSelectPopover {
+        width: 250px;
+        padding-top: 0 !important;
+    }
+</style>
+<style lang="css" module>
+    .inputSearch {
+        box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.05) !important;
+        background-color: #fff !important;
+        border: 1px solid #e9ebf1 !important;
+        color: #6f7590 !important;
+        border-radius: 8px !important;
+    }
+    :global(.ant-input) {
+        color: #6f7590 !important;
+    }
+    input::placeholder {
+        color: #6f7590 !important;
+    }
 </style>

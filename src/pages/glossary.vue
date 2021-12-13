e15810b8e6fa1a0c5f7ba2f1c735c59d5e6b24c7
Shubhankar <iamshubhankarkhare@gmail.com>
2021-12-13 18:03:55 +0530

    feat: splitpanes css

--- src/pages/glossary.vue
+++ src/pages/glossary.vue
@@ -1,34 +1,30 @@
 <template>
     <div class="flex w-full h-full bg-white">
-        <splitpanes class="w-full h-full bg-white">
+        <splitpanes
+            class="w-full h-full bg-white"
+            :class="$style.splitpane__styles"
+        >
             <pane
                 min-size="20"
                 max-size="50"
                 :size="22"
                 style="min-width: 264px"
             >
-                <div class="h-full border-r border-gray-200">
+                <div class="h-full">
                     <GlossaryDiscovery
                         ref="glossaryDiscovery"
                         class="h-full"
                     ></GlossaryDiscovery>
                 </div>
             </pane>
-            <pane :size="60" class="bg-white">
+            <pane :size="78" class="flex bg-white">
                 <div class="flex w-full h-full">
                     <router-view
                         v-if="isItem"
                         :selected-asset="selectedGlossary"
                     />
                 </div>
-            </pane>
-            <pane
-                class="bg-white asset-preview-container"
-                style="max-width: 420px"
-            >
-                <div
-                    class="h-full bg-white border-l xs:hidden sm:hidden md:block lg:block"
-                >
+                <div class="h-full bg-white border-l asset-preview-container">
                     <GlossaryPreview
                         :selected-asset="localSelected"
                     ></GlossaryPreview>
@@ -139,9 +135,38 @@
     .asset-preview-container {
         width: 420px !important;
         max-width: 420px !important;
+        min-width: 420px !important;
     }
 </style>
+<style lang="less" module>
+    :global(.splitpanes) {
+        &__splitter {
+            @apply bg-gray-200 !important;
+            position: relative;
+        }
 
+        &__splitter:before {
+            content: '';
+            position: absolute;
+            left: 0;
+            right: 0;
+            top: 0;
+            bottom: 0;
+            transition: 0.4s;
+        }
+        &--vertical > &__splitter:before {
+            left: -1px;
+            right: -1px;
+        }
+        &--horizontal > &__splitter:before {
+            top: -1px;
+            bottom: -1px;
+        }
+        &__splitter:hover:before {
+            background-color: #5277d7;
+        }
+    }
+</style>
 <route lang="yaml">
 meta:
     layout: default

1867c6788aa44099bcc608e6a059c84170f5e999
AtlanKautzar <kautzar@atlan.com>
2021-12-14 17:46:16 +0700

    text

--- src/components/governance/personas/overview/detailPolicy.vue
+++ src/components/governance/personas/overview/detailPolicy.vue
@@ -1,26 +1,26 @@
 <template>
     <div class="p-4 border-b border-bottom border-slate-300">
         <div class="flex justify-between">
-            <div class="text-lg font-bold"> {{selectedPolicy.name}} </div>
+            <div class="text-lg font-bold">{{ selectedPolicy.name }}</div>
             <AtlanBtn
                 class="flex-none"
                 size="sm"
                 color="secondary"
                 padding="compact"
             >
-                <AtlanIcon icon="Delete" class="-mx-1 text-black"/>
+                <AtlanIcon icon="Delete" class="-mx-1 text-black" />
             </AtlanBtn>
         </div>
         <div class="flex items-center">
-            <AtlanIcon icon="Policies" class="mr-1"/>
-            <span class="text-neutral-600">{{selectedPolicy.type}} </span>
+            <AtlanIcon icon="Policies" class="mr-1" />
+            <span class="text-neutral-600">{{ selectedPolicy.type }} </span>
         </div>
     </div>
     <div class="p-4">
-        <div class="font-bold base text-neutral-500"> Detail Info</div>
+        <div class="font-bold base text-neutral-500">Detailed Info</div>
         <div class="mt-4">
             <div class="text-gray-500">Name</div>
-            <div class="mt-1">{{selectedPolicy.name}}</div>
+            <div class="mt-1">{{ selectedPolicy.name }}</div>
         </div>
         <div class="mt-4">
             <div class="text-gray-500">Connection</div>
@@ -43,17 +43,15 @@
                     color="minimal"
                     padding="compact"
                 >
-                    <span class="text-primary">
-                        Add asset
-                    </span>
-                    <AtlanIcon icon="ArrowRight" class="ml-1 text-primary"/>
+                    <span class="text-primary"> Add asset </span>
+                    <AtlanIcon icon="ArrowRight" class="ml-1 text-primary" />
                 </AtlanBtn>
             </div>
-            <div class="flex justify-between p-3 mt-4 border rounded-sm border-bottom border-slate-300">
-                
-            </div>
+            <div
+                class="flex justify-between p-3 mt-4 border rounded-sm border-bottom border-slate-300"
+            ></div>
         </div>
-         <div class="mt-4">
+        <div class="mt-4">
             <div class="flex justify-between">
                 <div class="text-gray-500">Metadata permissions</div>
                 <AtlanBtn
@@ -62,21 +60,19 @@
                     color="minimal"
                     padding="compact"
                 >
-                    <span class="text-primary">
-                        Manage
-                    </span>
-                    <AtlanIcon icon="ArrowRight" class="ml-1 text-primary"/>
+                    <span class="text-primary"> Manage </span>
+                    <AtlanIcon icon="ArrowRight" class="ml-1 text-primary" />
                 </AtlanBtn>
             </div>
             <div class="flex items-center mt-1">
-                <span class="px-1 text-sm text-gray-700">
-                    Partial
-                </span>
+                <span class="px-1 text-sm text-gray-700"> Partial </span>
             </div>
         </div>
-        <div class="flex justify-between p-3 mt-4 border rounded-sm border-bottom border-slate-300">
+        <div
+            class="flex justify-between p-3 mt-4 border rounded-sm border-bottom border-slate-300"
+        >
             <span>Deny permissions</span>
-            <a-switch/>
+            <a-switch />
         </div>
     </div>
 </template>
@@ -93,30 +89,30 @@
             selectedPolicy: {
                 type: Object,
                 required: true,
-                default: () => ({})
+                default: () => ({}),
             },
         },
         emits: [],
         setup(props) {
-            const {selectedPolicy} = toRefs(props)
+            const { selectedPolicy } = toRefs(props)
             const connStore = useConnectionStore()
             const getImage = (id: string) => connStore.getImage(id)
             const connectionQfName = computed(() => {
                 console.log(selectedPolicy)
                 if (selectedPolicy.value.type === 'meta') {
                     const found = connStore.getList.find(
-                        (conn) => conn.guid === selectedPolicy.value.connectionId
+                        (conn) =>
+                            conn.guid === selectedPolicy.value.connectionId
                     )
                     return found?.attributes?.qualifiedName
                 }
                 return selectedPolicy.value.connectionName
             })
             return {
-              getImage,
-              connectionQfName
+                getImage,
+                connectionQfName,
             }
         },
     })
 </script>
-<style lang="less" scoped>
-</style>
+<style lang="less" scoped></style>

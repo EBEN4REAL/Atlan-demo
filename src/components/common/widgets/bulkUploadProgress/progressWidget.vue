65b61f314545b7859cc8955ea8caddf0356c1ee5
Shubhankar <iamshubhankarkhare@gmail.com>
2021-12-14 23:36:57 +0530

    feat: all success state UI

--- src/components/common/widgets/bulkUploadProgress/progressWidget.vue
+++ src/components/common/widgets/bulkUploadProgress/progressWidget.vue
@@ -1,19 +1,22 @@
 <template>
     <div
         v-if="workflowPhase !== ''"
-        class="w-full pt-2 bg-white rounded"
+        class="w-full bg-white rounded"
         style="height: max-content"
         :class="{
             'border-l-4 border-success':
                 workflowPhase === 'Succeeded' && !errorCount,
+            'border-l-4 border-red-500':
+                workflowPhase === 'Succeeded' && errorCount,
             'border-l-4 border-error':
-                (workflowPhase === 'Failed' || workflowPhase === 'Error') &&
-                errorCount,
+                workflowPhase === 'Failed' || workflowPhase === 'Error',
             hidden: !isVisible,
         }"
     >
         <!-- header  -->
-        <div class="flex items-center justify-between px-3">
+        <div
+            class="flex items-center justify-between px-4 py-2 border-t border-r border-gray-200"
+        >
             <span v-if="workflowPhase === 'Running'" class="font-bold">
                 Upload progress</span
             >
@@ -23,54 +26,58 @@
                 "
                 class="flex items-center font-bold"
             >
-                <AtlanIcon icon="RunFailed" class="w-auto h-4 mr-2" />
-                Upload not completed
+                <!-- <AtlanIcon icon="RunFailed" class="w-auto h-4 mr-2" /> -->
+                Upload Failed
             </span>
             <span
                 v-else-if="percentage === 100"
                 class="flex items-center font-bold"
             >
-                <AtlanIcon
+                <!-- <AtlanIcon
                     :icon="errorCount ? 'Warning' : 'RunSuccess'"
                     class="w-auto h-4 mr-2"
-                />
+                /> -->
                 Upload completed
-                <span v-if="errorCount !== -1" class="mx-2 text-gray-400 mb-0.5"
-                    >|</span
-                >
                 <span
-                    v-if="errorCount !== -1"
-                    class="font-normal"
-                    :class="{
-                        'text-success': !errorCount,
-                        'text-error': errorCount,
-                    }"
-                    >{{ errorCount ? errorCount : 'no' }} issues detected</span
+                    v-if="errorCount !== -1 && errorCount !== 0"
+                    class="p-0.5 ml-2 text-xs font-normal border-gray-100 border"
+                >
+                    <AtlanIcon icon="Warning" class="w-auto h-3 mb-0.5" />
+                    {{ errorCount }}</span
                 >
             </span>
-
-            <atlan-icon
-                icon="Cancel"
-                class="w-auto h-4 text-gray-500 cursor-pointer"
+            <span
+                class="text-xs text-gray-500 cursor-pointer mt-0.5"
                 @click="isVisible = false"
-            />
+                >CLOSE</span
+            >
         </div>
         <!-- body -->
+        <!-- WF running state -->
         <span
-            v-if="percentage !== 100 && percentage !== -1"
-            class="pt-3 pb-2 mx-3 text-lg font-bold text-primary"
+            v-if="
+                percentage !== 100 &&
+                percentage !== -1 &&
+                workflowPhase === 'Running'
+            "
+            class="pt-3 pb-2 mx-4 text-lg font-bold text-primary"
             >{{ percentage }}%</span
         >
         <a-progress
-            v-if="percentage !== 100 && percentage !== -1"
+            v-if="
+                percentage !== 100 &&
+                percentage !== -1 &&
+                workflowPhase === 'Running'
+            "
             :percent="percentage"
             status="active"
             class="p-2"
         />
+        <!-- WF running state end here-->
         <!-- upload failed state -->
         <div
             v-else-if="workflowPhase === 'Error' || workflowPhase === 'Failed'"
-            class="px-3 pb-5 bg-gray-100"
+            class="px-4 pb-5 bg-gray-100"
         >
             <a-divider class="mt-2 mb-5" />
             <span class="text-gray-500"
@@ -86,30 +93,49 @@
                 </BulkModal>
             </div>
         </div>
+        <!-- upload failed state ends here-->
 
-        <div
-            v-else
-            class="px-0 pb-2 mt-2"
-            :class="{ 'bg-gray-100': errorCount }"
-        >
-            <a-divider v-if="totalCount !== -1" class="mt-0 mb-5" />
+        <div v-else class="px-0 bg-gray-100 border-b border-r border-gray-200">
+            <a-divider v-if="totalCount !== -1" class="my-0" />
             <div
                 v-if="totalCount !== -1"
-                class="flex items-center px-3 mt-2 space-x-2"
+                class="flex items-center px-4 py-4 space-x-2"
             >
                 <div class="flex items-center">
-                    <AtlanIcon icon="Approve" class="w-auto h-3 mr-2" />
-                    <span class="border-0 shadow-none" size="small">
+                    <AtlanIcon icon="RunSuccess" class="w-auto h-3 mr-2" />
+                    <span
+                        class="font-bold text-gray-500 border-0 shadow-none"
+                        size="small"
+                    >
                         {{ totalCount }} terms uploaded
                     </span>
                 </div>
-                <span v-if="createdCount" class="px-4 text-gray-400 mb-0.5"
+                <span
+                    v-if="createdCount || updatedCount"
+                    class="text-gray-400 mb-0.5 font-bold"
+                    >-</span
+                >
+                <div v-if="updatedCount" class="flex items-center">
+                    <span
+                        class="text-gray-500 border-0 shadow-none"
+                        size="small"
+                    >
+                        {{ updatedCount }}
+                        {{ `${updatedCount > 1 ? 'terms' : 'term'}` }} updated
+                    </span>
+                </div>
+                <span
+                    v-if="createdCount && updatedCount"
+                    class="px-1 text-gray-400 mb-0.5 text-lg"
                     >|</span
                 >
                 <div v-if="createdCount" class="flex items-center">
-                    <AtlanIcon icon="Approve" class="w-auto h-3 mr-2" />
-                    <span class="border-0 shadow-none" size="small">
-                        {{ createdCount }} terms created
+                    <span
+                        class="text-gray-500 border-0 shadow-none"
+                        size="small"
+                    >
+                        {{ createdCount }}
+                        {{ createdCount > 1 ? 'terms' : 'term' }} created
                     </span>
                 </div>
             </div>
@@ -165,10 +191,13 @@
             const percentage = ref(20)
             const workflowTemplateName = ref()
             // data to display after upload complete
+            // default to -1 as they can be 0 as well
             const totalCount = ref(-1)
             const createdCount = ref(-1)
             const updatedCount = ref(-1)
             const errorCount = ref(-1)
+            const categoryCreatedCount=ref(-1)
+            const categoryErrorCount=ref(-1)
             // helpers
             const isVisible = ref(true)
             const workflowPhase = ref('')
@@ -187,18 +216,30 @@
             }
             // returns data to be displayed after upload complete
             const getFinalStatus = (data) => {
+                // gives node containing counts of terms
                 const createFinalCsvNode = Object.keys(data.nodes).find(
                     (el) => data.nodes[el].displayName === 'create-final-csv'
                 )
+                // gives node containing counts of categories
+                const extractCategoryStatusNode=Object.keys(data.nodes).find(
+                    (el) => data.nodes[el].displayName === 'extract-category-status'
+                )
+                const finalCategoryStatus=data.nodes[extractCategoryStatusNode]?.outputs?.parameters.find((el) => (el.name = 'status'))
                 const finalStatus = data.nodes[
                     createFinalCsvNode
                 ]?.outputs?.parameters.find((el) => (el.name = 'status'))
                 const statusJson = JSON.parse(finalStatus?.value)
+                const categoryStatusJson=JSON.parse(finalCategoryStatus?.value)
                 console.log(statusJson)
-                totalCount.value = statusJson.total_count
-                errorCount.value = statusJson.error_count
-                updatedCount.value = statusJson.updated_count
-                createdCount.value = statusJson.created_count
+                console.log(categoryStatusJson)
+                // for terms
+                totalCount.value = statusJson?.total_count
+                errorCount.value = statusJson?.error_count
+                updatedCount.value = statusJson?.updated_count
+                createdCount.value = statusJson?.created_count
+                // for categories
+                categoryCreatedCount.value=categoryStatusJson?.created_count
+                categoryErrorCount.value=categoryStatusJson?.error_count
                 nodeName.value = data.nodes[createFinalCsvNode].name
             }
 
@@ -249,10 +290,10 @@
                    const fileName='Results CSV'
                     downloadFile(data.value, fileName)
                     } else {
-                        message.error({
-                            content: `Could not get file!`,
-                            duration: 5,
-                        })
+                        message.error(
+                            `Could not get file!`,
+                             5,
+                        )
                     }
                 })
             }
@@ -276,17 +317,21 @@
                                progressPercent(liveList.value.items[0])
                                )
                             isWorkflowRunning.value='Running'
+                            }
                         }
-                    }
-            })
+                    getFinalStatus(liveList.value?.items[0].status)
+                    })
             })
             return {
-                percentage,
-                totalCount,
-                createdCount,
-                errorCount,
-                isVisible,
-                workflowPhase,
+                percentage:100,
+                totalCount:2,
+                createdCount:1,
+                errorCount:0,
+                categoryCreatedCount:0,
+                updatedCount:1,
+                categoryErrorCount,
+                isVisible:true,
+                workflowPhase:'Succeeded',
                 isWorkflowRunning,
                 getArtifacts,
                 reInitTree

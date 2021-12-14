c7ae0d5343f25779a7bbc5b018348dc16081702c
Izuorah Dubem kizito <dubemizuus@gmail.com>
2021-12-14 06:47:27 +0100

    RecentViewed Component setup

--- src/components/home/main/relevant.vue
+++ src/components/home/main/relevant.vue
@@ -22,11 +22,11 @@
     export default defineComponent({
         name: 'Relevant',
         components: {
-            // AssetList: defineAsyncComponent(
-            //     () => import('~/components/home/recentlyViewedAssets.vue')
-            // ),
             AssetList: defineAsyncComponent(
                 () => import('~/components/home/assetList.vue')
+            ),
+            recentlyViewedAssets: defineAsyncComponent(
+                () => import('~/components/home/recentlyViewedAssets.vue')
             ),
         },
         setup() {
@@ -45,20 +45,20 @@
                 }, */
                 {
                     id: 1,
+                    name: 'Recently visited',
+                    component: 'recentlyViewedAssets',
+                    typeName: ['Table'],
+                    icon: 'NoRelevantAsset',
+                    emptyText: 'All your assets will appear here.',
+                },
+                {
+                    id: 2,
                     name: 'My Assets',
                     component: 'AssetList',
                     typeName: ['Table'],
                     icon: 'NoRelevantAsset',
                     emptyText: 'All your assets will appear here.',
                 },
-                // {
-                //     id: 2,
-                //     name: 'Assets Owned',
-                //     component: 'AssetList',
-                //     typeName: ['Table'],
-                //     icon: 'NoRelevantAsset',
-                //     emptyText: 'All your assets will appear here.',
-                // },
                 /*{
                     id: 2,
                     name: 'Your terms',
--- src/components/home/recentlyViewedAssets.vue
+++ src/components/home/recentlyViewedAssets.vue
@@ -1,7 +1,57 @@
 <template>
     <div class="asset-list">
-        <pre>{{ list }}</pre>
-        <!-- WIP -->
+        <!-- <pre>{{ list }}</pre> -->
+        <template v-if="isLoading || list.length > 0">
+            <div v-if="isLoading" class="flex items-center justify-center">
+                <svg
+                    class="w-5 h-5 text-primary animate-spin"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                >
+                    <circle
+                        class="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        stroke-width="4"
+                    ></circle>
+                    <path
+                        class="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                </svg>
+            </div>
+            <VirtualList
+                v-else
+                :class="{ 'animate-pulse': isLoading }"
+                :data="list"
+                data-key="guid"
+                variable-height
+            >
+                <template #default="{ item }">
+                    <div class="p-3 border-b hover:bg-blue-50">
+                        <AccessLogItem
+                            :log="item"
+                            :asset-meta-map="assetMetaMap"
+                        />
+                    </div>
+                </template>
+            </VirtualList>
+        </template>
+        <template v-else>
+            <div class="flex flex-col items-center justify-center h-full">
+                <div class="flex flex-col items-center">
+                    <AtlanIcon
+                        :icon="icon"
+                        class="w-auto mb-4"
+                        style="height: 177px"
+                    />
+                    <span class="text-gray-500">{{ emptyText }}</span>
+                </div>
+            </div>
+        </template>
     </div>
     <slot name="listFooter"></slot>
 </template>
@@ -14,6 +64,8 @@
     import useAddEvent from '~/composables/eventTracking/useAddEvent'
     import { useAssetListing } from '~/composables/home/useHomeDSL'
     import { useAccessLogs } from '@/governance/accessLogs/composables/useAccessLogs.ts'
+    import AccessLogItem from '@/governance/accessLogs/accessLogItem.vue'
+
     // import redirect from '@/glossary/utils/redirectToProfile'
     import { useRouter } from 'vue-router'
 
@@ -22,6 +74,7 @@
         components: {
             VirtualList,
             ListItem,
+            AccessLogItem,
         },
         props: {
             typeNames: {
@@ -66,7 +119,12 @@
             const from = ref(0)
             const size = ref(20)
 
-            const { list, isLoading } = useAccessLogs(gte, lt, from, size)
+            const { list, isLoading, assetMetaMap } = useAccessLogs(
+                gte,
+                lt,
+                from,
+                size
+            )
 
             function handlePreview(item: any) {
                 if (item.guid === '-1') {
@@ -106,6 +164,7 @@
                 list,
                 handleCardClicked,
                 isLoading,
+                assetMetaMap,
             }
         },
     })

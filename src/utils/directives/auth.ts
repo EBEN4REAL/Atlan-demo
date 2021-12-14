7c0ba87d24ed734867557adba51a12d79f5ba7a1
Tanya Chawla <tanya@ip-10-10-10-11.ap-south-1.compute.internal>
2021-12-14 14:37:25 +0530

    remove extra LOC

--- src/utils/directives/auth.ts
+++ src/utils/directives/auth.ts
@@ -14,7 +14,7 @@ export function authDirective(app: any) {
                 return
             }
             watch(
-                () => permissions,
+                permissions,
                 () => {
                     if (Array.isArray(binding.value)) {
                         if (

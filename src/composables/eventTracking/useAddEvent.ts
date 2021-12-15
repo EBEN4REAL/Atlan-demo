4a904753a2bdf7f57fe942646f6a0937cb9c969b
goelrohan6 <goelrohan6@gmail.com>
2021-12-15 14:31:03 +0530

    wip: analytics

--- src/composables/eventTracking/useAddEvent.ts
+++ src/composables/eventTracking/useAddEvent.ts
@@ -1,8 +1,10 @@
 import keyMap from '~/composables/eventTracking/keyMap'
 
-const useAddEvents = (category, obj, action, props) => {
+const useAddEvent = (category, obj, action, props = {}) => {
+    console.log('analytics track args', { category, obj, action })
     // construct params for adding events
     const event = keyMap[category][obj][action]
+    console.log('analytics track', { event })
     const eventName = event.action
     const eventProperties = () => {
         if (event?.properties) return event?.properties(props)
@@ -10,10 +12,12 @@ const useAddEvents = (category, obj, action, props) => {
     }
 
     // API call for adding event to segment
+    const properties = eventProperties ? eventProperties() : {}
+    console.log('analytics track', { eventName, properties })
     if (eventProperties) {
-        ;(window as any).analytics.track(eventName, eventProperties())
+        ;(window as any).analytics.track(eventName, properties)
     } else {
         ;(window as any).analytics.track(eventName)
     }
 }
-export default useAddEvents
+export default useAddEvent

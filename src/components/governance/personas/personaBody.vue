e6944ec9a7a0b654ea343dcd4bc0e95c763de100
AtlanKautzar <kautzar@atlan.com>
2021-12-15 11:38:45 +0700

    z-index for overlapping list

--- src/components/governance/personas/personaBody.vue
+++ src/components/governance/personas/personaBody.vue
@@ -69,7 +69,7 @@
                 :persona="persona"
             />
             <div v-else-if="activeTabKey === 'policies'">
-                <div class="sticky top-0 bg-white">
+                <div class="sticky top-0 z-10 bg-white">
                     <div class="flex items-center pt-3 pr-4">
                         <SearchAndFilter
                             v-model:value="searchPersona"

5de8f4511cf807c9dd292536bcf1f1d843e07c38
Angad Sethi <angadsethi18@gmail.com>
2021-12-15 12:24:43 +0530

    Pushing Slack to the Header
    This commit pushes the slack CTA to the header, and removes it from the contact details.

--- src/components/admin/users/userPreview/viewUserDetails.vue
+++ src/components/admin/users/userPreview/viewUserDetails.vue
@@ -1,17 +1,9 @@
 <template>
     <div class="flex">
-        <div class="mb-3 text-lg font-bold text-gray-500">User info</div>
-        <div v-if="isCurrentUser" class="ml-auto">
-            <a-button
-                @click="$emit('toggleEdit')"
-            >
-                <AtlanIcon icon="Edit" />
-                <span class="ml-1 text-gray-700">Edit</span>
-            </a-button>
-        </div>
+        <div class="mb-3 text-lg font-bold text-gray-500">Profile</div>
     </div>
     <div class="pb-6 border-solid border-b border-gray-200">
-        <div class="mb-6">
+        <div class="mb-3">
             <div class="flex-1 mr-4">
                 <p class="mb-0 text-gray-500">Joined</p>
                 <a-tooltip>
@@ -22,7 +14,7 @@
                 </a-tooltip>
             </div>
         </div>
-        <div class="mb-6">
+        <div class="mb-3">
             <div class="flex-1 mr-4">
                 <p class="mb-0 text-gray-500">Designation</p>
                 <div class="text-gray">{{ selectedUser?.attributes?.designation?.length > 0 ? selectedUser?.attributes?.designation[0] : "-" }}</div>
@@ -36,12 +28,7 @@
                 @updated-user="$emit('updatedUser')"
             />
         </div>
-    </div>
-<!--    <div class="py-6 border-solid border-b border-gray-200">-->
-<!--        <OwnershipDetails :selected-user="selectedUser" />-->
-<!--    </div>-->
-    <div class="py-6 border-solid border-b border-gray-200">
-        <div class="mb-6">
+        <div class="mb-3">
             <ViewGroups :user="selectedUser" />
         </div>
         <div>
@@ -50,16 +37,7 @@
     </div>
     <div class="py-6">
         <p class="uppercase text-gray-500 text-sm">Contact Details</p>
-        <div v-if="slackEnabled" class="mt-2">
-            <div class="text-gray-500 text-sm">Slack</div>
-            <a
-                :href="slackUrl"
-                class="text-sm text-primary font-bold mt-2"
-            >
-                Slack
-            </a>
-        </div>
-        <div :class="slackEnabled ? 'mt-6' : 'mt-2'">
+        <div class="mt-2">
             <div class="text-gray-500 text-sm">Email Address</div>
             <a
                 :href="`mailto:${selectedUser.email}`"
@@ -76,17 +54,14 @@
     import UpdateSkills from '~/components/admin/users/userPreview/about/updateSkills.vue'
     import ViewGroups from '@/admin/users/userPreview/about/viewGroups.vue'
     import ViewPersonas from '@/admin/users/userPreview/about/viewPersonas.vue'
-    import OwnershipDetails from '~/components/admin/users/userPreview/about/ownershipDetails.vue'
     import dayjs from 'dayjs'
-    import { getDeepLinkFromUserDmLink } from '~/composables/integrations/useSlack'
 
     export default defineComponent({
         name: 'ViewUser',
         components: {
-            OwnershipDetails,
             ViewPersonas,
             ViewGroups,
-            UpdateSkills,
+            UpdateSkills
         },
         props: {
             selectedUser: {
@@ -101,26 +76,9 @@
         emits: ['updatedUser', 'toggleEdit'],
         setup(props) {
             const { selectedUser } = toRefs(props)
-            const userProfiles = computed(() => selectedUser.value?.attributes?.profiles)
-
             const createdAt = computed(() => dayjs.unix(selectedUser.value.createdTimestamp / 1000).format('MMMM D, YYYY h:mm A'))
 
-            const slackProfile = computed(() => {
-                if (userProfiles.value?.length > 0) {
-                    const firstProfile = JSON.parse(userProfiles.value[0])
-                    if (firstProfile && firstProfile.length > 0 && firstProfile[0].hasOwnProperty('slack')) {
-                        return firstProfile[0].slack
-                    }
-                }
-                return ""
-            })
-
-            const slackEnabled = computed(() => slackProfile.value)
-            const slackUrl = computed(() => slackEnabled.value ? getDeepLinkFromUserDmLink(slackEnabled.value) : "#")
-
             return {
-                slackEnabled,
-                slackUrl,
                 createdAt
             }
         }

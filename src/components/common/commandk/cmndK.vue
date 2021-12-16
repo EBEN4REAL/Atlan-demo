50a51014fe1fd4dfc4c3371c9670e1c1e1d94558
goelrohan6 <goelrohan6@gmail.com>
2021-12-17 00:44:19 +0530

    fix: paddings aggregates cmd k

--- src/components/common/commandk/cmndK.vue
+++ src/components/common/commandk/cmndK.vue
@@ -20,7 +20,7 @@
             </a-input>
         </div>
 
-        <div v-if="assetTypeAggregationList.length" class="w-full">
+        <div v-if="assetTypeAggregationList.length" class="w-full px-2">
             <AggregationTabs
                 v-model="postFacets.typeName"
                 class="mt-3"

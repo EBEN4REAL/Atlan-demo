2b72707e1cf84c6ffc3f765b5b6a3d75c1185871
Shubhankar <iamshubhankarkhare@gmail.com>
2021-12-16 13:02:04 +0530

    fix: back button route handle

--- src/pages/glossary/[id]/[tab].vue
+++ src/pages/glossary/[id]/[tab].vue
@@ -68,7 +68,7 @@
             ])
             const relationAttributes = ref([...DefaultRelationAttributes])
 
-            const { list, isLoading } = useDiscoverList({
+            const { list, isLoading, quickChange } = useDiscoverList({
                 isCache: false,
                 dependentKey,
                 facets,
@@ -83,6 +83,13 @@
                     localSelected.value = list.value[0]
 
                     handlePreview(list.value[0])
+                }
+            })
+            watch(id, () => {
+                if (id.value && selectedAsset.value.guid !== id.value) {
+                    dependentKey.value = fetchKey.value
+                    facets.value.guid = id.value
+                    quickChange()
                 }
             })
             watch(selectedAsset, () => {

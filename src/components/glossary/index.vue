489ea13a9f516e92f5319ed43e1e2c23a6f287cd
Aayush Dongre <aayushdongre4@gmail.com>
2021-12-17 17:39:55 +0530

    fix: increase virtual height for terms widget tree

--- src/components/glossary/index.vue
+++ src/components/glossary/index.vue
@@ -286,6 +286,8 @@
             // Virtual List Height
             const glossaryBox = ref()
             const height = computed(() => {
+                if(props.checkable) return glossaryBox.value.clientHeight
+                
                 if (glossaryBox.value) {
                     return glossaryBox.value.clientHeight - 150
                 }

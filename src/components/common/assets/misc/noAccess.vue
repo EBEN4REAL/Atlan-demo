a69cd4976b7e7c0d7930a2c1ce86af643ff81d73
shivanshpahwa24 <shivanshpahwa24@gmail.com>
2021-12-22 01:55:02 +0530

    no access illustration when tab is disabled

---
+++ src/components/common/assets/misc/noAccess.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="flex justify-center h-screen">
+        <div class="flex flex-col m-auto">
+            <AtlanIcon icon="NoAccess" class="h-32" />
+            <div class="leading-4 text-center text-gray-500 text">
+                Oops, looks like you don't have access to view this asset!
+            </div>
+            <AtlanButton v-if="backButton" color="primary" @click="backToHome"
+                >Take me home</AtlanButton
+            >
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue'
+    import { useRouter } from 'vue-router'
+    import AtlanButton from '@/UI/button.vue'
+
+    export default defineComponent({
+        components: { AtlanButton },
+        props: {
+            backButton: {
+                type: Boolean,
+                required: false,
+                default: false,
+            },
+        },
+        setup() {
+            const router = useRouter()
+
+            const backToHome = () => {
+                router.push('/')
+            }
+
+            return {
+                backToHome,
+            }
+        },
+    })
+</script>
+<style lang="less">
+    .text {
+        max-width: 216px;
+        margin-top: 35px;
+        margin-bottom: 1rem;
+    }
+</style>

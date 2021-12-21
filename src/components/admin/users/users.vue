c9ef8aec7606e808dd3510e09710f1f82851c606
Angad Sethi <angadsethi18@gmail.com>
2021-12-21 07:01:36 +0530

    Fix Loading in User Table
    This commit fixes the loading state in the user table.

--- src/components/admin/users/users.vue
+++ src/components/admin/users/users.vue
@@ -64,14 +64,7 @@
                 @event="clearFilter"
             />
             <template v-else>
-                <div
-                    v-if="isLoading"
-                    class="flex items-center justify-center h-full"
-                >
-                    <AtlanIcon icon="Loader" class="h-7 animate-spin" />
-                </div>
                 <UserListTable
-                    v-else
                     v-auth="map.LIST_USERS"
                     :user-list="userList"
                     :loading="isLoading"

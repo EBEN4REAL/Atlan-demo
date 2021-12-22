c92dc85fdf9d0cfe6265ab06132f860f0b86d1b0
goelrohan6 <goelrohan6@gmail.com>
2021-12-22 14:05:45 +0530

    change classification popover layout

--- src/components/common/popover/classification.vue
+++ src/components/common/popover/classification.vue
@@ -1,71 +1,77 @@
 <template>
     <a-popover placement="left">
         <template #content>
-            <div class="flex flex-col p-4 text-gray-700 w-96">
-                <div class="flex justify-between mb-2 text-sm">
-                    <span>
-                        <AtlanIcon icon="ShieldCheck" class="mb-1 mr-1" />
-                        <span class="text-gray-500">CLASSIFICATION</span>
-                    </span>
-                    <span v-if="classification?.propagate" class="text-primary">
-                        Propagated
-                    </span>
+            <div class="flex p-4 w-96">
+                <div class="mb-auto">
+                    <AtlanIcon
+                        icon="ShieldCheck"
+                        :color="classification.options?.color"
+                        class="h-5 mb-1 mr-1"
+                    />
                 </div>
-                <span class="font-bold">
-                    {{
-                        classification?.displayName || classification?.typeName
-                    }}
-                </span>
-
-                <span v-if="classification.description" class="mb-3 text-sm">
-                    {{ classification.description }}
-                </span>
-                <span v-else class="mb-3 text-sm"
-                    >This classification has no description added</span
-                >
-
-                <div class="">
-                    <span
-                        v-if="classification.propagatedBy"
-                        class="mt-4 mb-1 text-xs text-gray-500"
-                    >
-                        Propagated by
+                <div class="flex flex-col flex-1 text-gray-700">
+                    <span class="text-base font-bold">
+                        {{
+                            classification?.displayName ||
+                            classification?.typeName
+                        }}
                     </span>
-                    <div class="flex flex-wrap gap-3">
+                    <span v-if="classification.description" class="text-sm">
+                        {{ classification.description }}
+                    </span>
+                    <div class="mt-3">
                         <span
-                            v-if="classification.propagate"
-                            class="text-primary"
-                            >{{ classification.propagatedBy }}</span
+                            v-if="classification.propagatedBy"
+                            class="mt-4 mb-1 text-xs text-gray-500"
                         >
-                    </div>
-                </div>
-                <div v-if="classification.createdBy" class="flex gap-x-10">
-                    <div class="">
-                        <span class="mt-4 mb-1 text-xs text-gray-500">
-                            Created by
+                            Propagated by
                         </span>
                         <div class="flex flex-wrap gap-3">
-                            <UserPill
-                                :enable-hover="false"
-                                :username="classification.createdBy"
-                            ></UserPill>
+                            <span
+                                v-if="classification.propagate"
+                                class="text-primary"
+                                >{{ classification.propagatedBy }}</span
+                            >
+                        </div>
+                    </div>
+                    <div v-if="classification.createdBy" class="flex gap-x-10">
+                        <div class="">
+                            <span class="mt-4 mb-1 text-xs text-gray-500">
+                                Created by
+                            </span>
+                            <div class="flex flex-wrap gap-3">
+                                <UserPill
+                                    :enable-hover="false"
+                                    :username="classification.createdBy"
+                                ></UserPill>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex gap-2 mt-3 text-xs text-gray-500">
+                        <div v-if="classification.updateTime">
+                            Last updated
+                            <span class="font-bold text-gray-500">
+                                {{
+                                    useTimeAgo(classification.updateTime).value
+                                }}
+                            </span>
+                            <span v-if="classification.updatedBy">
+                                by
+                                <span
+                                    class="font-bold text-gray-500 capitalize"
+                                    >{{ classification.updatedBy }}</span
+                                >
+                            </span>
                         </div>
                     </div>
                 </div>
-                <div class="flex gap-2 mt-3 text-xs text-gray-500">
-                    <div v-if="classification.updateTime">
-                        Last updated
-
-                        <span class="font-bold text-gray-500">
-                            {{ useTimeAgo(classification.updateTime).value }}
-                        </span>
-                        <span v-if="classification.updatedBy">
-                            by
-                            <span class="font-bold text-gray-500 capitalize">{{
-                                classification.updatedBy
-                            }}</span>
-                        </span>
-                    </div>
+                <div>
+                    <span
+                        v-if="true || classification?.propagate"
+                        class="text-primary"
+                    >
+                        Propagated
+                    </span>
                 </div>
             </div>
         </template>

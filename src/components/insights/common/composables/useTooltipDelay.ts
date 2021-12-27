dd4dfc62a6dad70da7022eb160427c82e04b836b
Shekh Aliul <shekhaliul44@gmail.com>
2021-12-27 12:54:59 +0530

    feat: tooltip delay added

---
+++ src/components/insights/common/composables/useTooltipDelay.ts
@@ -0,0 +1,24 @@
+import { ref } from 'vue'
+export function useTooltipDelay() {
+    // delay in seconds
+    const MOUSE_ENTER_DELAY = 0.5
+    const MOUSE_TRACK_MAXIMUM_DELAY = 0.2
+    const ADJACENT_TOOLTIP_DELAY = 0.1
+    const lastTooltipPresence = ref()
+
+    const recordTooltipPresence = () => {
+        lastTooltipPresence.value = setTimeout(() => {
+            if (lastTooltipPresence.value !== undefined) {
+                clearTimeout(lastTooltipPresence.value)
+                lastTooltipPresence.value = undefined
+            }
+        }, MOUSE_TRACK_MAXIMUM_DELAY * 1000)
+    }
+    return {
+        recordTooltipPresence,
+        MOUSE_ENTER_DELAY,
+        MOUSE_TRACK_MAXIMUM_DELAY,
+        ADJACENT_TOOLTIP_DELAY,
+        lastTooltipPresence,
+    }
+}

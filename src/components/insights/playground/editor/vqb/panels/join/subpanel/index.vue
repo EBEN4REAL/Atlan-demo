eb0226ca7ed870160e0ecf261e1e44c3e9edc0f4
Shekh Aliul <shekhaliul44@gmail.com>
2022-01-03 17:56:24 +0530

    fix: subpanel click remove from joins panel added

--- src/components/insights/playground/editor/vqb/panels/join/subpanel/index.vue
+++ src/components/insights/playground/editor/vqb/panels/join/subpanel/index.vue
@@ -212,8 +212,6 @@
                 t = copySelectedTables.filter(
                     (selectedTable) => !addedBy.includes(selectedTable.addedBy)
                 )
-
-                debugger
                 selectedTables.value = t
                 subpanels.value.splice(index, 1)
             }

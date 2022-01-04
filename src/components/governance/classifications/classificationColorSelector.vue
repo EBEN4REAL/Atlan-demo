ed8d84f5a44c513178a9bb971a7d5be2eb647098
Samiran Konwar <saammmmmmy@gmail.com>
2022-01-04 18:34:23 +0530

    apply classification color definition & changes to non menu mode

--- src/components/governance/classifications/classificationColorSelector.vue
+++ src/components/governance/classifications/classificationColorSelector.vue
@@ -30,23 +30,42 @@
             <AtlanIcon icon="CaretDown" />
         </div>
     </a-dropdown>
-    <div v-else>
-        <div class="flex flex-col gap-1">
-            <span
-                v-for="option in options"
-                :key="option.color"
-                class="flex items-center px-2 rounded h-7 hover:bg-gray-light"
-                @click="selectedColor = option.color"
-            >
-                <AtlanIcon
-                    icon="Shield"
-                    class="self-center mr-1"
-                    :class="option.text"
-                />
-                <span>{{ option.color }}</span>
-            </span>
+    <template v-else>
+        <div
+            class="flex flex-col bg-white cursor-pointer"
+            style="max-width: 271px"
+        >
+            <template v-for="option in options" :key="option.color">
+                <div
+                    class="flex px-4 py-2 hover:bg-gray-light"
+                    :class="
+                        option.color === selectedColor ? 'bg-primary-light' : ''
+                    "
+                    @click="selectedColor = option.color"
+                >
+                    <div class="">
+                        <AtlanIcon
+                            icon="Shield"
+                            class="self-center mr-1"
+                            :class="option.text"
+                        />
+                    </div>
+                    <div class="flex flex-col">
+                        <span>{{ option.color }}</span>
+                        <span class="text-xs text-gray-500">{{
+                            option.desc
+                        }}</span>
+                    </div>
+                    <div
+                        v-if="selectedColor === option.color"
+                        class="flex items-center"
+                    >
+                        <AtlanIcon icon="Check" class="text-primary" />
+                    </div>
+                </div>
+            </template>
         </div>
-    </div>
+    </template>
 </template>
 
 <script lang="ts">
@@ -71,25 +90,28 @@
         emits: ['update:selectedColor'],
         setup(props, { emit }) {
             const options = [
-                {
-                    color: 'Blue',
-                    background: 'bg-indigo-50',
-                    text: 'text-blue-600',
-                },
+                // {
+                //     color: 'Blue',
+                //     background: 'bg-indigo-50',
+                //     text: 'text-blue-600',
+                // },
                 {
                     color: 'Green',
                     background: 'bg-green-50',
                     text: 'text-green-600',
+                    desc: 'Standard data that is non PII or less restricted PII',
                 },
                 {
                     color: 'Yellow',
                     background: 'bg-yellow-50',
                     text: 'text-yellow-600',
+                    desc: 'Restricted data and may include personal information or PII',
                 },
                 {
                     color: 'Red',
                     background: 'bg-red-50',
                     text: 'text-red-600',
+                    desc: 'Highly-restricted data, including personally identifiable information (PII)',
                 },
             ]
 

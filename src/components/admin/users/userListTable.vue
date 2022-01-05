e5754f801a41f0c9b22db0b63ec10a9da0dbdfd8
AtlanKautzar <kautzar@atlan.com>
2022-01-05 18:03:11 +0700

    fix align

--- src/components/admin/users/userListTable.vue
+++ src/components/admin/users/userListTable.vue
@@ -13,18 +13,7 @@
         @change="handleTableChange"
     >
         <template #headerCell="{ title, column }">
-            <a-tooltip
-                v-if="column.sortKey"
-                placement="top"
-                :class="
-                    column.align === 'center'
-                        ? 'justify-center'
-                        : column.align === 'right'
-                        ? 'justify-end'
-                        : 'justify-start'
-                "
-                class="p-4"
-            >
+            <a-tooltip v-if="column.sortKey" placement="top" class="p-4">
                 <template #title>{{
                     getSortTooltipText(
                         column.sortKey,
@@ -33,7 +22,7 @@
                     )
                 }}</template>
                 <div
-                    class="flex font-normal tracking-wide text-gray-500 uppercase cursor-pointer group"
+                    class="flex justify-start font-normal tracking-wide text-gray-500 uppercase cursor-pointer group"
                     :class="column.align === 'right' ? 'flex-row-reverse' : ''"
                 >
                     <div>

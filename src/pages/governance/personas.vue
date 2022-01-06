69cc598e3bb1acc437b43caa0207b35458d94ae8
goelrohan6 <goelrohan6@gmail.com>
2022-01-06 16:59:40 +0530

    feat: persona/purpose/classification page calls

--- src/pages/governance/personas.vue
+++ src/pages/governance/personas.vue
@@ -7,11 +7,12 @@
 <script lang="ts">
     import { defineComponent, onMounted, watch, computed } from 'vue'
     import { useHead } from '@vueuse/head'
+    import { useDebounceFn } from '@vueuse/core'
+    import { useRoute } from 'vue-router'
     import NoAccess from '@/common/secured/access.vue'
     import PersonaView from '@/governance/personas/personaView.vue'
     import useAuth from '~/composables/auth/useAuth'
     import { useTrackPage } from '~/composables/eventTracking/useAddEvent'
-    import { useRoute } from 'vue-router'
 
     export default defineComponent({
         components: {
@@ -27,13 +28,19 @@
             })
             const { isAccess } = useAuth()
 
+            const sendPageEvent = useDebounceFn(() => {
+                if (id.value) {
+                    useTrackPage('governance', 'personas')
+                }
+            }, 500)
+
             onMounted(() => {
-                useTrackPage('governance', 'personas')
+                sendPageEvent()
             })
 
             watch(id, () => {
                 if (id.value) {
-                    useTrackPage('governance', 'personas')
+                    sendPageEvent()
                 }
             })
 

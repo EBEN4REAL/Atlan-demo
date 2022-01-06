81bde47967a8bf9c1fc7b4d8284723b17603153b
Samiran Konwar <saammmmmmy@gmail.com>
2022-01-06 15:21:52 +0530

    hide edit owners button if no permission

--- src/components/common/input/owner/index.vue
+++ src/components/common/input/owner/index.vue
@@ -10,7 +10,7 @@
             @visibleChange="handleVisibleChange"
         >
             <template #content>
-                <div class="">
+                <div v-if="editPermission" class="">
                     <OwnerFacets
                         ref="ownerInputRef"
                         v-model="localValue"
@@ -21,6 +21,7 @@
         </a-popover>
         <div class="flex flex-wrap items-center gap-1 text-sm">
             <a-button
+                v-if="editPermission"
                 :disabled="!editPermission"
                 shape="circle"
                 size="small"

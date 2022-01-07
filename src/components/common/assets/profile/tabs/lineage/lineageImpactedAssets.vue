87281eb9907f88b3739b926ab16e7274f4490ece
Sarbik Betal <sarbikb55@gmail.com>
2022-01-07 20:53:37 +0530

    WIP Updates on Lineage Downstream Asset Table

--- src/components/common/assets/profile/tabs/lineage/lineageImpactedAssets.vue
+++ src/components/common/assets/profile/tabs/lineage/lineageImpactedAssets.vue
@@ -11,6 +11,15 @@
             <!-- Header -->
             <div class="mb-6 text-lg">Downstream Impacted Assets</div>
 
+            <!-- 
+                Name
+                Depth 
+                Owner 
+                Classifications
+                Terms
+                Custom Metadata
+             -->
+
             <!-- Content - Table -->
             <div class="relative mb-6">
                 <a-table
@@ -21,6 +30,86 @@
                     size="small"
                     :loading="isLoading"
                 >
+                    <template #bodyCell="{ text, column }">
+                        <template v-if="column.key === 'details'">
+                            <div class="flex items-center gap-1">
+                                <Tooltip
+                                    :tooltip-text="text.name"
+                                    classes="text-base font-bold text-gray-700  mb-0"
+                                />
+
+                                <!-- <CertificateBadge
+                                    v-if="text.certificateStatus"
+                                    :status="text.certificateStatus"
+                                    :username="text.certificateUpdatedBy"
+                                    :timestamp="text.certificateUpdatedAt"
+                                    class="mb-1 ml-1"
+                                ></CertificateBadge> -->
+                            </div>
+                            <!-- {{ text.name }} -->
+                        </template>
+                        <!-- Depth -->
+                        <template v-else-if="column.key === 'depth'">
+                            <div class="flex items-center gap-1">
+                                <AtlanIcon icon="Platform" />
+                                <span class="text-sm text-gray">{{
+                                    text
+                                }}</span>
+                            </div>
+                        </template>
+
+                        <!-- Classification -->
+                        <template v-else-if="column.key === 'classifications'">
+                            <div class="flex flex-wrap items-center gap-1">
+                                <ClassificationPill
+                                    v-for="(classif, idx) in getClassification(
+                                        text.slice(0, 2)
+                                    )"
+                                    :key="idx"
+                                    :name="classif.name"
+                                    :display-name="classif?.displayName"
+                                    :is-propagated="isPropagated(classif)"
+                                    :color="classif.options?.color"
+                                    :allow-delete="false"
+                                />
+
+                                <span v-if="text.length - 2 > 0">
+                                    +{{ text.length - 2 }}
+                                </span>
+                                <span
+                                    v-if="!text.length"
+                                    class="text-sm text-gray-500"
+                                    >--</span
+                                >
+                            </div>
+                        </template>
+
+                        <!-- Terms -->
+                        <template v-else-if="column.key === 'terms'">
+                            <div class="flex flex-wrap items-center gap-x-1">
+                                <TermPill
+                                    v-for="(term, idx) in text.slice(0, 2)"
+                                    :key="idx"
+                                    :term="term"
+                                    :allow-delete="false"
+                                />
+                                <span v-if="text.length - 2 > 0">
+                                    +{{ text.length - 2 }}
+                                </span>
+                                <span
+                                    v-if="!text.length"
+                                    class="text-sm text-gray-500"
+                                    >--</span
+                                >
+                            </div>
+                        </template>
+
+                        <template v-else>
+                            <span>
+                                {{ text }}
+                            </span>
+                        </template>
+                    </template>
                 </a-table>
             </div>
 
@@ -48,10 +137,19 @@
     import XLSX from 'xlsx'
 
     /** COMPOSABLES */
+    import useAssetInfo from '~/composables/asset/useAssetInfo'
     import useGetNodes from './useGetNodes'
+    import useTypedefData from '~/composables/typedefs/useTypedefData'
+
+    /** COMPONENTS */
+    import TermPill from '@/common/pills/term.vue'
+    import ClassificationPill from '@/common/pills/classification.vue'
+    import CertificateBadge from '@/common/badge/certificate/index.vue'
+    import Tooltip from '@/common/ellipsis/index.vue'
 
     export default defineComponent({
         name: 'LineageImpactedAssets',
+        components: { TermPill, ClassificationPill, CertificateBadge, Tooltip },
         props: {
             graph: {
                 required: true,
@@ -70,6 +168,24 @@
             const isLoading = ref(true)
             const { graph, guid } = toRefs(props)
             const columnsData = ref([])
+
+            const { ownerGroups, ownerUsers } = useAssetInfo()
+            const { classificationList } = useTypedefData()
+
+            const getClassification = (ids: String[]) =>
+                classificationList.value.filter((clsf) =>
+                    ids.includes(clsf.name)
+                )
+
+            const isPropagated = (classification) => {
+                if (!guid?.value) {
+                    return false
+                }
+                if (guid.value === classification.entityGuid) {
+                    return false
+                }
+                return true
+            }
 
             const getSource = (entity) => {
                 const item = entity.attributes.qualifiedName.split('/')
@@ -95,7 +211,6 @@
                 if (!guid.value) return
                 columnsData.value = []
                 const { successors } = useGetNodes(graph, guid.value, false)
-
                 successors.forEach((x) => {
                     const cell = getCell(x)
                     const { entity } = cell.store.data
@@ -107,16 +222,24 @@
                     )
                         columnsData.value.push({
                             key: columnsData.value.length + 1,
-                            hash_index: columnsData.value.length + 1,
-                            name: entity.displayText || entity.attributes.name,
-                            type: entity.typeName,
-                            source: getSource(entity),
-                            database: getTable(entity),
-                            schema: ['Table', 'View', 'Column'].includes(
-                                entity.typeName
-                            )
-                                ? getSchema(entity)
-                                : '---',
+                            details: {
+                                name:
+                                    entity.displayText ||
+                                    entity.attributes.name,
+                                certificateStatus:
+                                    entity.attributes?.certificateStatus,
+                                certificateUpdatedBy:
+                                    entity.attributes?.certificateUpdatedBy,
+                                certificateUpdatedAt:
+                                    entity.attributes?.certificateUpdatedAt,
+                            },
+                            depth: 1,
+                            owner: [
+                                ...ownerUsers(entity),
+                                ...ownerGroups(entity),
+                            ],
+                            classifications: entity.classificationNames,
+                            terms: entity.meanings,
                         })
                 })
 
@@ -124,31 +247,30 @@
             }
 
             const downloadImpactedAssets = () => {
-                const data = []
-                columnsData.value.forEach((x) => {
-                    data.push({
-                        Name: x.name,
-                        Type: x.type,
-                        Source: x.source,
-                        Database: x.database,
-                        Schema: x.schema,
-                    })
-                })
-
-                try {
-                    const cell = getCell(guid.value)
-                    const { entity } = cell.store.data
-                    const fileName = `${entity.displayText}_lineage_impact`
-                    const name = 'atlan'
-                    const ws = XLSX.utils.json_to_sheet(data)
-                    const wb = XLSX.utils.book_new()
-                    XLSX.utils.book_append_sheet(wb, ws, name)
-                    XLSX.writeFile(wb, `${fileName}.csv`, {
-                        bookType: 'csv',
-                    })
-                } catch (error) {
-                    console.error('downloadFileCsv', error)
-                }
+                // const data = []
+                // columnsData.value.forEach((x) => {
+                //     data.push({
+                //         Name: x.name,
+                //         Type: x.type,
+                //         Source: x.source,
+                //         Database: x.database,
+                //         Schema: x.schema,
+                //     })
+                // })
+                // try {
+                //     const cell = getCell(guid.value)
+                //     const { entity } = cell.store.data
+                //     const fileName = `${entity.displayText}_lineage_impact`
+                //     const name = 'atlan'
+                //     const ws = XLSX.utils.json_to_sheet(data)
+                //     const wb = XLSX.utils.book_new()
+                //     XLSX.utils.book_append_sheet(wb, ws, name)
+                //     XLSX.writeFile(wb, `${fileName}.csv`, {
+                //         bookType: 'csv',
+                //     })
+                // } catch (error) {
+                //     console.error('downloadFileCsv', error)
+                // }
             }
 
             watch(guid, () => {
@@ -162,45 +284,42 @@
             return {
                 emit,
                 downloadImpactedAssets,
+                getClassification,
+                isPropagated,
                 isLoading,
                 columnsData,
                 columns: [
                     {
-                        width: 40,
-                        title: '#',
-                        dataIndex: 'hash_index',
-                        key: 'hash_index',
+                        width: 400,
+                        title: 'Name',
+                        dataIndex: 'details',
+                        key: 'details',
+                        // ellipsis: true,
+                        fixed: 'left',
+                    },
+                    {
+                        width: 70,
+                        title: 'Depth',
+                        dataIndex: 'depth',
+                        key: 'depth',
                     },
                     {
                         width: 250,
-                        title: 'Name',
-                        dataIndex: 'name',
-                        key: 'name',
-                        ellipsis: true,
+                        title: 'Owner',
+                        dataIndex: 'owner',
+                        key: 'owner',
                     },
                     {
-                        width: 100,
-                        title: 'Type',
-                        dataIndex: 'type',
-                        key: 'type',
+                        width: 400,
+                        title: 'Classifications',
+                        dataIndex: 'classifications',
+                        key: 'classifications',
                     },
                     {
-                        width: 100,
-                        title: 'Source',
-                        dataIndex: 'source',
-                        key: 'source',
-                    },
-                    {
-                        width: 100,
-                        title: 'Database',
-                        dataIndex: 'database',
-                        key: 'databse',
-                    },
-                    {
-                        width: 150,
-                        title: 'Schema',
-                        dataIndex: 'schema',
-                        key: 'schema',
+                        width: 400,
+                        title: 'Terms',
+                        dataIndex: 'terms',
+                        key: 'terms',
                     },
                 ],
             }

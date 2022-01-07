b90e3b7010cf5b4ccbd8511db89877f3df4a02bd
Shekh Aliul <shekhaliul44@gmail.com>
2022-01-07 15:53:04 +0530

    fix: if checks ✔

--- src/components/insights/playground/editor/vqb/panels/join/index.vue
+++ src/components/insights/playground/editor/vqb/panels/join/index.vue
@@ -255,9 +255,9 @@
                 () => activeInlineTab.value.playground.vqb.panels,
                 () => {
                     expand.value =
-                        activeInlineTab.value.playground.vqb.panels[
+                        activeInlineTab.value.playground.vqb?.panels[
                             index.value
-                        ].expand
+                        ]?.expand
                 }
             )
             const checkbox = ref(true)

a3af88cd041ee3b5945c9da6107b3ae51a159f44
shivanshpahwa24 <shivanshpahwa24@gmail.com>
2022-01-12 02:44:39 +0530

    parent dataset context for datasource profile

---
+++ src/components/common/widgets/summary/types/parentDataset.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="flex flex-col text-sm cursor-pointer">
+        <span class="mb-1 text-sm text-gray-500">Dataset</span>
+        <router-link
+            :to="`/assets/${parentDataset(asset)?.guid}`"
+            class="font-bold text-primary hover:underline"
+            >{{ parentDataset(asset)?.attributes?.name }}</router-link
+        >
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent, PropType } from 'vue'
+
+    // Composables
+    import useAssetInfo from '~/composables/discovery/useAssetInfo'
+
+    // Types
+    import { assetInterface } from '~/types/assets/asset.interface'
+
+    export default defineComponent({
+        props: {
+            asset: {
+                type: Object as PropType<assetInterface>,
+                required: true,
+            },
+        },
+        setup() {
+            const { parentDataset } = useAssetInfo()
+            return {
+                parentDataset,
+            }
+        },
+    })
+</script>

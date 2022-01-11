6b8ec8d472ebb1c350efb2c60a131e0b6768b80f
Sarbik Betal <sarbikb55@gmail.com>
2022-01-12 02:40:53 +0530

    Added certificate status to lineage graph nodes

--- src/components/common/assets/profile/tabs/lineage/util.js
+++ src/components/common/assets/profile/tabs/lineage/util.js
@@ -1,3 +1,4 @@
+// Integration Icons
 import snowflake from '~/assets/images/source/snowflake.png'
 import tableau from '~/assets/images/source/tableau.png'
 import redshift from '~/assets/images/source/redshift.png'
@@ -6,6 +7,12 @@ import athena from '~/assets/images/source/athena.png'
 import mysql from '~/assets/images/source/mysql.png'
 import glue from '~/assets/images/source/glue.png'
 import powerbi from '~/assets/images/source/powerbi.png'
+
+// Certification Icons
+import deprecated from '~/assets/images/status/deprecated.svg?url'
+import verified from '~/assets/images/status/verified.svg?url'
+import draft from '~/assets/images/status/draft.svg?url'
+import noStatus from '~/assets/images/status/nostatus.svg?url'
 
 /**
  * Gets the mapped string for the Node type
@@ -43,4 +50,14 @@ export const getNodeSourceImage = {
     mysql,
     glue,
     powerbi,
+}
+
+/**
+ * Gets the icon for the certification of the node
+ */
+export const getNodeCertificationImage = {
+    deprecated,
+    verified,
+    draft,
+    noStatus,
 }

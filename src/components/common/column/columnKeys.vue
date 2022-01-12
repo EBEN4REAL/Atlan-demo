26dd3f8a69dbdb060c5f42757f48301a312fda94
shivanshpahwa24 <shivanshpahwa24@gmail.com>
2022-01-12 19:21:01 +0530

    label required prop in column keys

--- src/components/common/column/columnKeys.vue
+++ src/components/common/column/columnKeys.vue
@@ -6,7 +6,9 @@
             v-if="isPrimary"
         >
             <AtlanIcon icon="primaryKey" class="w-3 h-3 mr-0.5"></AtlanIcon>
-            <span style="color: #eb9d07" class="">Pkey</span>
+            <span v-if="requireLabel" style="color: #eb9d07" class=""
+                >Pkey</span
+            >
         </div>
         <div
             class="flex items-center p-1 ml-2 mr-1 rounded"
@@ -14,7 +16,9 @@
             v-if="isForeign"
         >
             <AtlanIcon icon="ForeignKey" class="w-3 h-3 mr-0.5"></AtlanIcon>
-            <span class="" style="color: #9040f5">Fkey</span>
+            <span v-if="requireLabel" class="" style="color: #9040f5"
+                >Fkey</span
+            >
         </div>
 
         <div
@@ -22,34 +26,42 @@
             style="background-color: #edfdf9"
             v-if="isPartition"
         >
-            <span class="" style="color: #00a680">Partition key</span>
+            <span v-if="requireLabel" class="" style="color: #00a680"
+                >Partition key</span
+            >
         </div>
         <div
             class="flex items-center p-1 ml-2 mr-1 rounded"
             style="background-color: #edfdf9"
             v-if="isDist"
         >
-            <span class="" style="color: #00a680">Distribution key</span>
+            <span v-if="requireLabel" class="" style="color: #00a680"
+                >Distribution key</span
+            >
         </div>
         <div
             class="flex items-center p-1 ml-2 mr-1 rounded"
             style="background-color: #edfdf9"
             v-if="isClustered"
         >
-            <span class="" style="color: #00a680">Clustured key</span>
+            <span v-if="requireLabel" class="" style="color: #00a680"
+                >Clustured key</span
+            >
         </div>
         <div
             class="flex items-center p-1 ml-2 mr-1 rounded"
             style="background-color: #edfdf9"
             v-if="isSort"
         >
-            <span class="" style="color: #00a680">Sort key</span>
+            <span v-if="requireLabel" class="" style="color: #00a680"
+                >Sort key</span
+            >
         </div>
     </div>
 </template>
 
 <script lang="ts">
-    import { defineComponent, toRefs } from 'vue'
+    import { defineComponent } from 'vue'
 
     export default defineComponent({
         name: 'ColumnKeys',
@@ -90,14 +102,11 @@
                 required: false,
                 default: 'top-0',
             },
-        },
-
-        setup(props, { emit }) {
-            const { topStyle } = toRefs(props)
-            return {
-                topStyle,
-            }
+            requireLabel: {
+                type: Boolean,
+                required: false,
+                default: true,
+            },
         },
     })
 </script>
-<style lang="less" scoped></style>

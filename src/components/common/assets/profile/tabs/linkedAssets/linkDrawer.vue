c3e801384c3de30ec13d32499c03678df0ebdb46
Shubhankar <iamshubhankarkhare@gmail.com>
2022-01-12 15:45:03 +0530

    style: make term name bold in drawer header

--- src/components/common/assets/profile/tabs/linkedAssets/linkDrawer.vue
+++ src/components/common/assets/profile/tabs/linkedAssets/linkDrawer.vue
@@ -15,9 +15,13 @@
         <div class="relative overflow-x-hidden overflow-y-hidden drawer_height">
             <div class="absolute w-full h-full pt-4 bg-white">
                 <div class="flex items-center mx-5 mt-2">
+                    <span class="text-base font-bold text-gray-500"
+                        >Select and link assets to
+                    </span>
                     <Tooltip
-                        :tooltip-text="`Select and link assets to ${selectedAsset?.displayText}`"
-                        classes="text-base font-bold text-gray-500"
+                        :tooltip-text="`${selectedAsset?.displayText}`"
+                        classes="text-base font-bold ml-0.5
+                    text-gray-700 "
                     />
                     <CertificateBadge
                         v-if="certificateStatus(selectedAsset)"

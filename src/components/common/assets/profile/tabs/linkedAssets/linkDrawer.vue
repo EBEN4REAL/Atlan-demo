65f6f2f3e8f71b03ebc452c88c9240414c1d0a75
Shubhankar <iamshubhankarkhare@gmail.com>
2022-01-12 15:22:57 +0530

    style: paddings in link asset drawer

--- src/components/common/assets/profile/tabs/linkedAssets/linkDrawer.vue
+++ src/components/common/assets/profile/tabs/linkedAssets/linkDrawer.vue
@@ -14,7 +14,7 @@
     >
         <div class="relative overflow-x-hidden overflow-y-hidden drawer_height">
             <div class="absolute w-full h-full pt-4 bg-white">
-                <div class="flex items-center mx-6 mt-2">
+                <div class="flex items-center mx-5 mt-2">
                     <Tooltip
                         :tooltip-text="`Select and link assets to ${selectedAsset?.displayText}`"
                         classes="text-base font-bold text-gray-500"
@@ -35,16 +35,16 @@
                     :selectable="true"
                     :openAssetProfileInNewTab="true"
                     :selectedItems="checkedGuids"
-                    assetListClass="px-6 mt-2"
-                    aggregationTabClass="px-6"
-                    searchBarClass="px-6"
+                    assetListClass="px-0 mt-2"
+                    aggregationTabClass="px-5"
+                    searchBarClass="px-5"
                     @listItem:check="handleAssetItemCheck"
                     @handleAssetCardClick="handleAssetCardClick"
                 />
             </div>
         </div>
         <a-divider />
-        <div class="flex items-center justify-between mx-6 gap-x-2">
+        <div class="flex items-center justify-between mx-5 gap-x-2">
             <span class="text-base font-bold text-gray-500 justify-self-start"
                 >{{ selectedAssetCount || 'No' }} items selected</span
             >

8ae365801f47feae9d29f772d0e13ca06ed9415e
Shekh Aliul <shekhaliul44@gmail.com>
2022-01-16 23:22:58 +0530

    fix: asset sidebar resize and close

--- src/components/insights/assetSidebar/index.vue
+++ src/components/insights/assetSidebar/index.vue
@@ -26,6 +26,7 @@
             class="close-btn-add-policy"
             @click="handleClose"
         >
+        <div v-if="activeInlineTab?.assetSidebar?.isVisible" class="close-btn-add-policy" @click="handleClose">
             <AtlanIcon icon="Add" class="text-white" />
         </div>
         <AtlanIcon icon="NoSelectedAsset" class="w-36 h-28" />

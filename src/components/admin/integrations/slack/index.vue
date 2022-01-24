2a554a5809222e29592f540821c1a4636a370b51
Samiran Konwar <saammmmmmy@gmail.com>
2022-01-24 22:51:03 +0530

    fix slack modal hide issue

--- src/components/admin/integrations/slack/index.vue
+++ src/components/admin/integrations/slack/index.vue
@@ -1,12 +1,13 @@
 <template>
     <a-modal
-        :visible="showSlackConfigModal && !tenantSlackStatus.configured"
+        :visible="showSlackConfigModal"
         :destroy-on-close="true"
         :footer="null"
         :closable="false"
         :width="692"
         :class="$style.inviteModal"
         @cancel="showSlackConfigModal = false"
+        @afterClose="showSlackConfigModal = false"
     >
         <SlackConfigModal @close="showSlackConfigModal = false" />
     </a-modal>
@@ -15,7 +16,7 @@
 </template>
 
 <script setup lang="ts">
-    import { ref, toRefs } from 'vue'
+    import { ref, toRefs, watch } from 'vue'
     import integrationStore from '~/store/integrations/index'
     import Add from './addSlackCard.vue'
     import Edit from './slackIntegrationCard.vue'
@@ -26,6 +27,16 @@
     const store = integrationStore()
 
     const { tenantSlackStatus } = toRefs(store)
+    watch(
+        () => tenantSlackStatus.value.configured,
+        (v) => {
+            if (
+                showSlackConfigModal.value &&
+                tenantSlackStatus.value.configured
+            )
+                showSlackConfigModal.value = false
+        }
+    )
 </script>
 
 <style lang="less" module>

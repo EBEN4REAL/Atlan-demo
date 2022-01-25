71f76dc11aca3bf6365a9f405040cff188640461
shivanshpahwa24 <shivanshpahwa24@gmail.com>
2022-01-25 12:11:03 +0530

    icon fixes

--- src/constant/previewTabs.ts
+++ src/constant/previewTabs.ts
@@ -98,7 +98,6 @@ export const previewTabs = [
         name: 'Request',
         component: 'request',
         icon: 'Request',
-        activeIcon: 'RequestActive',
         tooltip: 'Request',
         scrubbed: false,
         requiredInProfile: true,

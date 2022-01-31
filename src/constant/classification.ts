eb0d16c69ba1d977e43603dcc3803f35ecd0cda8
Angad Sethi <angadsethi18@gmail.com>
2022-01-31 16:34:36 +0530

    Fine-tuning the Classification Sidebar

--- src/constant/classification.ts
+++ src/constant/classification.ts
@@ -7,7 +7,7 @@ export const tabs = [
         // component name in userPreview.vue
         component: 'Overview',
         // unique id for tab - the one that'll get passed from different components
-        key: 'about',
+        key: 'overview',
         icon: 'Overview',
         tooltip: 'Overview',
         activeIcon: 'InfoActive',

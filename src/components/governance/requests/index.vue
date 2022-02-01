e175465fb9b9f1ab17ad1c4de86c5ec4bc93e427
Samiran Konwar <saammmmmmy@gmail.com>
2022-02-01 13:31:19 +0530

    request page: search empty screen

--- src/components/governance/requests/index.vue
+++ src/components/governance/requests/index.vue
@@ -218,7 +218,7 @@
                     v-else-if="searchTerm?.length > 0"
                     class="flex flex-col items-center justify-center h-96"
                 >
-                    <atlan-icon icon="NoRequestFound" class="h-36" />
+                    <AtlanIcon icon="EmptyRequest" style="height: 165px" />
                     <span class="mt-4 text-center text-gray-500 w-72">
                         Oops… we didn’t find any requests that match this search
                     </span>

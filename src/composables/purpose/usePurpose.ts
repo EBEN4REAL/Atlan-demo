3edee1be9d2ddb737e8c901bf5b673ba62eb0f8b
AtlanKautzar <kautzar@atlan.com>
2022-02-07 10:35:31 +0700

    fix query

--- src/composables/purpose/usePurpose.ts
+++ src/composables/purpose/usePurpose.ts
@@ -4,8 +4,9 @@ import { Purpose } from '~/services/service/purpose'
 import { usePurposeStore } from '~/store/purpose'
 
 export default function usePurpose() {
+
     const { data } = Purpose.List({
-        columns: ["displayName", "name", "createdAt", "createdBy", "updatedAt", "updatedBy", "type", "level", "description", "metadataPolicies", "dataPolicies", "users", "groups", "version", "enabled", "resources", "attributes"]
+        columns: ['attributes', 'createdAt', 'createdBy', 'dataPolicies', 'description', 'displayName', 'enabled', 'id', 'isActive', 'level', 'metadataPolicies', 'name', 'resources', 'tags', 'updatedAt', 'version']
     }, { 
         options: {
             paramsSerializer:  (params) => Qs.stringify(params, {arrayFormat: 'repeat'})

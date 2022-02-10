3e88b288d516726bff22eb88e063d41a2ad2232e
Nobal <nobal@atlan.com>
2022-02-10 09:17:15 +0530

    Zendesk

---
+++ src/modules/zendesk.ts
@@ -0,0 +1,9 @@
+// import { UserModule } from "~/types/vitessg";
+
+// export const install: UserModule = ({ app }) => {
+//     var script = document.createElement('script');
+//     script.defer = 1;
+//     script.id = "ze-snippet";
+//     script.src = 'https://static.zdassets.com/ekr/snippet.js?key=5a56374b-0c6a-43d9-8abb-ed8724bae48e';
+//     document.getElementsByTagName('head')[0].appendChild(script);
+// };

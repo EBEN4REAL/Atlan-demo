8e85dbde8818c8a79b8b158beec9204dd69e0008
Samiran Konwar <saammmmmmy@gmail.com>
2022-02-14 19:24:01 +0530

    regroup Applicable Asset type for cm property

--- src/constant/assetCategories.ts
+++ src/constant/assetCategories.ts
@@ -1,3 +1,5 @@
+import { assetTypeList } from '~/constant/assetType'
+
 const AssetTypeList: {
     id: string
     label: string
@@ -9,74 +11,60 @@ const AssetTypeList: {
     orderWeight?: number
     count?: number
 }[] = [
-    {
-        id: 'Dataset',
-        label: 'Dataset',
-        children: [
-            { id: 'Database', label: 'Database' },
-            { id: 'MaterialisedView', label: 'Materialised View' },
-            { id: 'PowerBIDataset', label: 'PowerBI Dataset' },
-            { id: 'PowerBIDatasource', label: 'PowerBI Datasource' },
-            { id: 'Schema', label: 'Schema' },
-            { id: 'Table', label: 'Table' },
-            { id: 'TablePartition', label: 'Table Partition' },
-            { id: 'TableauDatasource', label: 'Tableau Datasource' },
-            { id: 'View', label: 'View' },
-        ],
-    },
-
-    {
-        id: 'Fields',
-        label: 'Fields',
-        children: [
-            { id: 'Column', label: 'Column' },
-            { id: 'TableauCalculatedField', label: 'Tableau Calculated Field' },
-            { id: 'TableauDatasourceField', label: 'Tableau Datasource Field' },
-        ],
-    },
-    {
-        id: 'Visualisation',
-        label: 'Visualisation',
-        children: [
-            { id: 'PowerBIDashboard', label: 'PowerBI Dashboard' },
-            { id: 'PowerBIDataflow', label: 'PowerBI Dataflow' },
-            { id: 'PowerBIPage', label: 'PowerBI Page' },
-            { id: 'PowerBIReport', label: 'PowerBI Report' },
-            { id: 'PowerBIWorkspace', label: 'PowerBI Workspace' },
-            { id: 'TableauDashboard', label: 'Tableau Dashboard' },
-            { id: 'TableauFlow', label: 'Tableau Flow' },
-            { id: 'TableauProject', label: 'Tableau Project' },
-            { id: 'TableauSite', label: 'Tableau Site' },
-            { id: 'TableauWorkbook', label: 'Tableau Workbook' },
-            { id: 'TableauWorksheet', label: 'Tableau Worksheet' },
-        ],
-    },
-    {
-        id: 'Insights',
-        label: 'Insights',
-        children: [
-            { id: 'Query', label: 'Query' },
-            { id: 'Folder', label: 'Query Folder' },
-            { id: 'TableauMetric', label: 'Tableau Metric' },
-        ],
-    },
-    {
-        id: 'BusinessTerms',
-        label: 'Business Terms',
-        children: [
-            { id: 'AtlasGlossary', label: 'Glossary' },
-            { id: 'AtlasGlossaryCategory', label: 'Category' },
-            { id: 'AtlasGlossaryTerm', label: 'Term' },
-        ],
-    },
-    {
-        id: 'Lineage',
-        label: 'Lineage',
-        children: [
-            { id: 'Process', label: 'Process' },
-            { id: 'ColumnProcess', label: 'Column Process' },
-        ],
-    },
-]
+        {
+            id: 'SQL',
+            label: 'SQL',
+            children: [
+                ...assetTypeList.filter(t => t.categoryType === 'SQL').map(c => ({
+                    id: c.id, label: c.label
+                }))
+            ],
+        },
+        {
+            id: 'BI',
+            label: 'BI',
+            children: [
+                ...assetTypeList.filter(t => t.categoryType === 'BI').map(c => ({
+                    id: c.id, label: c.label
+                }))
+            ],
+        },
+        {
+            id: 'SaaS',
+            label: 'SaaS',
+            children: [
+                ...assetTypeList.filter(t => t.categoryType === 'SaaS').map(c => ({
+                    id: c.id, label: c.label
+                }))
+            ],
+        },
+        {
+            id: 'Insights',
+            label: 'Insights',
+            children: [
+                ...assetTypeList.filter(t => t.categoryType === 'Insights').map(c => ({
+                    id: c.id, label: c.label
+                }))
+            ],
+        },
+        {
+            id: 'Lineage',
+            label: 'Lineage',
+            children: [
+                ...assetTypeList.filter(t => t.categoryType === 'Lineage').map(c => ({
+                    id: c.id, label: c.label
+                }))
+            ],
+        },
+        {
+            id: 'BusinessTerms',
+            label: 'Business Terms',
+            children: [
+                ...assetTypeList.filter(t => t.categoryType === 'BusinessTerms').map(c => ({
+                    id: c.id, label: c.label
+                }))
+            ],
+        }
+    ]
 
 export default AssetTypeList

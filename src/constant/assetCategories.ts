4e4fbc39d099b0c5cabe620876b6b0f6237fd1fc
shivanshpahwa24 <shivanshpahwa24@gmail.com>
2022-02-15 18:08:01 +0530

    add lineage and business terms category to asset types

--- src/constant/assetCategories.ts
+++ src/constant/assetCategories.ts
@@ -11,60 +11,78 @@ const AssetTypeList: {
     orderWeight?: number
     count?: number
 }[] = [
-        {
-            id: 'SQL',
-            label: 'SQL',
-            children: [
-                ...assetTypeList.filter(t => t.categoryType === 'SQL').map(c => ({
-                    id: c.id, label: c.label
-                }))
-            ],
-        },
-        {
-            id: 'BI',
-            label: 'BI',
-            children: [
-                ...assetTypeList.filter(t => t.categoryType === 'BI').map(c => ({
-                    id: c.id, label: c.label
-                }))
-            ],
-        },
-        {
-            id: 'SaaS',
-            label: 'SaaS',
-            children: [
-                ...assetTypeList.filter(t => t.categoryType === 'SaaS').map(c => ({
-                    id: c.id, label: c.label
-                }))
-            ],
-        },
-        {
-            id: 'Insights',
-            label: 'Insights',
-            children: [
-                ...assetTypeList.filter(t => t.categoryType === 'Insights').map(c => ({
-                    id: c.id, label: c.label
-                }))
-            ],
-        },
-        {
-            id: 'Lineage',
-            label: 'Lineage',
-            children: [
-                ...assetTypeList.filter(t => t.categoryType === 'Lineage').map(c => ({
-                    id: c.id, label: c.label
-                }))
-            ],
-        },
-        {
-            id: 'BusinessTerms',
-            label: 'Business Terms',
-            children: [
-                ...assetTypeList.filter(t => t.categoryType === 'BusinessTerms').map(c => ({
-                    id: c.id, label: c.label
-                }))
-            ],
-        }
-    ]
+    {
+        id: 'SQL',
+        label: 'SQL',
+        children: [
+            ...assetTypeList
+                .filter((t) => t.categoryType === 'SQL')
+                .map((c) => ({
+                    id: c.id,
+                    label: c.label,
+                })),
+        ],
+    },
+    {
+        id: 'BI',
+        label: 'BI',
+        children: [
+            ...assetTypeList
+                .filter((t) => t.categoryType === 'BI')
+                .map((c) => ({
+                    id: c.id,
+                    label: c.label,
+                })),
+        ],
+    },
+    {
+        id: 'SaaS',
+        label: 'SaaS',
+        children: [
+            ...assetTypeList
+                .filter((t) => t.categoryType === 'SaaS')
+                .map((c) => ({
+                    id: c.id,
+                    label: c.label,
+                })),
+        ],
+    },
+    {
+        id: 'Insights',
+        label: 'Insights',
+        children: [
+            ...assetTypeList
+                .filter((t) => t.categoryType === 'Insights')
+                .map((c) => ({
+                    id: c.id,
+                    label: c.label,
+                })),
+        ],
+    },
+    {
+        id: 'Lineage',
+        label: 'Lineage',
+        children: [
+            ...assetTypeList
+                .filter((t) => t.categoryType === 'Lineage')
+                .map((c) => ({
+                    id: c.id,
+                    label: c.label,
+                })),
+        ],
+    },
+    {
+        id: 'BusinessTerms',
+        label: 'Business Terms',
+        children: [
+            ...assetTypeList
+                .filter((t) => t.categoryType === 'BusinessTerms')
+                .map((c) => ({
+                    id: c.id,
+                    label: c.label,
+                })),
+        ],
+    },
+]
 
 export default AssetTypeList

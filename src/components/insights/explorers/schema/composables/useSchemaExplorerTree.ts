23cbccbd243cb32ab9ce2614419d654c3cc7fdf7
Shekh Aliul <shekhaliul44@gmail.com>
2022-02-15 18:41:07 +0530

    fix: merge conflict

--- src/components/insights/explorers/schema/composables/useSchemaExplorerTree.ts
+++ src/components/insights/explorers/schema/composables/useSchemaExplorerTree.ts
@@ -40,6 +40,7 @@ interface useSchemaExplorerTreeProps {
     initSelectedKeys?: Ref<string | undefined>
     initialExapndedKeys?: Ref<string | undefined>
     isAccordion?: boolean
+    handleUpdateValue?: any
 }
 
 const useTree = ({
@@ -56,6 +57,7 @@ const useTree = ({
     sortOrderTable,
     sortOrderColumn,
     searchResultType,
+    handleUpdateValue,
 }: useSchemaExplorerTreeProps) => {
     // A map of node guids to the guid of their parent. Used for traversing the tree while doing local update
     const nodeToParentKeyMap: Record<string, 'root' | string> = {}
@@ -431,7 +433,8 @@ const useTree = ({
                 const schemaResponse = await getSchemaForDatabase(
                     treeNode.dataRef.qualifiedName
                 )
-
+                if (handleUpdateValue)
+                    handleUpdateValue(schemaResponse.entities)
                 schemaResponse.entities?.forEach((schema) => {
                     treeNode.dataRef.children?.push(
                         returnTreeDataItemAttributes(schema)
@@ -451,7 +454,7 @@ const useTree = ({
                 const tableResponse = await getTablesAndViewsForSchema(
                     treeNode.dataRef.qualifiedName
                 )
-
+                if (handleUpdateValue) handleUpdateValue(tableResponse.entities)
                 tableResponse.entities?.forEach((table) => {
                     treeNode.dataRef.children?.push(
                         returnTreeDataItemAttributes(table)
@@ -471,7 +474,8 @@ const useTree = ({
                 const columnResponse = await getColumnsForTable(
                     treeNode.dataRef.qualifiedName
                 )
-
+                if (handleUpdateValue)
+                    handleUpdateValue(columnResponse.entities)
                 columnResponse.entities?.forEach((column) => {
                     treeNode.dataRef.children?.push(
                         returnTreeDataItemAttributes(column)
@@ -488,7 +492,8 @@ const useTree = ({
                 const columnResponse = await getColumnsForView(
                     treeNode.dataRef.qualifiedName
                 )
-
+                if (handleUpdateValue)
+                    handleUpdateValue(columnResponse.entities)
                 columnResponse.entities?.forEach((column) => {
                     treeNode.dataRef.children?.push(
                         returnTreeDataItemAttributes(column)
@@ -505,7 +510,6 @@ const useTree = ({
             loadedKeys.value.push(treeNode.dataRef.key)
         } catch (error) {
             console.error(error, 'error')
-            alert(error)
             const er = Object.getOwnPropertyDescriptor(error, 'message')
             isNodeLoading.value = false
             nodeError.value = er?.value

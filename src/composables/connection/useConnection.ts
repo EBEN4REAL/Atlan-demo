e26d645427d37c34e9f085698b292998cf208ec9
shivanshpahwa24 <shivanshpahwa24@gmail.com>
2022-02-22 00:05:42 +0530

    connection fetch composable refactor

--- src/composables/connection/useConnection.ts
+++ src/composables/connection/useConnection.ts
@@ -1,10 +1,12 @@
 import { ref, watch } from 'vue'
 import { useConnectionStore } from '~/store/connection'
 
+import { useBody } from '~/composables/discovery/useBody'
 import useIndexSearch from '~/composables/discovery/useIndexSearch'
 import { ConnectionAttriibutes } from '~/constant/projection'
+import { assetInterface } from '~/types/assets/asset.interface'
 
-export const MAX_CONNECTIONS = 100
+export const MAX_CONNECTIONS = 200
 export const CONNECTION_ASSET_TYPE = 'Connection'
 export const CONNECTION_ATTRIBUTES = [
     'allowQuery',
@@ -20,7 +22,7 @@ export function useConnection() {
     const { data, aggregationMap } = useIndexSearch(
         {
             dsl: {
-                size: MAX_CONNECTIONS * 10,
+                size: MAX_CONNECTIONS,
                 query: {
                     bool: {
                         filter: [
@@ -78,64 +80,39 @@ export function useConnection() {
 }
 
 export function useConnectionByConnectorName(connectorName) {
-    const { data, aggregationMap } = useIndexSearch(
-        {
-            dsl: {
-                size: MAX_CONNECTIONS * 10,
-                query: {
-                    bool: {
-                        filter: [
-                            {
-                                term: {
-                                    __state: 'ACTIVE',
-                                },
-                            },
-                            {
-                                bool: {
-                                    must: {
-                                        term: {
-                                            connectorName,
-                                        },
-                                    },
-                                    must_not: {
-                                        terms: {
-                                            '__typeName.keyword': [
-                                                'Process',
-                                                'ColumnProcess',
-                                            ],
-                                        },
-                                    },
-                                },
-                            },
-                        ],
-                    },
-                },
-                post_filter: {
-                    bool: {
-                        filter: [
-                            {
-                                terms: {
-                                    '__typeName.keyword': ['Connection'],
-                                },
-                            },
-                        ],
-                    },
-                },
-                aggs: {
-                    [GROUP_AGGREATION]: {
-                        terms: {
-                            field: 'connectionQualifiedName',
-                            size: MAX_CONNECTIONS,
-                        },
-                    },
-                },
-            },
-            attributes: [...CONNECTION_ATTRIBUTES, ...ConnectionAttriibutes],
-            suppressLogs: true,
-        },
-        ref('DEFAULT_CONNECTIONS'),
-        false
-    )
+    const defaultBody = ref({})
 
-    return { data }
+    const defaultAttributes = ref([])
+
+    const facets = ref({
+        connector: connectorName,
+        typeName: 'Connection',
+    })
+
+    const generateBody = () => {
+        const dsl = useBody('', 0, MAX_CONNECTIONS, facets?.value, {}, [], {})
+        defaultBody.value = {
+            dsl,
+            attributes: defaultAttributes?.value,
+            suppressLogs: true,
+        }
+    }
+
+    const localKey = ref(connectorName)
+
+    generateBody()
+    const { data, isLoading, mutate, cancelRequest, error, isReady } =
+        useIndexSearch<assetInterface>(defaultBody, localKey, false, false, 1)
+
+    const list = ref<assetInterface[]>([])
+
+    watch(data, () => {
+        if (data.value?.entities) {
+            list.value = [...data?.value?.entities]
+        } else {
+            list.value = []
+        }
+    })
+
+    return { data, list, isLoading, error, mutate }
 }

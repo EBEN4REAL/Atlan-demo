c1ad6ea878ee44b8e47bd225bdc603f272ac3b9c
Sarbik Betal <sarbikb55@gmail.com>
2022-03-04 20:57:34 +0530

    Add new buttons - AtlanButton2, IconButton

--- src/modules/button.ts
+++ src/modules/button.ts
@@ -1,6 +1,10 @@
 import { UserModule } from '~/types/vitessg'
 import Button from '@/UI/button.vue'
+import Button2 from '@/UI/button2.vue'
+import IconButton from '@/UI/iconButton.vue'
 
 export const install: UserModule = ({ app }) => {
     app.component('AtlanButton', Button)
+    app.component('AtlanButton2', Button2)
+    app.component('IconButton', IconButton)
 }

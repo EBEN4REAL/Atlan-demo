2e495c49ef64a88a0729a379ca63cab0f50c8e28
AtlanKautzar <kautzar@atlan.com>
2022-03-15 20:19:42 +0700

    get meta data

--- src/components/governance/personas/overview/preferences.vue
+++ src/components/governance/personas/overview/preferences.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="p-6">
+    <div class="p-6 overflow-y-scroll container-preferences">
         <a-collapse
             v-model="activeKey"
             :bordered="false"
@@ -20,9 +20,26 @@
                 </template>
                 <div class="p-4">
                     <div
+                        v-for="meta in finalBusinessMetadataList"
+                        :key="meta.guid"
                         class="flex justify-between p-3 border-b border-gray-200"
                     >
-                        <span class="text-sm text-primary">DBT</span>
+                        <div class="flex">
+                            <CustomMetadataAvatar
+                                class="mr-2"
+                                :metadata="meta"
+                                :internal="
+                                    ['true', true].includes(
+                                        meta?.options?.isLocked
+                                    )
+                                "
+                                size="16px"
+                            />
+
+                            <span class="text-sm text-primary">{{
+                                meta.displayName
+                            }}</span>
+                        </div>
                         <a-switch class="ml-auro" default-checked />
                     </div>
                 </div>
@@ -33,19 +50,25 @@
 
 <script lang="ts">
     import { defineComponent, ref } from 'vue'
+    import useBusinessMetadata from '@/governance/custom-metadata/composables/useBusinessMetadata'
+    import CustomMetadataAvatar from '@/governance/custom-metadata/CustomMetadataAvatar.vue'
 
     export default defineComponent({
         name: 'Preferences',
-        components: {},
+        components: { CustomMetadataAvatar },
         props: {},
         emits: [],
         setup(props) {
             const activeKey = ref('')
-            return { activeKey }
+            const { finalBusinessMetadataList } = useBusinessMetadata()
+            return { activeKey, finalBusinessMetadataList }
         },
     })
 </script>
 <style lang="less">
+    .container-preferences {
+        max-height: 70vh;
+    }
     .panel {
         box-shadow: 0px 8px 24px 0px #1920380a;
 

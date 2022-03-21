b990fb42546617b792559d295dea776f05e2d267
Ebuka Ezeh <ebuka@ezeh.me>
2022-03-21 12:55:57 +0100

    fix: Make cyclic edges visible and persist arrow preferences

--- src/store/lineage/state.ts
+++ src/store/lineage/state.ts
@@ -1,6 +1,7 @@
 import { useStorage } from '@vueuse/core'
 
 export const state = {
+    cyclicRelations: useStorage('lin_cyclic_relations', []),
     columnToSelect: useStorage('lin_column_qn_to_select', {}),
     mergedLineageData: useStorage('lin_merged_lineage_data', {}),
     nodesColumnList: useStorage('lin_nodes_column_list', {}),

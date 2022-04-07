f962b9796689738bddcedcc8d9f53adbeccbe1b8
AtlanKautzar <kautzar@atlan.com>
2022-04-07 22:46:23 +0700

    remove cache when API purpose fails

--- src/composables/purpose/usePurpose.ts
+++ src/composables/purpose/usePurpose.ts
@@ -4,28 +4,51 @@ import { Purpose } from '~/services/service/purpose'
 import { usePurposeStore } from '~/store/purpose'
 
 export default function usePurpose(immediate = true) {
-
-    const { data, isLoading, isReady, error, mutate } = Purpose.List({
-        columns: ['attributes', 'readme', 'createdAt', 'createdBy', 'dataPolicies', 'description', 'displayName', 'enabled', 'id', 'isActive', 'level', 'metadataPolicies', 'name', 'resources', 'tags', 'updatedAt', 'version']
-    }, {
-        asyncOptions: { immediate },
-        options: {
-            paramsSerializer: (params) => Qs.stringify(params, { arrayFormat: 'repeat' })
+    const { data, isLoading, isReady, error, mutate } = Purpose.List(
+        {
+            columns: [
+                'attributes',
+                'readme',
+                'createdAt',
+                'createdBy',
+                'dataPolicies',
+                'description',
+                'displayName',
+                'enabled',
+                'id',
+                'isActive',
+                'level',
+                'metadataPolicies',
+                'name',
+                'resources',
+                'tags',
+                'updatedAt',
+                'version',
+            ],
+        },
+        {
+            asyncOptions: { immediate },
+            options: {
+                paramsSerializer: (params) =>
+                    Qs.stringify(params, { arrayFormat: 'repeat' }),
+            },
         }
-    }
     )
     const purposeStore = usePurposeStore()
     watch(data, () => {
         purposeStore.setList(data.value?.records)
         // console.log(tenantStore.tenantRaw)
     })
-    watch(error, () => {
-        if(error.value){
+    watch(error, (newErr) => {
+        if (newErr) {
             purposeStore.setList([])
         }
- 
     })
     return {
-        data, isLoading, isReady, error, mutate
+        data,
+        isLoading,
+        isReady,
+        error,
+        mutate,
     }
 }

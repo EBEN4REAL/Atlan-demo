5cdd62847898081a856db226645a4ccf3562333d
Angad Sethi <angadsethi18@gmail.com>
2022-04-13 11:32:34 +0530

    Feat: New i18n Integration Coming Up

--- src/modules/i18n.ts
+++ src/modules/i18n.ts
@@ -1,9 +1,9 @@
 import { createI18n } from 'vue-i18n'
+import messages from '@intlify/vite-plugin-vue-i18n/messages'
 import type { UserModule } from '~/types'
-import { MessageSchema, messages } from '~/locales/schema'
 
 export const install: UserModule = ({ app }) => {
-    const i18n = createI18n<[MessageSchema], 'en'>({
+    const i18n = createI18n({
         legacy: false,
         locale: 'en',
         fallbackLocale: 'en',

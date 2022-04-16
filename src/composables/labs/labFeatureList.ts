0d0e7f6e9340160a0b58a8b5cbdeb7745195f253
goelrohan6 <goelrohan6@gmail.com>
2022-04-16 17:13:24 +0530

    wip: labs

---
+++ src/composables/labs/labFeatureList.ts
@@ -0,0 +1,89 @@
+// 7 days in milliseconds
+const showNewTagRangeMs = 7 * 24 * 60 * 60 * 100
+export const INSIGHT_WORKSPACE_LEVEL_TAB = 'INSIGHT_WORKSPACE_TAB_ENABLED'
+export const INSIGHT_TABLE_PREVIEW = 'INSIGHT_TABLE_PREVIEW_ENABLED'
+export const INSIGHT_DATA_DOWNLOAD = 'INSIGHT_DATA_DOWNLOAD'
+export const WORKFLOW_CENTER_V2 = 'WORKFLOW_CENTER_V2_ENABLED'
+
+export const featureList = [
+    {
+        name: 'Insights',
+        key: INSIGHT_WORKSPACE_LEVEL_TAB,
+        description: 'You can toggle the Insights tab for all users',
+        // if the config isn't present in tenant/user preferences, default value will be picked up from here
+        defaultValue: true,
+        // only these users will be allowed
+        allowedUsers: [],
+        allowedGroups: [],
+        // these users will be deined even if its enabled for whole org
+        deniedUsers: [],
+        deniedGroups: [],
+        // should it show up in admin center to configure for organisation
+        isAdminLevel: true,
+        // should it show up in user preferences
+        isUserLevel: false,
+        // shows beta tag on UI
+        isBeta: false,
+        // shows `new` tag based on show_new_tag_range
+        releaseDate: null,
+    },
+    {
+        name: 'Download data',
+        key: INSIGHT_TABLE_PREVIEW,
+        description:
+            'Ability to download and copy results of a query in insights',
+        // if the config isn't present in tenant/user preferences, default value will be picked up from here
+        defaultValue: true,
+        // only these users will be allowed
+        allowedUsers: [],
+        allowedGroups: [],
+        // these users will be deined even if its enabled for whole org
+        deniedUsers: [],
+        deniedGroups: [],
+        // should it show up in admin center to configure for organisation
+        isAdminLevel: true,
+        // should it show up in user preferences
+        isUserLevel: true,
+        isBeta: false,
+    },
+    {
+        name: 'New table preview',
+        key: INSIGHT_DATA_DOWNLOAD,
+        description: 'View and compare multiple table previews at once',
+        // if the config isn't present in tenant/user preferences, default value will be picked up from here
+        defaultValue: true,
+        // only these users will be allowed
+        allowedUsers: [],
+        allowedGroups: [],
+        // these users will be deined even if its enabled for whole org
+        deniedUsers: [],
+        deniedGroups: [],
+        // should it show up in admin center to configure for organisation
+        isAdminLevel: true,
+        // should it show up in user preferences
+        isUserLevel: false,
+        isBeta: true,
+    },
+    {
+        name: 'New workflow center',
+        key: WORKFLOW_CENTER_V2,
+        description:
+            'New enhanced workflow monitoring and discovery experience',
+        // if the config isn't present in tenant/user preferences, default value will be picked up from here
+        defaultValue: true,
+        // only these users will be allowed
+        allowedUsers: [],
+        allowedGroups: [],
+        // these users will be deined even if its enabled for whole org
+        deniedUsers: [],
+        deniedGroups: [],
+        // should it show up in admin center to configure for organisation
+        isAdminLevel: true,
+        // should it show up in user preferences
+        isUserLevel: false,
+        isBeta: true,
+    },
+]
+
+export const showNewTagOnFeature = (feature) =>
+    feature.releaseDate < Date.now() - showNewTagRangeMs
---
+++ src/pages/admin/labs.vue
@@ -0,0 +1,52 @@
+<template>
+    <main class="mx-4 space-y-8 my-9">
+        <h1 class="text-3xl">Labs</h1>
+        <div class="grid w-full p-5 border gap-y-5">
+            <div
+                v-for="feature in featureList"
+                :key="feature.key"
+                class="flex flex-col pb-5 border-b last:border-b-0 last:pb-0"
+            >
+                <div class="flex justify-between gap-y-8">
+                    <div class="flex flex-col">
+                        <div class="flex">
+                            <span class="text-base font-bold text-primary">{{
+                                feature.name
+                            }}</span>
+                            <div
+                                v-if="feature.isBeta"
+                                class="flex items-center px-2 ml-2 rounded-full bg-success-muted border-success"
+                            >
+                                Beta
+                            </div>
+                            <div class=""></div>
+                        </div>
+                        <span>{{ feature.description }}</span>
+                    </div>
+                    <a-switch
+                        :checked="feature.defaultValue"
+                        :disabled="isLoading"
+                        :loading="isLoading"
+                        @click="handleSwitch(feature)"
+                    />
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script lang="ts">
+    import { computed, defineComponent, ref, toRefs, watch } from 'vue'
+    import { featureList } from '~/composables/labs/labFeatureList'
+
+    export default defineComponent({
+        name: 'AdminLabs',
+        components: {},
+        setup() {
+            const handleSwitch = (feature) => {}
+            return { featureList, handleSwitch }
+        },
+    })
+</script>
+
+<style scoped></style>

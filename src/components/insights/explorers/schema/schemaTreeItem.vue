241b1e36b98f0ab8f7de59ad54bf63b01de320bc
Kirtik Singh <kirtiksingh8@gmail.com>
2022-04-20 15:44:23 +0530

    Removed transition delay from schema tree

--- src/components/insights/explorers/schema/schemaTreeItem.vue
+++ src/components/insights/explorers/schema/schemaTreeItem.vue
@@ -54,7 +54,7 @@
                             </div>
                             <div
                                 v-if="hoverActions"
-                                class="absolute right-0 flex items-center text-gray-500 transition opacity-0 h-7 margin-align-top group-hover:opacity-100"
+                                class="absolute right-0 flex items-center text-gray-500 opacity-0 h-7 margin-align-top group-hover:opacity-100"
                                 style="width: "
                                 :class="
                                     item?.selected
@@ -193,7 +193,7 @@
 
                             <div
                                 v-if="hoverActions"
-                                class="absolute right-0 flex items-center text-gray-500 transition opacity-0 h-7 margin-align-top group-hover:opacity-100"
+                                class="absolute right-0 flex items-center text-gray-500 opacity-0 h-7 margin-align-top group-hover:opacity-100"
                                 @click.stop="() => {}"
                                 :class="
                                     item?.selected
@@ -368,7 +368,7 @@
 
                     <div
                         v-if="hoverActions"
-                        class="absolute right-0 flex items-center text-gray-500 transition opacity-0 h-7 margin-align-top group-hover:opacity-100"
+                        class="absolute right-0 flex items-center text-gray-500 opacity-0 h-7 margin-align-top group-hover:opacity-100"
                         :class="
                             item?.selected
                                 ? 'bg-gradient-to-l from-tree-light-color  via-tree-light-color '
@@ -485,7 +485,7 @@
                         </span>
                         <div
                             v-if="hoverActions"
-                            class="absolute right-0 flex items-center text-gray-500 transition opacity-0 h-7 margin-align-top group-hover:opacity-100"
+                            class="absolute right-0 flex items-center text-gray-500 opacity-0 h-7 margin-align-top group-hover:opacity-100"
                             @click.stop="() => {}"
                             :class="
                                 item?.selected

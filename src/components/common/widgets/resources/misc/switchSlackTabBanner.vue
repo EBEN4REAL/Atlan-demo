92391835e6f8e8f4a46864811832b6024653b150
Samiran Konwar <saammmmmmy@gmail.com>
2022-04-27 15:29:11 +0530

    fix: slack switcth tab CTA move event trigger to parent div

--- src/components/common/widgets/resources/misc/switchSlackTabBanner.vue
+++ src/components/common/widgets/resources/misc/switchSlackTabBanner.vue
@@ -7,8 +7,9 @@
         <div
             class="inline-block text-new-gray-800 py-1.5 px-2 mx-5 my-2 rounded cursor-pointer text-xs bg-new-gray-100 group hover:bg-new-gray-200"
             style="box-shadow: 0px 1px 0px rgba(0, 0, 0, 0.05)"
+            @click="$emit('switchTab')"
         >
-            <span class="" @click="$emit('switchTab')">
+            <span class="">
                 <AtlanIcon icon="Slack" class="h-4" /> Slack threads
             </span>
             <span

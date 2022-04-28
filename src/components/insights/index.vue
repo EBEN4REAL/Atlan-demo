1921af9b9d21a7dab79ca0e84de2ce9e346f537f
Samiran Konwar <saammmmmmy@gmail.com>
2022-04-28 15:45:54 +0530

    fix: last active tab on mount @ insights explorer

--- src/components/insights/index.vue
+++ src/components/insights/index.vue
@@ -310,10 +310,10 @@
                     ? assetSidebarPaneSize.value
                     : 0
             )
-            const activeExplorerTab =
+            const getActiveExplorerTabId =
                 getActiveExplorerTab() &&
-                tabsList.find((tab) => tab.id === getActiveExplorerTab())
-            const activeTabId = ref(activeExplorerTab || tabsList[0].id)
+                tabsList.find((tab) => tab.id === getActiveExplorerTab())?.id
+            const activeTabId = ref(getActiveExplorerTabId || tabsList[0].id)
 
             const activeTab = computed(() =>
                 tabsList.find((tab) => tab.id === activeTabId.value)

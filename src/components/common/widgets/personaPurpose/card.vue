dd843612461d90e4e025e499ff0f114fe6df583a
AtlanKautzar <kautzar@atlan.com>
2022-05-10 15:56:22 +0700

    feat: fix sliding and handle empty state

--- src/components/common/widgets/personaPurpose/card.vue
+++ src/components/common/widgets/personaPurpose/card.vue
@@ -1,76 +1,82 @@
 <template>
-    <div class="border border-gray-300 rounded-xl">
-        <div class="flex h-9">
-            <div
-                v-if="connection.length"
-                class="p-1 bg-gray-100 rounded-tl-xl rounded-br-xl"
-            >
-                <div class="flex p-1.5 bg-white rounded-tl-xl rounded-br-xl">
+    <div class="pr-3">
+        <div class="border border-gray-300 rounded-xl">
+            <div class="flex h-9">
+                <div
+                    v-if="connection.length"
+                    class="p-1 bg-gray-100 rounded-tl-xl rounded-br-xl"
+                >
                     <div
-                        v-for="imgPath in connection"
-                        :key="imgPath"
-                        class="rounded-full relativebg-white fit"
+                        class="flex p-1.5 bg-white rounded-tl-xl rounded-br-xl"
                     >
-                        <img class="w-4 h-4" :src="imgPath" />
+                        <div
+                            v-for="imgPath in connection"
+                            :key="imgPath"
+                            class="rounded-full relativebg-white fit"
+                        >
+                            <img class="w-4 h-4" :src="imgPath" />
+                        </div>
+                    </div>
+                </div>
+                <div
+                    v-if="item?.tags?.length"
+                    class="p-1 bg-gray-100 rounded-tl-xl rounded-br-xl"
+                >
+                    <div
+                        class="flex items-center p-1.5 bg-white rounded-tl-xl rounded-br-xl text-xs text-gray-700"
+                    >
+                        <AtlanIcon
+                            icon="ClassificationShield"
+                            class="w-4 h-4 mr-1 stroke-current text-primary"
+                        />
+                        {{
+                            `${item?.tags?.length} ${
+                                item?.tags?.length > 1
+                                    ? 'classifications'
+                                    : 'classification'
+                            }`
+                        }}
                     </div>
                 </div>
             </div>
-            <div
-                v-if="item?.tags?.length"
-                class="p-1 bg-gray-100 rounded-tl-xl rounded-br-xl"
-            >
-                <div
-                    class="flex items-center p-1.5 bg-white rounded-tl-xl rounded-br-xl text-xs text-gray-700"
-                >
-                    <AtlanIcon
-                        icon="ClassificationShield"
-                        class="w-4 h-4 mr-1 stroke-current text-primary"
-                    />
-                    {{
-                        `${item?.tags?.length} ${
-                            item?.tags?.length > 1
-                                ? 'classifications'
-                                : 'classification'
-                        }`
-                    }}
+            <div class="px-4 mt-3">
+                <div class="w-40 text-base font-bold truncate text-primary">
+                    {{ item.name }}
+                </div>
+                <div class="w-40 h-16 mt-2 text-xs text-gray-600 line-clamp-2">
+                    {{ item.description || 'No description' }}
                 </div>
             </div>
-        </div>
-        <div class="px-4 mt-3">
-            <div class="w-40 text-base font-bold truncate text-primary">
-                {{ item.name }}
+            <div class="flex items-center h-6 px-4">
+                <Avatar
+                    v-for="(user, index) in users"
+                    :key="user"
+                    :avatar-size="24"
+                    :avatar-shape="'circle'"
+                    :style="{
+                        'z-index': `${users.length - 1 - index}`,
+                        transform: `translateX(-${index * 8}px)`,
+                    }"
+                />
+                <div
+                    v-if="item.users?.length > 3"
+                    class="flex items-center justify-center w-6 h-6 mt-1 text-xs text-gray-600 bg-gray-100 rounded-full"
+                    :style="{
+                        'z-index': `4`,
+                        transform: `translateX(-24px)`,
+                    }"
+                >
+                    +{{ item.users?.length - 3 }}
+                </div>
             </div>
-            <div class="w-40 h-16 mt-2 text-xs text-gray-600 line-clamp-2">
-                {{ item.description || 'No description' }}
-            </div>
-        </div>
-        <div class="flex items-center h-6 px-4">
-            <Avatar
-                v-for="(user, index) in users"
-                :key="user"
-                :avatar-size="24"
-                :avatar-shape="'circle'"
-                :style="{
-                    'z-index': `${users.length - 1 - index}`,
-                    transform: `translateX(-${index * 8}px)`,
-                }"
-            />
             <div
-                v-if="item.users?.length > 3"
-                class="flex items-center justify-center w-6 h-6 mt-1 text-xs text-gray-600 bg-gray-100 rounded-full"
-                :style="{
-                    'z-index': `4`,
-                    transform: `translateX(-24px)`,
-                }"
+                class="flex items-center px-4 py-3 mt-4 border-t border-gray-300"
             >
-                +{{ item.users?.length - 3 }}
+                <div class="text-xs cursor-pointer text-primary">
+                    View from 1.2k assets
+                </div>
+                <AtlanIcon icon="ArrowRight" class="ml-2 text-primary" />
             </div>
-        </div>
-        <div class="flex items-center px-4 py-3 mt-4 border-t border-gray-300">
-            <div class="text-xs cursor-pointer text-primary">
-                View from 1.2k assets
-            </div>
-            <AtlanIcon icon="ArrowRight" class="ml-2 text-primary" />
         </div>
     </div>
 </template>
--- src/components/common/widgets/personaPurpose/index.vue
+++ src/components/common/widgets/personaPurpose/index.vue
@@ -43,14 +43,68 @@
                 </div>
             </div>
         </div>
-        <div v-if="!loadingChange" class="p-6">
+        <div
+            v-if="!items.length"
+            class="relative flex flex-col items-center p-4 m-6 bg-gray-100 rounded-xl h-60"
+        >
+            <div
+                class="absolute flex items-center p-2 text-xs text-gray-800 bg-white rounded top-2 right-2"
+            >
+                <AtlanIcon icon="Info" class="w-3 h-3 mr-1" />
+                {{
+                    activeTab === 'persona'
+                        ? 'You are not part of any persona yet'
+                        : 'You are not part of any purpose yet'
+                }}
+            </div>
+            <img
+                :src="
+                    activeTab === 'persona'
+                        ? illustrationPersonaDemo
+                        : illustrationPurposeDemo
+                "
+                class="ilustration"
+            />
+            <div class="mt-6 text-base font-bold">
+                {{ activeTab === 'persona' ? 'Personas' : 'Purpose' }}
+            </div>
+            <div class="w-40 mt-1 text-sm text-center text-gray-600">
+                {{
+                    activeTab === 'persona'
+                        ? 'Some description about personas goes here'
+                        : 'Some description about purpose goes here'
+                }}
+            </div>
+            <div
+                class="flex items-center mt-5 text-sm cursor-pointer text-primary"
+            >
+                View documentation
+                <AtlanIcon icon="External" class="h-4 ml-1" />
+            </div>
+        </div>
+        <div v-else-if="!loadingChange" class="p-6">
             <Carousel
-                :variable-width="200"
                 :draggable="true"
                 :swipe="true"
                 :arrows="true"
                 :infinite="false"
+                :slides-to-show="3"
+                :slides-to-scroll="3"
             >
+                <template #prevArrow>
+                    <div
+                        class="z-10 flex items-center justify-center w-8 h-8 text-gray-800 bg-white border border-gray-300 border-solid rounded-full"
+                    >
+                        <AtlanIcon icon="ChevronLeft" class="w-4 h-4" />
+                    </div>
+                </template>
+                <template #nextArrow>
+                    <div
+                        class="flex items-center justify-center w-8 h-8 text-gray-800 bg-white border border-gray-300 border-solid rounded-full"
+                    >
+                        <AtlanIcon icon="ChevronRight" class="w-4 h-4" />
+                    </div>
+                </template>
                 <Card
                     v-for="item in items"
                     :key="item.id"
@@ -59,19 +113,25 @@
                 />
             </Carousel>
         </div>
+        <div v-else class="flex items-center justify-center h-64">
+            <AtlanLoader icon="CircleLoader" class="h-8 animate-spin" />
+        </div>
     </div>
 </template>
 
 <script lang="ts">
     import { defineComponent, computed, ref, watch } from 'vue'
     import { Carousel } from 'ant-design-vue'
+    import AtlanLoader from '@common/loaders/atlanLoader.vue'
     import Card from './card.vue'
     import { usePersonaStore } from '~/store/persona'
     import { usePurposeStore } from '~/store/purpose'
+    import illustrationPersonaDemo from '~/assets/images/illustrations/illustration-persona-demo.png'
+    import illustrationPurposeDemo from '~/assets/images/illustrations/illustration-purpose-demo.png'
 
     export default defineComponent({
         name: 'WidgetPersonaPurpose',
-        components: { Card, Carousel },
+        components: { Card, Carousel, AtlanLoader },
         props: {},
         setup() {
             const activeTab = ref('persona')
@@ -84,7 +144,7 @@
                 loadingChange.value = true
                 setTimeout(() => {
                     loadingChange.value = false
-                }, 400)
+                }, 100)
             })
             const items = computed(() =>
                 activeTab.value === 'persona' ? personas.value : purposes.value
@@ -95,22 +155,22 @@
                 purposes,
                 items,
                 loadingChange,
+                illustrationPersonaDemo,
+                illustrationPurposeDemo,
             }
         },
     })
 </script>
 
 <style lang="less" scoped>
+    .ilustration {
+        height: 75px;
+        width: 92px;
+    }
     .persona-purpose-widget {
         box-shadow: 0px 8px 24px 0px #1920380a;
     }
     .active-widget-tab {
         box-shadow: 0px 1px 4px 0px #0000001f;
-    }
-</style>
-<style lang="less">
-    .slick-slide {
-        // width: 200px !important;
-        margin-right: 16px;
     }
 </style>

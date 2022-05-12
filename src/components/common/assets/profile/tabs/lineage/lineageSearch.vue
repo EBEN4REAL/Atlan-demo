1e2c519218ed876837f33315c32413b34c55138d
Abdulhakeem <abdulhakeem@atlan.com>
2022-05-12 20:33:41 +0100

    FIX: Ignore event on empty search query

--- src/components/common/assets/profile/tabs/lineage/lineageSearch.vue
+++ src/components/common/assets/profile/tabs/lineage/lineageSearch.vue
@@ -143,7 +143,7 @@
             const setQuery = (e) => {
                 query.value = e.target.value
 
-                sendSearchEvent()
+                if (e.target.value) sendSearchEvent()
             }
 
             // setSearchItem

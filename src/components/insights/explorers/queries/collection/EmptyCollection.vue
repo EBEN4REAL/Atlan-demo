c0ff53697fedd4b5741a1265478d8ce4a286c4c1
Kirtik Singh <kirtiksingh8@gmail.com>
2022-05-17 01:13:33 +0530

    CSS changes in collections' empty state

--- src/components/insights/explorers/queries/collection/EmptyCollection.vue
+++ src/components/insights/explorers/queries/collection/EmptyCollection.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="flex flex-col items-center w-full h-full">
+    <div class="flex flex-col items-center h-full">
         <div class="flex flex-col items-center w-full">
             <AtlanIcon icon="CollectionHeader" class="w-full h-auto" />
-            <span class="-mt-12 text-2xl font-bold bg-transparent"
+            <span class="-mt-20 text-2xl font-bold bg-transparent"
                 >Collections</span
             >
             <p style="max-width: 18rem" class="mt-2 text-base text-center">
@@ -26,7 +26,7 @@
         </div>
 
         <AtlanButton2
-            class="mt-6 font-bold w-36"
+            class="w-56 mt-6 font-bold"
             size="large"
             label="Create a collection"
             suffixIcon="ArrowRight"

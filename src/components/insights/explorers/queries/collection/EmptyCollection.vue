2f2f4b96d1facd9958d8db2871154eee60d36bc7
Kirtik Singh <kirtiksingh8@gmail.com>
2022-05-17 12:28:39 +0530

    Fixed collection empty state layout

--- src/components/insights/explorers/queries/collection/EmptyCollection.vue
+++ src/components/insights/explorers/queries/collection/EmptyCollection.vue
@@ -1,8 +1,7 @@
 <template>
-    <div class="flex flex-col items-center h-full">
+    <div class="flex flex-col items-center h-full" style="width: 350px">
         <div class="flex flex-col items-center w-full">
-            <AtlanIcon icon="CollectionHeader" class="w-full h-auto" />
-            <span class="-mt-20 text-2xl font-bold bg-transparent"
+            <span class="mt-4 text-2xl font-bold bg-transparent"
                 >Collections</span
             >
             <p style="max-width: 18rem" class="mt-2 text-base text-center">

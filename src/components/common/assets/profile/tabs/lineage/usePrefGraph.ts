a5714e1ecc77373ab6a671a7673b9f9a96d17d0a
Abdulhakeem <abdulhakeem@atlan.com>
2022-05-20 03:20:14 +0100

    FEAT: Lineage control panel events (dispaly pref, full screen, zoom, refocus, control toggle)

--- src/components/common/assets/profile/tabs/lineage/usePrefGraph.ts
+++ src/components/common/assets/profile/tabs/lineage/usePrefGraph.ts
@@ -1,5 +1,7 @@
 /** VUE */
 import { watch } from 'vue'
+import { useDebounceFn } from '@vueuse/core'
+import useAddEvent from '~/composables/eventTracking/useAddEvent'
 
 /** COMPOSABLES */
 import useLineageStore from '~/store/lineage'
@@ -7,6 +9,14 @@ import useLineageStore from '~/store/lineage'
 export default function useGraph({ graph }) {
     const lineageStore = useLineageStore()
     const preferences = lineageStore.getPreferences()
+
+    /** EVENT DEFINITIONS */
+    const sendDisplayPreferenceEvent = useDebounceFn((option, is_enabled) => {
+        useAddEvent('lineage', 'control_panel_display_preference', 'updated', {
+            option,
+            is_enabled,
+        })
+    }, 600)
 
     // controlEdgesArrow
     const controlEdgesArrow = () => {
@@ -44,11 +54,23 @@ export default function useGraph({ graph }) {
     }
 
     watch(
-        preferences,
-        () => {
+        () => ({ ...preferences }),
+        (newVal, oldVal) => {
             controlPrefRetainer()
-        },
-        { deep: true }
+
+            // Handle Event - lineage_control_panel_full_screen_toggled
+            // eslint-disable-next-line no-restricted-syntax
+            for (const key in newVal) {
+                if (newVal[key] !== oldVal[key]) {
+                    sendDisplayPreferenceEvent(
+                        key.replace('show', '').toLowerCase(),
+                        newVal[key]
+                    )
+
+                    break
+                }
+            }
+        }
     )
 
     return {

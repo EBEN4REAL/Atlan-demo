e719737d9ff93672dbf7c4c6eca0ff74526d7156
Abdulhakeem <abdulhakeem@atlan.com>
2022-06-01 06:56:42 +0100

    Design guide fix for update request popover

--- src/components/governance/requests/pieces/attributeUpdate.vue
+++ src/components/governance/requests/pieces/attributeUpdate.vue
@@ -2,8 +2,8 @@
     <template v-if="name === 'certificateStatus'">
         <div class="flex flex-col">
             <span class="pb-1 mr-2 text-sm text-gray-500">
-                Update Certificate</span
-            >
+                Update Certificate
+            </span>
             <StatusBadge :status-id="value" show-no-status show-label />
         </div>
     </template>
@@ -58,32 +58,37 @@
                         <a-popover v-model:visible="visible" trigger="hover">
                             <template #title>
                                 <div
-                                    class="flex flex-row items-center justify-between py-2"
+                                    class="flex flex-row items-center justify-between py-2 text-gray"
                                 >
-                                    <h1 class="text-lg font-semibold">
-                                        User Description
+                                    <h1 class="text-sm font-bold">
+                                        Update Description
                                     </h1>
                                     <atlan-icon
                                         icon="Close"
-                                        class="text-gray-400 cursor-pointer h-7 hover:text-blueGray"
+                                        class="cursor-pointer text-blueGray h-7 hover:text-gray"
                                         @click="visible = !visible"
                                     />
                                 </div>
                             </template>
 
                             <template #content>
-                                <div class="flex flex-col px-4 pt-5 w-96">
+                                <div
+                                    class="flex flex-col px-4 pt-4 -mb-2 text-sm text-gray"
+                                    :style="{
+                                        width: '400px',
+                                    }"
+                                >
                                     <div
-                                        class="w-full p-3 py-2.5 bg-gray-100 border-l-4 rounded-md border-primary"
+                                        class="w-full p-2 py-2.5 bg-gray-100 border-l-2 rounded-md border-primary"
                                     >
-                                        <h2 class="font-bold text-gray-400">
+                                        <h2 class="font-normal text-gray-500">
                                             New description
                                         </h2>
 
                                         <div
-                                            class="w-full overflow-x-scroll desc-wrapper"
+                                            class="w-full mt-2 overflow-x-scroll desc-wrapper"
                                         >
-                                            <p class="mt-3 desc-text">
+                                            <p class="font-bold desc-text">
                                                 {{ value }}
                                             </p>
                                         </div>
@@ -91,15 +96,15 @@
 
                                     <div
                                         v-if="request.message"
-                                        class="flex flex-row mt-8"
+                                        class="flex flex-row mt-6 font-normal"
                                     >
                                         <atlan-icon
                                             icon="Quotes"
-                                            class="h-5 cursor-pointer text-blueGray"
+                                            class="h-4 text-gray-500 cursor-pointer"
                                             @click="visible = !visible"
                                         />
 
-                                        <p class="ml-4">
+                                        <p class="ml-3">
                                             {{ request.message }}
                                         </p>
                                     </div>
@@ -110,7 +115,7 @@
                                         <Avatar
                                             :allow-upload="false"
                                             :avatar-name="request.createdBy"
-                                            :avatar-size="28"
+                                            :avatar-size="20"
                                             :avatar-shape="'circle'"
                                             class="mr-2"
                                             :image-url="
@@ -119,7 +124,7 @@
                                         />
 
                                         <p
-                                            class="text-sm text-gray-400 font-base"
+                                            class="text-xs font-normal text-gray-500"
                                         >
                                             From {{ request.createdBy }}
                                         </p>
@@ -188,9 +193,6 @@
             RequestActions,
         },
 
-        mounted() {
-            console.log('Heyyyy: ', this.request)
-        },
         setup(props, { emit }) {
             const { name } = toRefs(props)
             const labelMap = {

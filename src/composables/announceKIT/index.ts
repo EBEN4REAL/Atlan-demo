5af24aee40e6b57f0165409f54276088b05c0554
Shekh Aliul <shekhaliul44@gmail.com>
2022-06-14 16:52:24 +0530

    refactor: [EXP-79] removed jose

--- src/composables/announceKIT/index.ts
+++ src/composables/announceKIT/index.ts
@@ -1,6 +1,4 @@
 import whoami from '~/composables/user/whoami'
-import { signJWT } from '~/composables/utils/jose'
-import { getEnv } from '~/modules/__env'
 
 export const initAnnounceKit = async (
     target: string,
@@ -13,12 +11,6 @@ export const initAnnounceKit = async (
         name: username.value,
         email: email.value,
     }
-    const user_token = await signJWT({
-        data,
-        secretKey: getEnv().ANNOUNCEKIT_SECRET,
-        algorithm: 'HS256',
-        expirationTime: '24h',
-    })
 
     if (window?.announcekit)
         window.announcekit.push({
@@ -26,7 +18,9 @@ export const initAnnounceKit = async (
             widget: `https://announcekit.co/widgets/v2/${widgetID}`,
             selector: target,
             lang: 'en',
-            user_token,
+            user: {
+                ...data,
+            },
             data: {
                 domain: window.location.hostname,
                 username: username.value,

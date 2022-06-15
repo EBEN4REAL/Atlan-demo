dafc9de19a665d65777070758f6a564c7a00e852
shivanshpahwa24 <shivanshpahwa24@gmail.com>
2022-06-15 14:42:50 +0530

    remove readme CTA from the sidebar when in profile

--- src/components/common/assets/preview/info/index.vue
+++ src/components/common/assets/preview/info/index.vue
@@ -1158,8 +1158,9 @@
 
             <div
                 v-if="
-                    selectedAsset.typeName === 'Column' ||
-                    readmeGuid(selectedAsset)
+                    (selectedAsset.typeName === 'Column' ||
+                        readmeGuid(selectedAsset)) &&
+                    !isProfile
                 "
                 class="flex flex-col px-5"
             >

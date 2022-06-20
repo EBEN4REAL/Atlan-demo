4c43e46f367a27bbfa4d4e34f600129b9a4d36aa
Shekh Aliul <shekhaliul44@gmail.com>
2022-06-20 12:04:16 +0530

    feat: [INSIG-541] skeleton loader add as suspense to async image load

--- src/components/common/icon/atlanIcon.vue
+++ src/components/common/icon/atlanIcon.vue
@@ -1,9 +1,21 @@
 <template>
-    <component
-        :is="svgIcon"
-        :style="{ height: '1rem' }"
-        class="flex-none inline w-auto"
-    />
+    <Suspense>
+        <template #default>
+            <component
+                :is="svgIcon"
+                :style="{ height: '1rem' }"
+                class="flex-none inline w-auto"
+                :class="className"
+            />
+        </template>
+        <template #fallback>
+            <div
+                class="rounded-md skeleton"
+                style="min-height: 16px; min-width: 16px"
+                :class="className"
+            ></div>
+        </template>
+    </Suspense>
 </template>
 
 <script lang="ts">
@@ -17,11 +29,16 @@
                 type: String as PropType<keyof typeof iconMap>,
                 required: true,
             },
+            class: {
+                type: String,
+                required: false,
+                default: '',
+            },
         },
         setup(props) {
-            const { icon } = toRefs(props)
+            const { icon, class: className } = toRefs(props)
             const svgIcon = computed(() => iconMap[icon.value] || 'div')
-            return { svgIcon, icon }
+            return { svgIcon, icon, className }
         },
     })
 </script>
@@ -30,5 +47,20 @@
     .test {
         height: 100%;
         width: 100%;
+    }
+</style>
+<style lang="less" scoped>
+    .skeleton {
+        background: linear-gradient(
+            90deg,
+            rgba(190, 190, 190, 0.2) 25%,
+            rgba(129, 129, 129, 0.24) 37%,
+            rgba(190, 190, 190, 0.2) 63%
+        );
+        background-size: 400% 100%;
+        -webkit-animation: ant-skeleton-loading 1.4s ease infinite;
+        animation: ant-skeleton-loading 1.4s ease infinite;
+
+        list-style: none;
     }
 </style>

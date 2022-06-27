fa4b261629aa9f2d4740ad07e121a88d47dea3b6
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2022-06-27 14:56:14 +0100

    Fix: added check for when search query is greater than zero

--- src/components/common/assets/preview/columns/index.vue
+++ src/components/common/assets/preview/columns/index.vue
@@ -61,7 +61,7 @@
             <EmptyView
                 empty-screen="NoAssetsFound"
                 image-class="h-44"
-                :desc="`No columns found with ${queryText}`"
+                :desc="`No columns found ${queryText.length > 0 ? 'with ' + queryText : ''}`"
             ></EmptyView>
         </div>
         <div
